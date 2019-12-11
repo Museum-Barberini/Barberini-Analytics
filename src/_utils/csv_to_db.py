Museum-Barberini/Barberini-Analytics

commit e4ca2fe68d459fa36dba0343db177707bd6620b2

Apply suggestions to csv_to_db.py
- Move sql files into subfolder
- Update documentation comments for them
- Specify sql_file_path_pattern as a luigi parameter
- Change quotes

src/_utils/csv_to_db.py
@@ -18,6 +18,7 @@ class CsvToDb(CopyToTable):
 		super().__init__(*args, **kwargs)
 		set_db_connection_options(self)
 
+	sql_file_path_pattern = luigi.Parameter(default='src/_utils/csv_to_db/{0}.sql')
 	
 	def init_copy(self, connection):
 		if not self.check_existence(connection):
@@ -25,7 +26,8 @@ class CsvToDb(CopyToTable):
 		super().init_copy(connection)
 	
 	def copy(self, cursor, file):
-		query = load_sql_script("copy", self.table, ",".join([f"{col[0]} = EXCLUDED.{col[0]}" for col in self.columns]))
+		query = load_sql_script('copy', self.table, ','.join(
+			[f'{col[0]} = EXCLUDED.{col[0]}' for col in self.columns]))
 		cursor.copy_expert(query, file)
 	
 	def rows(self):
@@ -35,7 +37,7 @@ class CsvToDb(CopyToTable):
 	
 	def check_existence(self, connection):
 		cursor = connection.cursor()
-		cursor.execute(load_sql_script("check_existence", self.table))
+		cursor.execute(load_sql_script('check_existence', self.table))
 		return cursor.fetchone()[0]
 	
 	def create_table(self, connection):
@@ -45,19 +47,17 @@ class CsvToDb(CopyToTable):
 	def create_primary_key(self, connection):
 		prim_key = self.primary_key
 		if isinstance(prim_key, tuple):
-			prim_key = ",".join(prim_key)
-		prim_key = f"({prim_key})"
-		connection.cursor().execute(load_sql_script("set_primary_key", self.table, prim_key))
+			prim_key = ','.join(prim_key)
+		prim_key = f'({prim_key})'
+		connection.cursor().execute(load_sql_script('set_primary_key', self.table, prim_key))
 	
 	@property
 	def primary_key(self):
 		raise NotImplemented()
-
-
-sql_file_path_pattern = 'src/_utils/csv_to_db.{0}.sql'
-def load_sql_script(name, *args):
-	with open(sql_file_path_pattern.format(name)) as sql_file:
-		return sql_file.read().format(*args)
+	
+	def load_sql_script(self, name, *args):
+		with open(self.sql_file_path_pattern.format(name)) as sql_file:
+			return sql_file.read().format(*args)
 
 class UndefinedTableError(psycopg2.ProgrammingError):
 	pgcode = psycopg2.errorcodes.UNDEFINED_TABLE

src/_utils/csv_to_db/check_existence.sql
@@ -1,3 +1,5 @@
+-- {0} -- table name
+
 SELECT EXISTS (
 	SELECT 42
 	FROM   information_schema.tables 

src/_utils/csv_to_db/copy.sql
@@ -1,5 +1,5 @@
 -- {0} -- target table name
--- {1} -- temporary table name
+-- {1} -- columns to update
 
 BEGIN;
 CREATE TEMPORARY TABLE {0}_tmp (LIKE {0} INCLUDING ALL);

commit 3f4d40a8aca9026d2fe35a16b5c7884582a23522

Fix a bug introduced in e4ca2fe68d459fa36dba0343db177707bd6620b2

src/_utils/csv_to_db.py
@@ -30,7 +30,7 @@ class CsvToDb(CopyToTable):
 		super().init_copy(connection)
 	
 	def copy(self, cursor, file):
-		query = load_sql_script('copy', self.table, ','.join(
+		query = self.load_sql_script('copy', self.table, ','.join(
 			[f'{col[0]} = EXCLUDED.{col[0]}' for col in self.columns]))
 		cursor.copy_expert(query, file)
 	
@@ -41,7 +41,7 @@ class CsvToDb(CopyToTable):
 	
 	def check_existence(self, connection):
 		cursor = connection.cursor()
-		cursor.execute(load_sql_script('check_existence', self.table))
+		cursor.execute(self.load_sql_script('check_existence', self.table))
 		existence_boolean = cursor.fetchone()[0]
 		return existence_boolean
 	
@@ -50,7 +50,7 @@ class CsvToDb(CopyToTable):
 		self.create_primary_key(connection)
 	
 	def create_primary_key(self, connection):
-		connection.cursor().execute(load_sql_script(
+		connection.cursor().execute(self.load_sql_script(
 			'set_primary_key',
 			self.table,
 			self.tuple_like_string(self.primary_key)))