fd6c39386cb534cd7c7503bbda4f03d18cda613a
LinqLover <christoph.thiede@student.hpi.de>
2019-12-11 18:56:50 +0100

    First draft of schemaspy

---
+++ scripts/update_schemaspy.sh
@@ -0,0 +1,6 @@
+# TODO: This does not respect db_config.yaml
+# TODO: I have no clue why this does need sudo
+cd output && sudo java -jar ../schemaspy.jar \
+	-t pgsql -dp ../postgresql.jar \
+	-host localhost -s public -db barberini -u postgres -p docker
+	-o schemaspy
