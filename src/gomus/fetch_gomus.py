f3f43c2815af9b021cc172f916536519e7199830
LinqLover <christoph.thiede@student.hpi.de>
2019-12-12 14:54:51 +0100

    Fix spaces and indentations in gomus

--- src/gomus/fetch_gomus.py
+++ src/gomus/fetch_gomus.py
@@ -7,7 +7,6 @@
 ####  ######      ###   ####        ## ###
 ##########################################
 # TODOs for the next editor:
-# - This file is in old space-indentation layout. Migrate to tabs.
 # - Merge this file with the calling task. We decided not to have separate scripts for things that represent a task.
 # ct.
 
@@ -25,26 +24,26 @@ report_ids = {
 	'customers_7days': 1186,
 	'orders_7days': 1188,
 	'bookings_7days': 0,
-
+	
 	'bookings_1year': -1,
-
+	
 	'guides': -2
 }
 
 def parse_arguments(args):
-	parser = argparse.ArgumentParser(description='Refresh and fetch reports from go~mus')
+	parser = argparse.ArgumentParser(description="Refresh and fetch reports from go~mus")
 	report_group = parser.add_mutually_exclusive_group(required=True)
-
-	report_group.add_argument('-i', '--report-id', type=int, help='ID of the report', choices=report_ids.values())
-	report_group.add_argument('-t', '--report-type', type=str, help='Type of the report', choices=report_ids.keys())
-
-	parser.add_argument('action', type=str, help='Action to take', choices=['refresh', 'fetch'], nargs='?', default='fetch')
-	parser.add_argument('-s', '--session-id', type=str, help='Session ID to use for authentication', required=True)
-
-	parser.add_argument('-o', '--output-file', type=str, help='Name of Output file (for fetching)')
-
-	parser.add_argument('-l', '--luigi', help='Set true if run as part of a Luigi task', action='store_true')
-
+	
+	report_group.add_argument('-i', '--report-id', type=int, help="ID of the report", choices=report_ids.values())
+	report_group.add_argument('-t', '--report-type', type=str, help="Type of the report", choices=report_ids.keys())
+	
+	parser.add_argument('action', type=str, help="Action to take", choices=['refresh', 'fetch'], nargs='?', default='fetch')
+	parser.add_argument('-s', '--session-id', type=str, help="Session ID to use for authentication", required=True)
+	
+	parser.add_argument('-o', '--output-file', type=str, help="Name of Output file (for fetching)")
+	
+	parser.add_argument('-l', '--luigi', help="Set true if run as part of a Luigi task", action='store_true')
+	
 	return parser.parse_args(args)
 			
 def direct_download_url(base_url, report, timespan):
@@ -56,13 +55,13 @@ def direct_download_url(base_url, report, timespan):
 	elif timespan == '1year':
 		start_time = today - datetime.timedelta(days=365)
 	else: no_time = True
-
+	
 	base_return = base_url + f'/{report}.xlsx'
-
+	
 	if not no_time:
 		end_time = end_time.strftime("%Y-%m-%d")
 		start_time = start_time.strftime("%Y-%m-%d")
-		print(f'Requesting report for timespan from {start_time} to {end_time}')
+		print(f"Requesting report for timespan from {start_time} to {end_time}"")
 		return base_return + f'?end_at={end_time}&start_at={start_time}'
 	
 	return base_return
@@ -72,10 +71,10 @@ def get_request(url, sess_id):
 	cookies = dict(_session_id=sess_id)
 	res = requests.get(url, cookies=cookies)
 	if not res.status_code == 200:
-		print(f'Error with HTTP request: Status code {res.status_code}')
+		print(f"Error with HTTP request: Status code {res.status_code}")
 		exit(1)
 	else:
-		print('HTTP request successful')
+		print("HTTP request successful")
 	
 	return res.content
 
@@ -94,31 +93,31 @@ def request_report(args=sys.argv[1:]):
 		try:
 			report_id = report_ids[args.report_type]
 		except KeyError: # should never happen because of argparse choices
-			print(f'Error: Report type \'{args.report_type}\' not supported!')
+			print(f"Error: Report type '{args.report_type}' not supported!")
 			exit(1)
-
+	
 	base_url = 'https://barberini.gomus.de'
-
+	
 	report_ids_inv = {v: k for k, v in report_ids.items()}
 	report_parts = report_ids_inv[report_id].split("_")
 	
-	print(f'Working with report "{report_parts[0]}.xlsx"')
-
+	print(f"Working with report '{report_parts[0]}.xlsx'")
+	
 	if report_id > 0: # Work with the kind of report that is generated and maintained
 		base_url += f'/admin/reports/{report_id}'
-
+		
 		if args.action == 'refresh':
-			print('Refreshing report')
+			print("Refreshing report")
 			url = base_url + '/refresh'
-
+		
 		elif args.action == 'fetch':
-			print('Fetching report')
+			print("Fetching report")
 			url = base_url + '.xlsx'
 
 	else: # Work with the kind of report that is requested directly
-		print('Directly downloading report')
+		print("Directly downloading report")
 		if args.action == 'refresh':
-			print('Error: Directly downloaded reports cannot be refreshed')
+			print("Error: Directly downloaded reports cannot be refreshed")
 			exit(1)
 		if len(report_parts) < 2:
 			timespan = ''
@@ -135,9 +134,9 @@ def request_report(args=sys.argv[1:]):
 			if not filename: filename = report_ids_inv[report_id] + '.csv'
 			with open(filename, 'w', encoding='utf-8') as csv_file:
 				csv_from_excel(res_content, csv_file)
-			print(f'Saved report to file "{filename}"')
+			print(f"Saved report to file '{filename}'")
 		else:
-			print('Running as Luigi task, returning response content')
+			print("Running as Luigi task, returning response content")
 			return res_content
 
 if __name__ == '__main__':
--- src/gomus/gomus_report.py
+++ src/gomus/gomus_report.py
@@ -6,24 +6,24 @@ import os
 import time
 
 class FetchGomusReport(luigi.Task):
-	report = luigi.parameter.Parameter(description='The report name (e.g. \'bookings\')')
-	suffix = luigi.parameter.OptionalParameter(default='_7days', description='The report suffix (default: \'_7days\')')
-
+	report = luigi.parameter.Parameter(description="The report name (e.g. 'bookings')")
+	suffix = luigi.parameter.OptionalParameter(default='_7days', description="The report suffix (default: '_7days')")
+	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.report_name = self.report + self.suffix
-
+	
 	def output(self):
 		return luigi.LocalTarget(f'output/{self.report_name}.csv', format=UTF8)
-
+	
 	def run(self):
 		sess_id = os.environ['GOMUS_SESS_ID']
-
+		
 		if report_ids[f'{self.report_name}'] > 0: # report refreshable
 			request_report(args=['-s', f'{sess_id}', '-t', f'{self.report_name}', 'refresh'])
-			print('Waiting 60 seconds for the report to refresh')
+			print("Waiting 60 seconds for the report to refresh")
 			time.sleep(60)
-
+		
 		res_content = request_report(args=['-s', f'{sess_id}', '-t', f'{self.report_name}', '-l'])
 		with self.output().open('w') as target_csv:
 			csv_from_excel(res_content, target_csv)
