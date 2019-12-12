b06e07c250685b2be6930ce7b80cf37203e9ed2d
LinqLover <christoph.thiede@student.hpi.de>
2019-12-12 10:29:21 +0100

    Add a missing \ in update_schemaspy.sh

--- scripts/update_schemaspy.sh
+++ scripts/update_schemaspy.sh
@@ -2,5 +2,5 @@
 # TODO: I have no clue why this does need sudo
 cd output && sudo java -jar ../schemaspy.jar \
 	-t pgsql -dp ../postgresql.jar \
-	-host localhost -s public -db barberini -u postgres -p docker
+	-host localhost -s public -db barberini -u postgres -p docker \
 	-o schemaspy
