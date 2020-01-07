6b81616f4c7ca4c9560102e342600d7cddf75bfb
LinqLover <christoph.thiede@student.hpi.de>
2020-01-07 12:25:55 +0100

    plenktab

--- src/_utils/json_to_csv_task.py
+++ src/_utils/json_to_csv_task.py
@@ -4,7 +4,7 @@ import json, csv
 class JsonToCsvTask(luigi.Task): 
 	def run(self):
 		my_json = self.getJson()
-	
+		
 		with self.output().open('w') as csv_output:
 			csvwriter = csv.writer(csv_output)
 			count = 0
