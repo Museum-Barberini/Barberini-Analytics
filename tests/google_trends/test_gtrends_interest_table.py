b2efe2b79582a34b3aa6a2ac21ae190d8404bd8d
LinqLover <christoph.thiede@student.hpi.de>
2020-01-08 15:47:37 +0100

    Extend TestGtrendsInterestToDB

--- tests/google_trends/test_gtrends_interest_table.py
+++ tests/google_trends/test_gtrends_interest_table.py
@@ -1,6 +1,12 @@
 from google_trends.gtrends_interest_table import *
 from task_test import TaskTest
-import pandas as pd
+import panadas as pd
+import datetime as dt
+
+from database_helper import DatabaseHelper
+
+psycogp2.cursor.column_names = lambda self: [self.desc[0] for desc in self.description]
+
 
 class TestGtrendsInterestTable(TaskTest):
 	def __init__(self, methodName):
@@ -15,7 +21,28 @@ class TestGtrendsInterestTable(TaskTest):
 		self.assertEqual(2, csv.ndim)
 		self.assertCountEqual(['topicId', 'date', 'interestValue'], list(csv.columns))
 
+
 class TestGtrendsInterestToDB(TaskTest):
+	db = DatabaseHelper()
+	
+	def setUp(self):
+		super.setUp(self)
+		self.db.setUp()
+	
+	def tearDown(self):
+		super.tearDown()
+		self.db.tearDown()
+	
 	def test_topicsToDB(self):
-		GtrendsInterestToDB() # shouldnt raise
-	# TODO: Test the data layout returned by postgres here & at similar places ...
+		GtrendsInterestToDB().run()
+		
+		result = db.request("SELECT * FROM gtrends_interests WHERE topic_id LIKE 'TESTING_%'")
+		
+		self.assertListEqual(['topicId', 'date', 'interestValue'], db.cursor.column_names())
+		self.assertListEqual([
+				('TESTING_foo', dt.date(year=2001, month=10, day=12)),
+				('TESTING_bar', dt.date(year=2017, month=1, day=20))],
+			result
+		)
+	
+	# TODO: Do the same at similar places ...
