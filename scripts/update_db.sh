fede9eb47c050daed62ef6e9d153dc9a86edbf0f
Tom Wollnik <tom.wollnik@student.hpi.de>
2020-01-08 14:57:57 +0100

    update script update_db.sh

--- scripts/update_db.sh
+++ scripts/update_db.sh
@@ -3,9 +3,11 @@ LOG=/var/log/bp-logs/daily.log
 BASEDIR=$(dirname "$0")/..
 {
 echo "================================================================================================"
+PATH=/usr/local/bin:$PATH
 echo [$(date +"%Y-%m-%d %H:%M")]
 make -C $BASEDIR startup
-docker-compose -f $BASEDIR/docker-compose.yml exec luigi /bin/sh -c 'cd /app && make luigi-ui && sleep 5 && make luigi && make luigi-clean'
+docker-compose -f $BASEDIR/docker-compose.yml exec -T luigi /bin/sh -c 'cd /app && make luigi-ui && sleep 5 && make luigi && make luigi-clean'
+make -C $BASEDIR db-backup
 make -C $BASEDIR shutdown
 echo "================================================================================================"
 } >> $LOG 2>&1
