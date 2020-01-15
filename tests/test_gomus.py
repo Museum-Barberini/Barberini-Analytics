77bc44d6425a3ea19f7983b50e9ce4bb74326150
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-01-15 15:55:46 +0100

    pioneering in luigi task testing

--- tests/test_gomus.py
+++ tests/test_gomus.py
@@ -3,6 +3,10 @@ import os
 import requests
 import unittest
 
+from luigi.mock import MockTarget
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
 # No tests that data is put into DB correctly because csv_to_db is already tested
 
 class TestGomusConnection(unittest.TestCase):
@@ -17,4 +21,9 @@ class TestFetchGomusReports(unittest.TestCase):
 
 class TestGomusTransformations(unittest.TestCase):
 	# TODO: Check that exports get transformed into correct db format (using pandas)
-	pass
+	@patch('src.gomus.customers_to_db.ExtractCustomerData.requires')
+	def test_customers_transformation(self, mock):
+		mock_input = MockTarget('test_customer_data')
+		# idea: write data to MockTarget with open('w') and then call ExtractCustomerData task
+		# still need to see how to check output
+		mock.return_value = mock_input
