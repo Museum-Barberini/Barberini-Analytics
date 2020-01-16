09f46e1b3ea871452d8fd8223217efb02784440e
LinqLover <christoph.thiede@student.hpi.de>
2020-01-16 09:15:11 +0100

    WIP

--- src/google_maps/hello_google_api_client.py
+++ src/google_maps/hello_google_api_client.py
@@ -7,26 +7,44 @@ import oauth2client
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.file import Storage
 
 pp = pprint.PrettyPrinter(indent=2)
 
 # The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
 # the OAuth 2.0 information for this application, including its client_id and
 # client_secret.
-CLIENT_SECRETS_FILE = "client_secret_other.json"
+CLIENT_SECRET_FILE = "client_secret_other.json"
 
 # This access scope grants read-only access to the authenticated user's Drive
 # account.
+with open(CLIENT_SECRET_FILE) as secret_file:
+	secret = json.load(secret_file)['installed']
+	print(secret)
 SCOPES = ['https://www.googleapis.com/auth/business.manage']
 API_SERVICE_NAME = 'mybusiness'
 API_VERSION = 'v4'
 
 
 def get_authenticated_service():
-	#flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, SCOPES)
-	#credentials = flow.run_console()
-	credentials = oauth2client.client.GoogleCredentials.new_from_json(open(CLIENT_SECRETS_FILE).read())
-	oauth2client.client.save_to_well_known_file(credentials, os.path.abspath('saved_secrets.json'))
+	# Create a credential storage object.  You pick the filename.
+	storage = Storage('bankOfLon.don')
+	
+	# Attempt to load existing credentials.  Null is returned if it fails.
+	credentials = storage.get()
+	
+	# Only attempt to get new credentials if the load failed.
+	if not credentials:
+		# Run through the OAuth flow and retrieve credentials                                                                                 
+		flow = OAuth2WebServerFlow(secret['client_id'], secret['client_secret'], SCOPES, secret['redirect_uris'])
+		
+		authorize_url = flow.step1_get_authorize_url()
+		print('Go to the following link in your browser: ' + authorize_url)
+		code = raw_input('Enter verification code: ').strip()
+		
+		credentials = flow.step2_exchange(code)
+		storage.put(credentials)
 	return build(API_SERVICE_NAME, API_VERSION, credentials = credentials, 
 		discoveryServiceUrl='https://developers.google.com/my-business/samples/mybusiness_google_rest_v4p5.json')
 
