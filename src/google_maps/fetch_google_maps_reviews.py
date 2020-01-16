bb1c9e06eca4e538f1b1d00b1c7c70884d25025b
LinqLover <christoph.thiede@student.hpi.de>
2020-01-16 13:26:01 +0100

    Fix: indents, spaces, quotes

--- src/google_maps/fetch_google_maps_reviews.py
+++ src/google_maps/fetch_google_maps_reviews.py
@@ -8,93 +8,94 @@ import pprint
 
 class FetchGoogleMapsReviews(luigi.Task):
 
-    token_cache = luigi.Parameter(default='secret_files/google_gmb_credential_cache.json')
-    client_secret = luigi.Parameter(default='secret_files/google_gmb_client_secret.json')
-    scopes = ['https://www.googleapis.com/auth/business.manage']
-    google_gmb_discovery_url = 'https://developers.google.com/my-business/samples/mybusiness_google_rest_v4p5.json'
-    
-    api_service_name = 'mybusiness'
-    api_version = 'v4'
+	token_cache = luigi.Parameter(default='secret_files/google_gmb_credential_cache.json')
+	client_secret = luigi.Parameter(default='secret_files/google_gmb_client_secret.json')
+	scopes = ['https://www.googleapis.com/auth/business.manage']
+	google_gmb_discovery_url = 'https://developers.google.com/my-business/samples/mybusiness_google_rest_v4p5.json'
+	
+	api_service_name = 'mybusiness'
+	api_version = 'v4'
+	
+	stars_dict = dict({ # google returns rating as a string
+		'STAR_RATING_UNSPECIFIED': 0,
+		'ONE': 1,
+		'TWO': 2,
+		'THREE': 3,
+		'FOUR': 4,
+		'FIVE': 5
+	})
+	
+	def output(self):
+		return luigi.LocalTarget('output/google_maps/maps_reviews.csv')
+	
+	def run(self):
+		credentials = self.load_credentials()
+		service = self.load_service(credentials)
+		raw_reviews = self.fetch_raw_reviews(service)
+		reviews_df = self.extract_reviews(raw_reviews)
 
-    stars_dict = dict({ # google returns rating as a string
-        'STAR_RATING_UNSPECIFIED': 0,
-        'ONE': 1,
-        'TWO': 2,
-        'THREE': 3,
-        'FOUR': 4,
-        'FIVE': 5
-    })
-    
-    def output(self):
-        return luigi.LocalTarget('output/google_maps/maps_reviews.csv')
-    
-    def run(self):
-        credentials = self.load_credentials()
-        service = self.load_service(credentials)
-        raw_reviews = self.fetch_raw_reviews(service)
-        reviews_df = self.extract_reviews(raw_reviews)
-
-        #with self.output().open('w') as output_file:
-        #    reviews.to_csv(output_file)
-    
-    def load_credentials(self):
-        storage = Storage(self.token_cache)
-        
-        credentials = storage.get()
-        
-        if not credentials:
-        
-            with open(self.client_secret) as client_secret:
-                secret = json.load(client_secret)['installed']
-            flow = OAuth2WebServerFlow(secret['client_id'], secret['client_secret'], self.scopes, secret['redirect_uris'][0])
-            
-            authorize_url = flow.step1_get_authorize_url()
-            print('Go to the following link in your browser: ' + authorize_url)
-            code = input('Enter verification code: ').strip()
-            
-            credentials = flow.step2_exchange(code)
-            storage.put(credentials)
-        return credentials
-    
-    def load_service(self, credentials):
-        return build(self.api_service_name, self.api_version, credentials = credentials, 
-		discoveryServiceUrl=self.google_gmb_discovery_url)
-    
-    def fetch_raw_reviews(self, service, page_size=100):
-        # get account identifier 
-        account_list = service.accounts().list().execute()
-        account = account_list['accounts'][0]['name'] # in almost all cases one only has access to one account
-        
-        # get location identifier of the first location available to this account
-        # it seems like this identifier is unique per user
-        location_list = service.accounts().locations().list(parent=account).execute()
-        if len(location_list['locations']) == 0:
-            print("ERROR: This user seems to not have access to any google location, unable to fetch reviews")
-            exit(1)
-        location = location_list['locations'][0]['name']
-        
-        # get reviews for that location
-        reviews = []
-        review_list = service.accounts().locations().reviews().list(
-            parent=location,
-            pageSize=page_size).execute()
-        reviews = reviews + review_list['reviews']
-
-        while ('nextPageToken' in review_list):
-            next_page_token = review_list['nextPageToken']
-            review_list = service.accounts().locations().reviews().list(
-                parent=location,
-                pageSize=page_size,
-                pageToken=next_page_token).execute()
-            reviews = reviews + review_list['reviews']
-        return reviews
-    
-    def extract_reviews(self, raw_reviews):
-        extracted_reviews = []
-        for (raw in raw_reviews):
-            new = dict()
-            new['id'] = raw['reviewId']
-            new['rating'] = self.stars_dict(raw['starRating'])
-            new['content'] = getattr(raw, 'comment', '') # not all reviews have a comment
-            new['date'] = raw['createTime']
-
+		#with self.output().open('w') as output_file:
+		#    reviews.to_csv(output_file)
+	
+	def load_credentials(self):
+		storage = Storage(self.token_cache)
+		credentials = storage.get()
+		if not credentials:
+			with open(self.client_secret) as client_secret:
+				secret = json.load(client_secret)['installed']
+			flow = OAuth2WebServerFlow(
+				secret['client_id'],
+				secret['client_secret'],
+				self.scopes,
+				secret['redirect_uris'][0])
+			
+			authorize_url = flow.step1_get_authorize_url()
+			print('Go to the following link in your browser: ' + authorize_url)
+			code = input('Enter verification code: ').strip()
+			
+			credentials = flow.step2_exchange(code)
+			storage.put(credentials)
+		return credentials
+	
+	def load_service(self, credentials):
+		return build(self.api_service_name, self.api_version,
+			credentials=credentials,
+			discoveryServiceUrl=self.google_gmb_discovery_url)
+	
+	def fetch_raw_reviews(self, service, page_size=100):
+		# get account identifier 
+		account_list = service.accounts().list().execute()
+		account = account_list['accounts'][0]['name'] # in almost all cases one only has access to one account
+		
+		# get location identifier of the first location available to this account
+		# it seems like this identifier is unique per user
+		location_list = service.accounts().locations().list(parent=account).execute()
+		if len(location_list['locations']) == 0:
+			print("ERROR: This user seems to not have access to any google location, unable to fetch reviews")
+			exit(1)
+		location = location_list['locations'][0]['name']
+		
+		# get reviews for that location
+		reviews = []
+		review_list = service.accounts().locations().reviews().list(
+			parent=location,
+			pageSize=page_size).execute()
+		reviews = reviews + review_list['reviews']
+		
+		while ('nextPageToken' in review_list):
+			next_page_token = review_list['nextPageToken']
+			review_list = service.accounts().locations().reviews().list(
+				parent=location,
+				pageSize=page_size,
+				pageToken=next_page_token).execute()
+			reviews = reviews + review_list['reviews']
+		return reviews
+	
+	def extract_reviews(self, raw_reviews):
+		extracted_reviews = []
+		for raw in raw_reviews:
+			new = dict()
+			new['id'] = raw['reviewId']
+			new['rating'] = self.stars_dict(raw['starRating'])
+			new['content'] = getattr(raw, 'comment', '') # not all reviews have a comment
+			new['date'] = raw['createTime']
