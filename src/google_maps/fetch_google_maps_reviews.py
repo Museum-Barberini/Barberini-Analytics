ec921f3994c38d4db1295c8b466ce504eaec53aa
georg.tennigkeit <georg.tennigkeit@student.hpi.de>
2020-01-16 14:42:47 +0100

    google maps reviews are properly fetched and written into csv!

--- src/google_maps/fetch_google_maps_reviews.py
+++ src/google_maps/fetch_google_maps_reviews.py
@@ -1,9 +1,9 @@
 import luigi
 from oauth2client.file import Storage
 from oauth2client.client import OAuth2WebServerFlow
-from googleapiclient.discovery import build
+import googleapiclient.discovery
 import json
-import pprint
+import pandas as pd
 
 
 class FetchGoogleMapsReviews(luigi.Task):
@@ -15,7 +15,7 @@ class FetchGoogleMapsReviews(luigi.Task):
 	
 	api_service_name = 'mybusiness'
 	api_version = 'v4'
-	
+
 	stars_dict = dict({ # google returns rating as a string
 		'STAR_RATING_UNSPECIFIED': 0,
 		'ONE': 1,
@@ -26,28 +26,32 @@ class FetchGoogleMapsReviews(luigi.Task):
 	})
 	
 	def output(self):
-		return luigi.LocalTarget('output/google_maps/maps_reviews.csv')
+		return luigi.LocalTarget('output/google_maps/maps_reviews.csv', format=luigi.format.UTF8)
 	
 	def run(self):
+		print("loading credentials...")
 		credentials = self.load_credentials()
+		print("creating service...")
 		service = self.load_service(credentials)
+		print("fetching reviews...")
 		raw_reviews = self.fetch_raw_reviews(service)
+		print("extracting reviews...")
 		reviews_df = self.extract_reviews(raw_reviews)
-
-		#with self.output().open('w') as output_file:
-		#    reviews.to_csv(output_file)
+		print("success! writing...")
+		
+		with self.output().open('w') as output_file:
+			reviews_df.to_csv(output_file, index=False)
 	
 	def load_credentials(self):
 		storage = Storage(self.token_cache)
+		
 		credentials = storage.get()
+		
 		if not credentials:
+			
 			with open(self.client_secret) as client_secret:
 				secret = json.load(client_secret)['installed']
-			flow = OAuth2WebServerFlow(
-				secret['client_id'],
-				secret['client_secret'],
-				self.scopes,
-				secret['redirect_uris'][0])
+			flow = OAuth2WebServerFlow(secret['client_id'], secret['client_secret'], self.scopes, secret['redirect_uris'][0])
 			
 			authorize_url = flow.step1_get_authorize_url()
 			print('Go to the following link in your browser: ' + authorize_url)
@@ -58,8 +62,8 @@ class FetchGoogleMapsReviews(luigi.Task):
 		return credentials
 	
 	def load_service(self, credentials):
-		return build(self.api_service_name, self.api_version,
-			credentials=credentials,
+		return googleapiclient.discovery.build(self.api_service_name, self.api_version,
+			credentials=credentials, 
 			discoveryServiceUrl=self.google_gmb_discovery_url)
 	
 	def fetch_raw_reviews(self, service, page_size=100):
@@ -81,14 +85,17 @@ class FetchGoogleMapsReviews(luigi.Task):
 			parent=location,
 			pageSize=page_size).execute()
 		reviews = reviews + review_list['reviews']
-		
-		while ('nextPageToken' in review_list):
+		total_reviews = review_list['totalReviewCount']
+		print(f"Fetched {len(reviews)} out of {total_reviews} reviews", end='', flush=True) # creates the most beautiful loading line in the project!
+		while 'nextPageToken' in review_list:
 			next_page_token = review_list['nextPageToken']
 			review_list = service.accounts().locations().reviews().list(
 				parent=location,
 				pageSize=page_size,
 				pageToken=next_page_token).execute()
 			reviews = reviews + review_list['reviews']
+			print(f"\rFetched {len(reviews)} out of {total_reviews} reviews", end='', flush=True)
+		print()
 		return reviews
 	
 	def extract_reviews(self, raw_reviews):
@@ -96,6 +103,14 @@ class FetchGoogleMapsReviews(luigi.Task):
 		for raw in raw_reviews:
 			new = dict()
 			new['id'] = raw['reviewId']
-			new['rating'] = self.stars_dict(raw['starRating'])
-			new['content'] = getattr(raw, 'comment', '') # not all reviews have a comment
+			new['rating'] = self.stars_dict[raw['starRating']]
+			
+			raw_comment = raw.get('comment', None)
+			if (raw_comment):
+				raw_comment.replace("\n\n(Original)\n", "\n\n(Translated by Google)\n") # making google consistent with itself
+				comment_pieces = raw_comment.split("\n\n(Translated by Google)\n")
+				new['content'] = comment_pieces[0].strip()
+				new['content_original'] = comment_pieces[-1].strip()
 			new['date'] = raw['createTime']
+			extracted_reviews.append(new)
+		return pd.DataFrame(extracted_reviews)
