f3a4d75ab5b3903c1fc168b9ddb980170b6d06b1
Leon Schmidt <leon.hand09@gmail.com>
2020-01-20 00:33:27 +0100

    Refactored cron scripts

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-USER=daily-run
-LOG=/var/log/bp-logs/daily.log
+USER=$1-run
+LOG=/var/log/bp-logs/$1.log
 BASEDIR=$(dirname "$0")/..
 PATH=/usr/local/bin:$PATH
 
 {
 echo "================================================================================================"
-echo [$(date +"%Y-%m-%d %H:%M")]
+echo "Starting $1 run at [$(date +"%Y-%m-%d %H:%M")]"
 make -C $BASEDIR startup USER=$USER
-docker-compose -p $USER -f $BASEDIR/docker-compose.yml exec -T luigi /bin/bash -c 'cd /app \
-    && make luigi-ui \
-    && sleep 5 \
-    && make luigi-task LMODULE=fill_db LTASK=FillDBDaily \
-    && make luigi-clean'
-make -C $BASEDIR db-backup
+docker-compose -p $USER -f $BASEDIR/docker-compose.yml exec -T luigi /app/scripts/fill_db.sh $1
 make -C $BASEDIR shutdown USER=$USER
+if [ $1 == "daily" ]
+    then make -C $BASEDIR db-backup
+fi
+echo "Ending $1 run at [$(date +"%Y-%m-%d %H:%M")]"
 echo "================================================================================================"
 } >> $LOG 2>&1
+
