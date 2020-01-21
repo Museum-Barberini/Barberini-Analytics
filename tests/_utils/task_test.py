723e96aeb9ed92eb5cddde817029f797daf32890
LinqLover <christoph.thiede@student.hpi.de>
2020-01-21 16:34:52 +0100

    Lots of retabs and unfortunately I can't see what has changed else. But I fixed some tests by mocking barberini-facts.json locally to test_gtrends_interest_json, hopefully, correctly.

--- tests/_utils/task_test.py
+++ tests/_utils/task_test.py
@@ -12,52 +12,52 @@ To be able to run tests that use this helper, you will need
 """
 # ------ CREATE DATABASE IF NECESSARY -------
 conn = psycopg2.connect(
-        host=os.environ['POSTGRES_HOST'],
-        user=os.environ['POSTGRES_USER'],
-        password=os.environ['POSTGRES_PASSWORD'],
-        database=os.environ['POSTGRES_DB'])
+		host=os.environ['POSTGRES_HOST'],
+		user=os.environ['POSTGRES_USER'],
+		password=os.environ['POSTGRES_PASSWORD'],
+		database=os.environ['POSTGRES_DB'])
 try:
-        conn.autocommit = True
-        cur = conn.cursor()
-        try:
-                cur.execute("CREATE DATABASE barberini_test;")
-        except:
-                pass
-        finally:
-                cur.close()
+		conn.autocommit = True
+		cur = conn.cursor()
+		try:
+				cur.execute("CREATE DATABASE barberini_test;")
+		except:
+				pass
+		finally:
+				cur.close()
 finally:
-        conn.close()
+		conn.close()
 
 class DatabaseHelper:
-        def setUp(self):
-                self.connection = psycopg2.connect(
-                        host=os.environ['POSTGRES_HOST'],
-                        dbname="barberini_test",
-            user=os.environ['POSTGRES_USER'],
-                        password=os.environ['POSTGRES_PASSWORD'])
-        
-        def tearDown(self):
-                self.connection.close()
-        
-        def request(self, query):
-                self.cursor = self.connection.cursor()
-                self.cursor.execute(query)
-                self.result = cursor.fetchall()
-                self.cursor.close()
-                return result
-        
-        def commit(self, *queries):
-                self.cursor = self.connection.cursor()
-                for query in queries:
-                        cursor.execute(query)
-                self.cursor.close()
-                self.connection.commit()
-        
-        def column_names(self):
-                self.cursor = self.connection.cursor()
-                result = [self.cursor.desc[0] for desc in self.cursor.description]
-                self.cursor.close()
-                return result
+		def setUp(self):
+				self.connection = psycopg2.connect(
+						host=os.environ['POSTGRES_HOST'],
+						dbname="barberini_test",
+			user=os.environ['POSTGRES_USER'],
+						password=os.environ['POSTGRES_PASSWORD'])
+		
+		def tearDown(self):
+				self.connection.close()
+		
+		def request(self, query):
+				self.cursor = self.connection.cursor()
+				self.cursor.execute(query)
+				self.result = cursor.fetchall()
+				self.cursor.close()
+				return result
+		
+		def commit(self, *queries):
+				self.cursor = self.connection.cursor()
+				for query in queries:
+						cursor.execute(query)
+				self.cursor.close()
+				self.connection.commit()
+		
+		def column_names(self):
+				self.cursor = self.connection.cursor()
+				result = [self.cursor.desc[0] for desc in self.cursor.description]
+				self.cursor.close()
+				return result
 
 
 # DOCUMENTATION: This is kind of 💩💩💩
@@ -65,23 +65,18 @@ class DatabaseHelper:
 # This should be very easy by using unittest.mock the right way, but I failed ...
 
 class DatabaseTaskTest(unittest.TestCase):
-        db = DatabaseHelper()
-        
-        def setUp(self):
-                super().setUp()
-                self.db.setUp()
-                subprocess.call('cp -r tests_fake_files/. . --backup'.split())
-                with open('data/barberini-facts.json', 'r') as fp:
-                    before = json.load(fp)
-                before['foundingDate'] = '2013-01-01T00:00:00.000Z'
-                with open('data/barberini-facts.json', 'w') as fp:
-                    json.dump(before, fp)
-        
-        def tearDown(self):
-                subprocess.call(['bash', '-c', 'find -iname *~ | awk \'{system("bash -c \'"\'"\'file="$1" bash -c \\"mv \\\\$file \\\\${file::-1}\\"\'"\'"\'")}\''])
-                super().tearDown()
-                self.db.tearDown()
-        
-        def isolate(self, task):
-                task.complete = lambda: True
-                return task
+		db = DatabaseHelper()
+		
+		def setUp(self):
+				super().setUp()
+				self.db.setUp()
+				subprocess.call('cp -r tests_fake_files/. . --backup'.split())
+		
+		def tearDown(self):
+				subprocess.call(['bash', '-c', 'find -iname *~ | awk \'{system("bash -c \'"\'"\'file="$1" bash -c \\"mv \\\\$file \\\\${file::-1}\\"\'"\'"\'")}\''])
+				super().tearDown()
+				self.db.tearDown()
+		
+		def isolate(self, task):
+				task.complete = lambda: True
+				return task
