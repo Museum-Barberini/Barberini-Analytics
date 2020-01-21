191b28217c36a9a009fb7279c348c5b6e32f01d3
LinqLover <christoph.thiede@student.hpi.de>
2020-01-21 16:01:40 +0100

    Change faked founding date
    Barberini was not trending before it was announced

--- tests/google_trends/test_gtrends_interest_json.py
+++ tests/google_trends/test_gtrends_interest_json.py
@@ -14,7 +14,7 @@ class TestGtrendsInterestJson(DatabaseTaskTest):
                         json_string = file.read()
                 self.assertTrue(json_string) # not empty
                 json_values = json.loads(json_string)
-                min = datetime(2012, 12, 1) # treshold, see faked barberini-facts.json version
+                min = datetime(2014, 12, 1) # treshold, see faked barberini-facts.json version
                 now = datetime.now()
                 for row in json_values:
                         self.assertEqual(set(['topicId', 'interestValue', 'date']), set(row.keys()))
