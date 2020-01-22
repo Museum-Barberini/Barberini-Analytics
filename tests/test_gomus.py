2d320d70f9d10cdd1605c0f00cd648f2d0a7099a
Leon Schmidt <leon.hand09@gmail.com>
2020-01-22 20:41:38 +0100

    Added booking transformation test

--- tests/test_gomus.py
+++ tests/test_gomus.py
@@ -10,6 +10,7 @@ from unittest.mock import patch
 
 from customers_to_db import ExtractCustomerData
 from orders_to_db import ExtractOrderData
+from bookings_to_db import ExtractGomusBookings
 from gomus_report import FetchGomusReport
 
 # No tests that data is put into DB correctly because csv_to_db is already tested
@@ -95,6 +96,7 @@ class TestGomusOrdersTransformations(unittest.TestCase):
 		input_mock.return_value = input_target
 		output_mock.return_value = output_target
 		cust_id_mock.return_value = 0
+		# TODO: Test query of customer id
 
 		# Write test data to input mock
 		with input_target.open('w') as input_data:
@@ -120,3 +122,35 @@ class TestGomusOrdersTransformations(unittest.TestCase):
 
 		# 10698846.0 should be out of range
 		self.assertRaises(OverflowError, ExtractOrderData(self.columns).run)
+
+class TestGomusBookingsTransformations(unittest.TestCase):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.columns = ['id', 'booker_id', 'category',
+			'participants', 'guide_id', 'date', 'daytime',
+			'duration', 'exhibition', 'title', 'status',
+			'order_date', 'language']
+	
+	@patch.object(ExtractGomusBookings, 'output')
+	@patch.object(ExtractGomusBookings, 'input')
+	def test_bookings_transformation(self, input_mock, output_mock):
+		self.columns = ['id', 'booker_id', 'category',
+			'participants', 'guide_id', 'date', 'daytime',
+			'duration','exhibition', 'title', 'status']
+		input_target = MockTarget('input_data', format=UTF8)
+		input_mock.return_value = input_target
+		output_target = MockTarget('output_data', format=UTF8)
+		output_mock.return_value = output_target
+		
+		# Set up input data
+		with input_target.open('w') as input_data:
+			with open('tests/test_data/gomus_bookings_in.csv', 'r', encoding='utf-8') as test_data_in:
+				input_data.write(test_data_in.read())
+			
+		# Run
+		ExtractGomusBookings().run()
+
+		# Check output data
+		with output_target.open('r') as output_data:
+			with open('tests/test_data/gomus_bookings_out.csv', 'r', encoding='utf-8') as test_data_out:
+				self.assertEqual(output_data.read(), test_data_out.read())
