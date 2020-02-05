abcdcddd887e3c6cad7fd46d1596670c50dac284
georg.tennigkeit <georg.tennigkeit@student.hpi.de>
2020-02-05 17:48:05 +0100

    appstore reviews also contain date

--- src/apple_appstore/fetch_apple_app_reviews.py
+++ src/apple_appstore/fetch_apple_app_reviews.py
@@ -4,113 +4,115 @@ import luigi
 from csv_to_db import CsvToDb
 from luigi.format import UTF8
 import json
-from lxml import html
 import xmltodict
 
-######### TARGETS ############
 
 class FetchAppstoreReviews(luigi.Task):
 
     def output(self):
-        return luigi.LocalTarget("output/appstore_reviews.csv", format=UTF8)
+        return luigi.LocalTarget('output/appstore_reviews.csv', format=UTF8)
     
     def run(self):
-        reviews = fetch_all()
+        reviews = self.fetch_all()
         with self.output().open('w') as output_file:
             reviews.to_csv(output_file, index=False, header=True)
 
+    def fetch_all(self):
+        data = []
+        country_codes = self.get_country_codes()
+        for country_code in country_codes:
+            
+            try:
+                data += [self.fetch_for_country(country_code)]
+            except ValueError:
+                pass  # no data for given country code
+            except requests.HTTPError as error:
+                if error.response.status_code == 400: # not all countries are available
+                    pass
+                else:
+                    raise
+        ret = pd.concat(data)
+        return ret.drop_duplicates(subset=['id'])
+
+    def get_country_codes(self):
+        return requests.get('http://country.io/names.json').json().keys()
+
+    def fetch_for_country(self, country_code):
+        with open('data/barberini-facts.json') as facts_json:
+            barberini_facts = json.load(facts_json)
+            app_id = barberini_facts['ids']['apple']['appId']
+        url = f'https://itunes.apple.com/{country_code}/rss/customerreviews/page=1/id={app_id}/sortby=mostrecent/xml'
+        data_list = []
+        
+        while True:
+            try:
+                data, url = self.fetch_page(url)
+            except StopIteration:
+                break
+            
+            data_list += data
+        
+        if len(data_list) == 0:
+            # no reviews for the given country code
+            raise ValueError()
+            
+        result = pd.DataFrame(data_list)
+        result['country_code'] = country_code
+        
+        return result
+
+    def fetch_page(self, url):
+        
+        response = requests.get(url)
+        response.raise_for_status()
+        response_content = xmltodict.parse(response.text)['feed']
+        
+        if 'entry' not in response_content:
+            raise StopIteration()
+            
+        entries = response_content['entry']
+
+        if isinstance(entries, dict):
+            entries = [entries]
+        data = [{
+            'id': item['id'], 
+            'content': self.find_first_conditional_tag(item['content'], lambda each: each['@type'] == 'text')['#text'],
+            'rating': item['im:rating'],
+            'app_version': item['im:version'],
+            'vote_count': item['im:voteCount'],
+            'vote_sum': item['im:voteSum'],
+            'title': item['title'],
+            'date': item['updated']
+        } for item in entries]
+        
+        next_page_url = self.find_first_conditional_tag(response_content['link'], lambda each: each['@rel'] == 'next')['@href']
+
+        return data, next_page_url
+
+    # for when there are multiple 'contents'-elements in our response
+    def find_first_conditional_tag(self, tags, condition):
+        return next(each for each in tags if condition(each))
+
 class AppstoreReviewsToDB(CsvToDb):
     
-    table = "appstore_review"
+    table = 'appstore_review'
     
     columns = [
-        ("id", "TEXT"),
-        ("content", "TEXT"),
-        ("rating", "INT"),
-        ("app_version", "TEXT"),
-        ("vote_count", "INT"),
-        ("vote_sum", "INT"),
-        ("title", "TEXT"),
-        ("date", "DATE"),
-        ("country_code", "TEXT")
+        ('id', 'TEXT'),
+        ('content', 'TEXT'),
+        ('rating', 'INT'),
+        ('app_version', 'TEXT'),
+        ('vote_count', 'INT'),
+        ('vote_sum', 'INT'),
+        ('title', 'TEXT'),
+        ('date', 'TIMESTAMP'),
+        ('country_code', 'TEXT')
     ]
     
-    primary_key = "id"
+    primary_key = 'id'
     
     def requires(self):
         return FetchAppstoreReviews()
 
 
 
-######### FETCH LOGIC ###########
-
-def fetch_all():
-    data = []
-    country_codes = get_country_codes()
-    for country_code in country_codes:
-        try:
-            data = [*data, fetch_country(country_code)]
-        except ValueError:
-            pass  # no data for given country code
-        except requests.HTTPError:
-            pass
-    ret = pd.concat(data)
-    return ret.drop_duplicates()
-
-def get_country_codes():
-    return requests.get("http://country.io/names.json").json().keys()
-
-def fetch_country(country_code):
-    with open('data/barberini-facts.json') as facts_json:
-        barberini_facts = json.load(facts_json)
-        app_id = barberini_facts['ids']['apple']['appId']
-    url = f"https://itunes.apple.com/{country_code}/rss/customerreviews/page=1/id={app_id}/sortby=mostrecent/xml"
-    data_list = []
-    
-    while True:
-        try:
-            data, response = fetch_single_url(url)
-        except StopIteration:
-            break
-        
-        data_list = [*data_list, *data]
-        next_page = filter(lambda x: x["attributes"]["rel"] == "next", response["link"])
-        url = next(next_page)["attributes"]["href"]
-    
-    if len(data_list) == 0:
-        # no reviews for the given country code
-        raise ValueError()
-        
-    result = pd.DataFrame(data_list)
-    result["country_code"] = country_code
-    
-    return result
-
-def fetch_single_url(url):
-    
-    response = requests.get(url)
-    
-    if response.ok is False:
-        raise requests.HTTPError
-    
-    response_content = xmltodict.parse(response.text)["feed"]
-    
-    if "entry" not in response_content:
-        raise StopIteration()
-        
-    entries = response_content["entry"]
-    import pdb;pdb.set_trace()
-    if isinstance(entries, dict):
-        entries = [entries]
-    data = [{
-        "id": item["id"], 
-        "content": item["content"], 
-        "rating": item["im:rating"],
-        "app_version": item["im:version"],
-        "vote_count": item["im:voteCount"],
-        "vote_sum": item["im:voteSum"],
-        "title": item["title"],
-        "date": item["updated"]
-    } for item in entries]
-    
-    return data, response_content
--- tests/apple_appstore/test_fetch_apple_app_reviews.py
+++ tests/apple_appstore/test_fetch_apple_app_reviews.py
@@ -10,26 +10,34 @@ class MockRequest:
     def __init__(self):
             self.status_code = 450
 
+fake_country_codes = ['DE', 'US', 'PL', 'BB']
+
 
 class TestFetchAppleReviews(unittest.TestCase):
     
+    def setUp(self):
+        super().setUp()
+        self.task = FetchAppstoreReviews()
+        self.task.get_country_codes = lambda: fake_country_codes
+
     def test_germany_basic(self):
-        result = fetch_country("DE")
+        result = self.task.fetch_for_country('DE')
         self.assertIsInstance(result, pd.DataFrame)
     
-    @patch("src.apple_appstore.fetch_apple_app_reviews.requests.get")
+    @patch('src.apple_appstore.fetch_apple_app_reviews.requests.get')
     def test_get_request_returns_bad_status_code(self, mock):
         mock.return_value = MagicMock(ok = False)
-        self.assertRaises(requests.HTTPError, fetch_country, "de")
+        self.assertRaises(requests.HTTPError, self.task.fetch_for_country, 'de')
     
-    @patch("src.apple_appstore.fetch_apple_app_reviews.requests.get")
+    @patch('src.apple_appstore.fetch_apple_app_reviews.requests.get')
     def test_only_one_review_fetched(self, mock):
         
-        first_return = """<?xml version="1.0" encoding="utf-8"?>
+        first_return = '''<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns:im="http://itunes.apple.com/rss" xmlns="http://www.w3.org/2005/Atom" xml:lang="de">
 	<id>https://itunes.apple.com/de/rss/customerreviews/id=1150432552/mostrecent/xml</id>
     <title>iTunes Store: Rezensionen</title>
     <updated>2020-02-04T02:59:47-07:00</updated>
+    <link rel="not-next" href="this is some other link in the response; we don't ever want to access this"/>
     <link rel="next" href="https://itunes.apple.com/de/rss/customerreviews/page=2/id=1150432552/sortby=mostrecent/xml?urlDesc=/customerreviews/id=1150432552/mostrecent/xml"/>
     
     <entry>
@@ -45,116 +53,121 @@ class TestFetchAppleReviews(unittest.TestCase):
         <content type="html">&lt;somethtml note=&quot;We don't want to parse this&quot;&gt;&lt;anIrrelevantTag /&gt;&lt;/somehtml&gt;</content>
     </entry>
 
-</feed>"""
+</feed>'''
         
-        second_return = ""
+        second_return = '''<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns:im="http://itunes.apple.com/rss" xmlns="http://www.w3.org/2005/Atom" xml:lang="de">
+	<id>https://itunes.apple.com/de/rss/customerreviews/id=1150432552/mostrecent/xml</id>
+    <title>iTunes Store: Rezensionen</title>
+    <updated>2020-02-04T02:59:47-07:00</updated>
+    <link rel="not-next" href="this is some other link in the response; we don't ever want to access this"/>
+    <link rel="next" href="https://itunes.apple.com/de/rss/customerreviews/page=2/id=1150432552/sortby=mostrecent/xml?urlDesc=/customerreviews/id=1150432552/mostrecent/xml"/>
+    </feed>'''
         
         mock.side_effect = [
-                MagicMock(ok = True, json = lambda: first_return),
-                MagicMock(ok = True, json = lambda: second_return)
+            MagicMock(ok = True, text = first_return),
+            MagicMock(ok = True, text = second_return)
         ]
         
-        result = fetch_country("made_up_country")
+        result = self.task.fetch_for_country('made_up_country')
         
         self.assertIsInstance(result, pd.DataFrame)
         self.assertEqual(len(result), 1)
         self.assertListEqual(
-		["id","content","rating",
-		 "app_version","vote_count","vote_sum","title", "date", "country_code"],
+		['id','content','rating',
+		 'app_version','vote_count','vote_sum','title', 'date', 'country_code'],
 		list(result.columns)
 	)
     
-    @patch("src.apple_appstore.fetch_apple_app_reviews.fetch_country")
+    @patch.object(FetchAppstoreReviews, 'fetch_for_country')
     def test_all_countries(self, mock):
         
         def mock_return():
-                for i in range(5000):
-                        yield pd.DataFrame({"countryId": [f"{i}"], "id": [i]})
+            for i in range(5000):
+                yield pd.DataFrame({'countryId': [f'{i}'], 'id': [i]})
         mock.side_effect = mock_return()
         
-        result = fetch_all()
+        result = self.task.fetch_all()
         
         self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(len(result), 250)
+        self.assertEqual(len(result), len(fake_country_codes))
         
         # get a list with all args passed to the mock (hopefully all country ids)
         args = [args[0] for (args, _) in mock.call_args_list]
         for arg in args:
-                self.assertRegex(arg, r"^\w{2}$")
+            self.assertRegex(arg, r'^\w{2}$')
     
-    @patch("src.apple_appstore.fetch_apple_app_reviews.fetch_country")
+    @patch.object(FetchAppstoreReviews, 'fetch_for_country')
     def test_all_countries_some_countries_dont_have_data(self, mock):
-         
+        
         def mock_return(country_code):
-                if country_code == "BB":
-                        raise ValueError()
-                return pd.DataFrame({"countryId": [country_code], "id": [country_code]})
+            if country_code == 'BB':
+                raise ValueError()
+            return pd.DataFrame({'countryId': [country_code], 'id': [country_code]})
         mock.side_effect = mock_return
         
-        result = fetch_all()
+        result = self.task.fetch_all()
         
         self.assertIsInstance(result, pd.DataFrame)
-        self.assertEqual(len(result), 249)
+        self.assertEqual(len(result), len(fake_country_codes) - 1)
     
-    @patch("src.apple_appstore.fetch_apple_app_reviews.fetch_country")
-    def test_drop_duplicate_tweets(self, mock):
+    @patch.object(FetchAppstoreReviews, 'fetch_for_country')
+    def test_drop_duplicate_reviews(self, mock):
         
         def mock_return(country_code):
-            if country_code == "BB":
+            if country_code == 'BB': # simulate no available data
                 raise ValueError()
-            return pd.DataFrame({"id": ["xyz"], "countryId": [country_code]})
+            return pd.DataFrame({'id': ['xyz'], 'countryId': [country_code]})
         mock.side_effect = mock_return
         
-        result = fetch_all()
+        result = self.task.fetch_all()
         
         self.assertEqual(len(result), 1)
     
-    @patch("src.apple_appstore.fetch_apple_app_reviews.get_country_codes")
-    @patch("src.apple_appstore.fetch_apple_app_reviews.fetch_country")
-    def test_same_review_for_multiple_country_codes(self, mock_fetch_country, mock_get_country_codes):
+    @patch.object(FetchAppstoreReviews, 'fetch_for_country')
+    def test_same_review_for_multiple_country_codes(self, mock_fetch_for_country):
         
-        mock_fetch_country_return = [
+        mock_fetch_for_country_return = [
             pd.DataFrame({
-                "id": ["1", "2"],
-                "content": ["C_1", "C_2"],
-                "content_type": ["CT_1", "CT_2"],
-                "rating": ["R_1", "R_2"],
-                "app_version": ["AV_1", "AV_2"],
-                "vote_count": [1, 2],
-                "vote_sum": [1, 2],
-                "title": ["T_2", "T_2"],
-                "countryId": ["AB", "AB"]
+                'id': ['1', '2'],
+                'content': ['C_1', 'C_2'],
+                'content_type': ['CT_1', 'CT_2'],
+                'rating': ['R_1', 'R_2'],
+                'app_version': ['AV_1', 'AV_2'],
+                'vote_count': [1, 2],
+                'vote_sum': [1, 2],
+                'title': ['T_2', 'T_2'],
+                'country_code': ['AB', 'AB']
             }),
             pd.DataFrame({
-                "id": ["1"],
-                "content": ["C_1"],
-                "content_type": ["CT_1"],
-                "rating": ["R_1"],
-                "app_version": ["AV_1"],
-                "vote_count": [1],
-                "vote_sum": [1],
-                "title": ["T_2" ],
-                "countryId": ["CD"]
+                'id': ['1'],
+                'content': ['C_1'],
+                'content_type': ['CT_1'],
+                'rating': ['R_1'],
+                'app_version': ['AV_1'],
+                'vote_count': [1],
+                'vote_sum': [1],
+                'title': ['T_2' ],
+                'country_code': ['CD']
             })
         ]
-        mock_fetch_country.side_effect = mock_fetch_country_return
-        mock_get_country_codes.return_value = ["AB", "CD"]
+        mock_fetch_for_country.side_effect = mock_fetch_for_country_return
+        self.task.get_country_codes = lambda: ['AB', 'CD']
         
-        result = fetch_all()
-        
+        result = self.task.fetch_all()
         self.assertEqual(len(result), 2)
-        pd.util.testing.assert_frame_equal(
+        pd.testing.assert_frame_equal(
             pd.DataFrame({
-                "id": ["1", "2"],
-                "content": ["C_1", "C_2"],
-                "content_type": ["CT_1", "CT_2"],
-                "rating": ["R_1", "R_2"],
-                "app_version": ["AV_1", "AV_2"],
-                "vote_count": [1, 2],
-                "vote_sum": [1, 2],
-                "title": ["T_2", "T_2"]
+                'id': ['1', '2'],
+                'content': ['C_1', 'C_2'],
+                'content_type': ['CT_1', 'CT_2'],
+                'rating': ['R_1', 'R_2'],
+                'app_version': ['AV_1', 'AV_2'],
+                'vote_count': [1, 2],
+                'vote_sum': [1, 2],
+                'title': ['T_2', 'T_2']
             }),
-            result
+            result.drop(columns=['country_code'])
         )
 
 
