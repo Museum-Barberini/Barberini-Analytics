6376ddc7565ebcfc46874774bccb32476178b26b
LinqLover <christoph.thiede@student.hpi.de>
2020-02-11 13:55:45 +0100

    Union test_gtrends_interest_{json,table}.py into test_gtrends_values.py

--- tests/google_trends/test_gtrends_values.py
+++ tests/google_trends/test_gtrends_values.py
@@ -1,8 +1,10 @@
+from unittest.mock import patch
 from google_trends.gtrends_values import *
 from task_test import DatabaseTaskTest
-from unittest.mock import patch
+import datetime as dt
 import json
-from datetime import datetime
+import pandas as pd
+
 
 class TestFetchGtrendsValues(DatabaseTaskTest):
     def __init__(self, methodName):
@@ -10,7 +12,7 @@ class TestFetchGtrendsValues(DatabaseTaskTest):
         self.task = self.isolate(FetchGtrendsValues())
     
     @patch("src.google_trends.gtrends_values.open")
-    def test_gtrends_interests_json(self, mock_open):
+    def test_gtrends_values(self, mock_open):
         with open('data/barberini_facts.jsonc') as facts_file:
             facts = json.load(facts_file)
         facts['foundingDate'] = '2015-01-01T00:00:00.000Z'
@@ -25,19 +27,49 @@ class TestFetchGtrendsValues(DatabaseTaskTest):
             json_string = file.read()
         self.assertTrue(json_string) # not empty
         json_values = json.loads(json_string)
-        min = datetime(2014, 12, 1) # treshold, see faked barberini_facts.jsonc version
-        now = datetime.now()
+        min = dt.datetime(2014, 12, 1) # treshold, see faked barberini_facts.jsonc version
+        now = dt.datetime.now()
         for row in json_values:
             self.assertEqual(set(['topicId', 'interestValue', 'date']), set(row.keys()))
         for expected_id in ['1', '2']:
             self.assertTrue(any(entry['topicId'] == expected_id for entry in json_values))
         for entry in json_values:
             date = entry['date']
-            date = datetime.strptime(date, '%Y-%m-%d')
-            self.assertTrue(date.time() == datetime.min.time())
+            date = dt.datetime.strptime(date, '%Y-%m-%d')
+            self.assertTrue(date.time() == dt.datetime.min.time())
             self.assertTrue(min <= date <= now)
         for entry in json_values:
             value = entry['interestValue']
             self.assertTrue(isinstance(value, int))
             self.assertTrue(0 <= value <= 100)
             self.assertTrue(0 < value, "Barberini is cool! It must be trending.")
+
+
+class TestConvertGtrendsValues(DatabaseTaskTest):
+    
+    def __init__(self, methodName):
+        super().__init__(methodName)
+        self.task = self.isolate(ConvertGtrendsValues())
+    
+    def test_convert_gtrends_values(self):
+        self.task = ConvertGtrendsValues() # todo: (why) do we need this
+        self.task.run()
+        csv = pd.read_csv("output/google_trends/interest_values.csv")
+        self.assertFalse(csv.empty)
+        self.assertEqual(2, csv.ndim)
+        self.assertCountEqual(['topicId', 'date', 'interestValue'], list(csv.columns))
+
+
+class TestGtrendsValuesToDB(DatabaseTaskTest):
+    
+    def test_values_to_db(self):
+        GtrendsValuesToDB().run()
+        
+        result = self.db.request("SELECT * FROM gtrends_values WHERE topic_id LIKE 'TESTING_%'")
+        
+        self.assertListEqual(['topic_id', 'date', 'interest_value'], self.db.column_names)
+        self.assertListEqual([
+                ('TESTING_foo', dt.date(year=2001, month=10, day=12), 9),
+                ('TESTING_bar', dt.date(year=2017, month=1, day=20), 42)],
+            result
+        )
