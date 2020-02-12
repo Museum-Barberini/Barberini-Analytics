e3d8ea5586965000a48808deff89a20daa9e510d
Leon Schmidt <leon.schmidt@student.hpi.uni-potsdam.de>
2020-02-12 22:37:10 +0100

    Redid public tours to events, added test for gomus version and added report editing automatisation (almost finished)

---
+++ tests/gomus/test_gomus_version.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+import requests
+import unittest
+
+GOMUS_SESS_ID = os.environ['GOMUS_SESS_ID']
+EXPECTED_VERSION_TAG = 'v4.1.1.4 – Premium Edition'
+
+class TestGomusVersion(unittest.TestCase):
+    def test_gomus_version(self):
+        if GOMUS_SESS_ID == '':
+            print("Please make sure a valid Gomus session ID is provided")
+            exit(1)
+
+        response = requests.get('https://barberini.gomus.de/', cookies=dict(_session_id=GOMUS_SESS_ID))
+        if not response.ok:
+            response.raise_for_status()
+    
+        # currently, the version tag is in this particular line in the HTML
+        # if this line no. changes, that also means that adjustments to Gomus have been made
+        version_tag = response.text.splitlines()[762]
+        
+        self.assertEqual(version_tag, EXPECTED_VERSION_TAG)
