56fe305945c60d1f3b4c60c434dec3dc59d09b8a
LinqLover <christoph.thiede@student.hpi.de>
2020-02-13 11:58:27 +0100

    Migrate GtrendsTopics to use BarberiniFacts and redesign gtrends-specific facts

--- src/google_trends/gtrends_topics.py
+++ src/google_trends/gtrends_topics.py
@@ -1,11 +1,13 @@
 import luigi
 import json
 from json_to_csv_task import JsonToCsvTask
+from barberini_facts import BarberiniFacts
 
 
 class GtrendsTopics(luigi.Task):
     
-    input_file = luigi.Parameter(default='data/barberini_facts.jsonc')
+    def requires(self):
+        return BarberiniFacts()
     
     def output(self):
         return luigi.LocalTarget('output/google_trends/topics.json')
@@ -13,15 +15,18 @@ class GtrendsTopics(luigi.Task):
     def run(self):
         topics = self.collect_topics()
         with self.output().open('w') as output_file:
-            output_file.write(json.dumps(dict(enumerate(topics))))
+            json.dump(topics, output_file)
     
     def collect_topics(self):
-        with open(self.input_file) as json_top_10_facts_about_barberini_you_didnt_know_number_8_will_shock_you:
-            barberini_facts = \
-                json.load(json_top_10_facts_about_barberini_you_didnt_know_number_8_will_shock_you)
-            barberini_topic = barberini_facts['ids']['google']['keyword']
-            exhibitions_topics = ['barberini ' + exhibition for exhibition in barberini_facts['exhibitions']]
-            
-            return [barberini_topic] + exhibitions_topics
-
-
+        with self.input().open('r') as facts_file:
+            facts = json.load(facts_file)
+        
+        museum_topic = facts['ids']['google']['knowledgeId']
+        gtrends_facts = facts['gtrends']
+        museum_names = gtrends_facts['museumNames']
+        extra_topics = gtrends_facts['topics']
+        complex_topics = [' '.join([museum_name, extra_topic])
+            for extra_topic in extra_topics
+            for museum_name in museum_names]
+        
+        return [museum_topic] + complex_topics
