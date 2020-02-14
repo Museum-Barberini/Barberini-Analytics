c8b8b9c2c6adeffc9d076dd9dcd3001c8265888e
LinqLover <christoph.thiede@student.hpi.de>
2020-02-14 19:36:46 +0100

    Add chmod +x

--- scripts/install_toolchain.sh
+++ scripts/install_toolchain.sh
@@ -18,5 +18,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 sudo -H pip3 install docker-compose
