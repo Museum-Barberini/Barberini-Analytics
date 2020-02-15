c6dd7dcd9481055a79c11d79ca16d714317e8d04
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-02-15 14:58:04 +0100

    Apply suggestion to scripts/install_toolchain.sh

--- scripts/install_toolchain.sh
+++ scripts/install_toolchain.sh
@@ -17,7 +17,7 @@ apt-get install -y \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get install -y docker-ce docker-ce-cli containerd.io
-curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 sudo -H pip3 install docker-compose
