dcd9adeb0ac7e77311218a31bd6c6b26a25c7ffb
Leon Schmidt <leon.hand09@gmail.com>
2020-02-27 15:59:28 +0100

    Fixing more bugs

--- src/google_trends/gtrends_topics_json.py
+++ src/google_trends/gtrends_topics_json.py
@@ -18,8 +18,8 @@ class GTrendsTopicsJson(luigi.Task):
             output_file.write(json.dumps(dict(enumerate(topics))))
 
     def collect_topics(self):
-        with open(self.input_file, encoding='utf-8') as json:
-            barberini_facts = json.load(json)
+        with open(self.input_file, encoding='utf-8') as json_file:
+            barberini_facts = json.load(json_file)
             barberini_topic = barberini_facts['ids']['google']['keyword']
             exhibitions_topics = [
                 'barberini ' +
