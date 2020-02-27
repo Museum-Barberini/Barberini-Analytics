6cd0517bedd009d0a12d269473f716a9cc5ace0f
LinqLover <christoph.thiede@student.hpi.de>
2020-02-27 14:51:51 +0100

    WIP fix?

--- src/gomus/events.py
+++ src/gomus/events.py
@@ -97,8 +97,13 @@ class EnsureBookingsIsRun(luigi.Task):
         self.output_list = []
         self.is_complete = False
         self.row_list = []
-
+    
     def run(self):
+        # Enforce the generator to evaluate completely so the task is marked as complete before returning new dependencies
+        # LATEST TODO: Re-run pipeline and see whether this works.
+        return [dep for dep in list(self._run())]
+    
+    def _run(self):
         try:
             conn = psycopg2.connect(
                 host=self.host, database=self.database,
@@ -118,13 +123,13 @@ class EnsureBookingsIsRun(luigi.Task):
                     self.output_list.append(cancelled)
                     self.row_list.append(event_id)
                 row = cur.fetchone()
-                        
+            
             self.is_complete = True
         
         finally:
             if conn is not None:
                 conn.close()
-        
+    
     def output(self):
         return self.output_list
         
