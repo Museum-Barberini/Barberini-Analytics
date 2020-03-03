21e1cf04936d43b0c0cbab9616159f384f7ccbe4
LinqLover <christoph.thiede@student.hpi.de>
2020-03-03 10:38:46 +0100

    Calm the linter

--- tests/google_trends/test_gtrends_topics.py
+++ tests/google_trends/test_gtrends_topics.py
@@ -17,16 +17,18 @@ class TestGtrendsTopics(DatabaseTaskTest):
             'topics': ['life', 'universe', 'everything']
         }
         self.install_mock_target(facts_mock, lambda file: json.dump(facts, file))
-        
+
         self.task = GtrendsTopics()
         self.task.run()
-        
+
         with self.task.output().open('r') as output_file:
             topics_string = output_file.read()
-        self.assertTrue(topics_string) # not empty
+        self.assertTrue(topics_string)  # not empty
         topics = json.loads(topics_string)
         for item in topics:
             self.assertTrue(isinstance(item, str))
         self.assertEqual(
-            len([facts['ids']['google']['knowledgeId']]) + len(facts['gtrends']['museumNames']) * len(facts['gtrends']['topics']),
+            len([facts['ids']['google']['knowledgeId']])
+                + len(facts['gtrends']['museumNames'])
+                    * len(facts['gtrends']['topics']),
             len(topics))
--- tests/google_trends/test_gtrends_values.py
+++ tests/google_trends/test_gtrends_values.py
@@ -3,10 +3,7 @@ from queue import Queue
 
 from unittest.mock import patch
 import luigi
-from luigi.mock import MockTarget
-from luigi.format import UTF8
 import json
-import pandas as pd
 
 from task_test import DatabaseTaskTest
 from google_trends.gtrends_values import *
@@ -14,11 +11,11 @@ from museum_facts import MuseumFacts
 
 
 class TestFetchGtrendsValues(DatabaseTaskTest):
-    
+
     def __init__(self, methodName):
         super().__init__(methodName)
         self.task = self.isolate(FetchGtrendsValues())
-    
+
     @patch.object(GtrendsTopics, 'output')
     @patch.object(MuseumFacts, 'output')
     def test_gtrends_values(self, facts_mock, topics_mock):
@@ -26,25 +23,30 @@ class TestFetchGtrendsValues(DatabaseTaskTest):
         min_date = dt.datetime(2015, 1, 1)
         facts['foundingDate'] = min_date.isoformat()
         topics = ['41', '42', '43']
-        facts_target = self.install_mock_target(facts_mock, lambda file: json.dump(facts, file))
-        topics_target = self.install_mock_target(topics_mock, lambda file: json.dump(topics, file))
+        self.install_mock_target(facts_mock,
+            lambda file: json.dump(facts, file))
+        topics_target = self.install_mock_target(topics_mock,
+            lambda file: json.dump(topics, file))
         self.dump_mock_target_into_fs(topics_target)
-        
+
         try:
             self.task = FetchGtrendsValues()
             self.task.run()
         finally:
-            pass # too lazy to delete the file again
-        
+            pass  # too lazy to delete the file again
+
         with self.task.output().open('r') as output_file:
             values_string = output_file.read()
-        self.assertTrue(values_string) # not empty
+        self.assertTrue(values_string)  # not empty
         json_values = json.loads(values_string)
-        min_date -= dt.timedelta(days=7) # treshold
+        min_date -= dt.timedelta(days=7)  # treshold
         now = dt.datetime.now()
         for entry in json_values:
             self.assertCountEqual(['topic', 'date', 'interestValue'], entry.keys())
-        rows_per_topic = { topic: len([entry for entry in json_values if entry['topic'] == topic]) for topic in topics }
+        rows_per_topic = {
+            topic: len([entry for entry in json_values if entry['topic'] == topic])
+            for topic in topics
+        }
         self.assertTrue(max(rows_per_topic.values()) == min(rows_per_topic.values()),
             msg="All topics should be measured in about the same time interval")
         for entry in json_values:
@@ -57,8 +59,9 @@ class TestFetchGtrendsValues(DatabaseTaskTest):
             self.assertTrue(0 <= value <= 100)
             self.assertTrue(0 < value, "Numbers are cool! They must be trending.")
 
+
 class TestGtrendsValuesToDB(DatabaseTaskTest):
-    
+
     def setUp(self):
         super().setUp()
         # super ugly way to ensure that the table exists
@@ -67,24 +70,25 @@ class TestGtrendsValuesToDB(DatabaseTaskTest):
         task = GtrendsValuesAddToDB()
         task.dummy_date = 'noway'
         task.run()
-        self.db.commit(f'''DROP TABLE table_updates''') # WORKAROUND for UniqueViolation: duplicate key value violates unique constraint "table_updates_pkey" 😭
-    
+        self.db.commit(f'''DROP TABLE table_updates''')
+        # WORKAROUND for UniqueViolation: duplicate key value violates unique constraint "table_updates_pkey" 😭
+
     @patch.object(GtrendsTopics, 'run')
     @patch.object(GtrendsTopics, 'output')
     def test_updated_values_are_overridden(self, topics_mock, topics_run_mock):
         topics = ['41', '42', '43']
         topics_target = self.install_mock_target(topics_mock, lambda file: json.dump(topics, file))        
         self.dump_mock_target_into_fs(topics_target)
-        topics_run_mock.return_value = None # don't execute this
+        topics_run_mock.return_value = None  # don't execute this
         self.db.commit(f'''INSERT INTO gtrends_value VALUES (
             \'{42}\', DATE('{dt.datetime.now().strftime('%Y-%m-%d')}'), {200})''')
-        
+
         self.task = GtrendsValuesToDB()
         self.run_task(self.task)
-        
+
         self.assertCountEqual([(0,)],
             self.db.request(f'''SELECT COUNT(*) FROM gtrends_value where interest_value > 100'''))
-    
+
     @patch.object(GtrendsTopics, 'run')
     @patch.object(GtrendsTopics, 'output')
     def test_non_updated_values_are_overridden(self, topics_mock, topics_run_mock):
@@ -94,13 +98,13 @@ class TestGtrendsValuesToDB(DatabaseTaskTest):
         topics_run_mock.return_value = None # don't execute this
         self.db.commit(f'''INSERT INTO gtrends_value VALUES (
             \'{43}\', DATE('{dt.datetime.now().strftime('%Y-%m-%d')}'), {200})''')
-        
+
         self.task = GtrendsValuesToDB()
         self.task.run()
-        
+
         self.assertCountEqual([(1,)],
             self.db.request(f'''SELECT COUNT(*) FROM gtrends_value where interest_value > 100'''))
-    
+
     def run_task(self, task: luigi.Task):
         """
         Run task and all its dependencies synchronous.
