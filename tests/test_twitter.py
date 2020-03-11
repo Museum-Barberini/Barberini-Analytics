e8ef6657b2b2e662e629f0fb26505a2d9db2ae92
georg.tennigkeit <georg.tennigkeit@student.hpi.de>
2020-03-11 12:52:50 +0100

    finish first test

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -1,46 +1,36 @@
-from unittest.mock import MagicMock, patch
-from unittest import mock
+from unittest.mock import patch
 
-import pandas as pd
-
-from twitter import ExtractTweets, ExtractPerformanceTweets
+from twitter import FetchTwitter, ExtractTweets, ExtractPerformanceTweets
 from task_test import DatabaseTaskTest
 
 
 class TestExtractTweets(DatabaseTaskTest):
 
-    def __init__(self, methodName):
-        super().__init__(methodName)
-        self.task = self.isolate(ExtractTweets())
+    def setUp(self):
+        super().setUp()
+        self.task = ExtractTweets()
 
+    @patch.object(FetchTwitter, 'output')
     @patch.object(ExtractTweets, 'museum_user_id')
-    def test_extract_tweets(self, user_id_mock):
+    def test_extract_tweets(self, user_id_mock, raw_tweets_mock):
 
-        raw_tweets = pd.DataFrame(data={
-            'timestamp': ['2020-01-01 23:59:59,5', '2020-01-02 00:00:00,5'],
-            'user_id': ['42', '43'],
-            'tweet_id': ['1337', '9999'],
-            'text': ['Welcome to the exhibition!', 'I am so exited!'],
-            'parent_tweet_id': ['', '1337'],
-            'value_we_dont_care_about': ['foo', 'foooo']
-        })
-        extracted_tweets = pd.DataFrame(data={
-            'user_id': ['42', '43'],
-            'tweet_id': ['1337', '9999'],
-            'text': ['Welcome to the exhibition!', 'I am so exited!'],
-            'response_to': ['', '1337'],
-            'post_date': ['2020-01-01 23:59:59,5', '2020-01-02 00:00:00,5'],
-            'is_from_barberini': [True, False]
-        })
+        raw_tweets = '''timestamp,user_id,tweet_id,text,parent_tweet_id,some_other_value
+2020-01-01 23:59:59,42,1337,Welcome to the exhibition!,,foo
+2020-01-02 00:00:00,43,9999,I am so exited!,1337,fooooo
+'''
+        extracted_tweets = '''user_id,tweet_id,text,response_to,post_date,is_from_barberini
+42,1337,Welcome to the exhibition!,,2020-01-01 23:59:59,True
+43,9999,I am so exited!,1337,2020-01-02 00:00:00,False
+'''
         user_id = '42'
 
-        raw_tweets_mock = MagicMock()
-        raw_tweets_mock.return_value = raw_tweets
+        self.install_mock_target(
+            raw_tweets_mock,
+            lambda file: file.write(raw_tweets))
         user_id_mock.return_value = user_id
 
-        with mock.patch('twitter.pd.read_csv', new=raw_tweets_mock):
-            self.task.run()
+        self.task.run()
 
         with self.task.output().open('r') as output_file:
-            output = pd.read_csv(output_file)
-        pd.util.testing.assert_frame_equal(output, extracted_tweets)
+            output = output_file.read()
+        self.assertEquals(output, extracted_tweets)
