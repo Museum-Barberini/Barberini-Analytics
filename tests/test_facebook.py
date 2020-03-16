d19d26d3672cf7078b8fbbf13ad704d7ee73f5b9
Leon Schmidt <leon.hand09@gmail.com>
2020-03-16 15:48:40 +0100

    Added pagination test

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -1,4 +1,5 @@
 import json
+import jstyleson
 import unittest
 from unittest.mock import MagicMock, patch
 
@@ -13,15 +14,23 @@ class TestFacebookPost(unittest.TestCase):
 
     @patch('facebook.requests.get')
     @patch.object(facebook.FetchFbPosts, 'output')
-    def test_post_transformation(self, output_mock, requests_get_mock):
+    @patch.object(facebook.FetchFbPosts, 'input')
+    def test_post_transformation(self, input_mock, output_mock, requests_get_mock):
+        input_target = MockTarget('facts_in', format=UTF8)
+        input_mock.return_value = input_target
         output_target = MockTarget('post_out', format=UTF8)
         output_mock.return_value = output_target
 
-        with open('tests/test_data/facebook/post_actual.json', 'r') as data_in:
+        with open('data/barberini_facts.jsonc') as facts_in:
+            with input_target.open('w') as facts_target:
+                json.dump(jstyleson.load(facts_in), facts_target)
+
+        with open('tests/test_data/facebook/post_actual.json', 'r', encoding='utf-8') as data_in:
             actual_data = data_in.read()
 
         with open('tests/test_data/facebook/post_expected.csv',
-                  'r') as data_out:
+                  'r',
+                  encoding='utf-8') as data_out:
             expected_data = data_out.read()
 
         # Overwrite requests 'get' return value to provide our test data
@@ -37,8 +46,54 @@ class TestFacebookPost(unittest.TestCase):
             self.assertEqual(output_data.read(), expected_data)
 
     @patch('facebook.requests.get')
-    def test_invalid_response_raises_error(self, requests_get_mock):
+    @patch.object(facebook.FetchFbPosts, 'output')
+    @patch.object(facebook.FetchFbPosts, 'input')
+    def test_pagination(self, input_mock, output_mock, requests_get_mock):
+        input_target = MockTarget('facts_in', format=UTF8)
+        input_mock.return_value = input_target
+        output_target = MockTarget('post_out', format=UTF8)
+        output_mock.return_value = output_target
+
+        with open('data/barberini_facts.jsonc') as facts_in:
+            with input_target.open('w') as facts_target:
+                json.dump(jstyleson.load(facts_in), facts_target)
+
+        with open('tests/test_data/facebook/post_next.json', 'r') \
+        as next_data_in:
+            next_data = next_data_in.read()
+        
+        with open('tests/test_data/facebook/post_previous.json', 'r') \
+        as previous_data_in:
+            previous_data = previous_data_in.read()
+
+        def next_json():
+            return json.loads(next_data)
+
+        def previous_json():
+            return json.loads(previous_data)
+
+        next_response = MagicMock(ok=True, json=next_json)
+        previous_response = MagicMock(ok=True, json=previous_json)
+
+        requests_get_mock.side_effect = [
+            next_response,
+            previous_response
+        ]
+
+        facebook.FetchFbPosts().run()
+
+        self.assertEqual(requests_get_mock.call_count, 2)
+
+    @patch('facebook.requests.get')
+    @patch.object(facebook.FetchFbPosts, 'input')
+    def test_invalid_response_raises_error(self, input_mock, requests_get_mock):
+        input_target = MockTarget('facts_in', format=UTF8)
+        input_mock.return_value = input_target
         error_mock = MagicMock(status_code=404)
+
+        with open('data/barberini_facts.jsonc') as facts_in:
+            with input_target.open('w') as facts_target:
+                json.dump(jstyleson.load(facts_in), facts_target)
 
         def error_raiser():
             return facebook.requests.Response.raise_for_status(error_mock)
@@ -49,7 +104,6 @@ class TestFacebookPost(unittest.TestCase):
         requests_get_mock.return_value = mock_response
         with self.assertRaises(HTTPError):
             facebook.FetchFbPosts().run()
-
 
 class TestFacebookPostPerformance(unittest.TestCase):
 
