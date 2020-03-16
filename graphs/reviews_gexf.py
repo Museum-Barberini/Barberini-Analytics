b07d08db0c783d24ab9d3dd61efb79378a7afcad
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-03-16 17:11:29 +0100

    working commit of first color coding

--- graphs/reviews_gexf.py
+++ graphs/reviews_gexf.py
@@ -15,48 +15,77 @@ conn = psycopg2.connect(
     user=os.environ['POSTGRES_USER'],
     password=os.environ['POSTGRES_PASSWORD'])
 cur = conn.cursor()
-cur.execute('(select text from google_maps_review) UNION (select text from appstore_review) UNION (select text from tweet)')
-reviews = cur.fetchall()
-#reviews = reviews[:10000]  # debug
-texts = [
-    re.sub('[;\r\n,;]', '', text).lower()
-    for [text] in reviews
-    if text]
-stopwords = set([''] + list('-+/&.,;:') + [word.lower() for word in pd.read_csv('graphs/stopwords.csv', header=None)[0]])
+all_reviews = {}
+sources = ['appstore_review', 'google_maps_review', 'tweet']
+for source in sources:
+    cur.execute(f'select text from {source}')
+    all_reviews[source] = cur.fetchall()
+all_texts = {
+    source: [
+        re.sub('[;\r\n,;]', '', text).lower()
+        for [text] in reviews
+        if text
+    ]
+    for source, reviews in all_reviews.items()
+}
+stopwords = set([''] + list('-–—+/&.,;:') + [word.lower() for word in pd.read_csv('graphs/stopwords.csv', header=None)[0]])
 stopwords.add('museumbarberini')
-text_words = {
-    text: [
-        word
-        for word in [word.strip(',-+/&.,;:#@_()[]"\'') for word in text.split(' ')]
-        if word not in stopwords]
-    for text in texts}
+all_text_words = {
+    source: {
+        text: [
+            word
+            for word in [word.strip(',-–—+/&.,;:#@_()[]"\'') for word in text.split(' ')]
+            if word not in stopwords]
+        for text in texts
+    }
+    for source, texts in all_texts.items()
+}
 """
 further cleansing ideas:
 - define custom ignore words/replacements (e. g., museumbarberini -> barberini)
 """
 
-words = [word for words in text_words.values() for word in words]
+all_words = {
+    source: [
+        word
+        for words in text_words.values()
+        for word in words
+    ]
+    for source, text_words in all_text_words.items()
+}
 
-word_counts = pd.Series(words).value_counts()
+word_counts = {
+    source: pd.Series(words).value_counts()
+    for source, words in all_words.items()
+}
+all_word_counts = {}
+for source, word_count in word_counts.items():
+    for word, count in word_count.iteritems():
+        if word not in all_word_counts:
+            all_word_counts[word] = {source: 0 for source in sources}
+        word_counts = all_word_counts[word]
+        word_counts[source] = float(count)/len(all_words[source]) # todo
 
 G = nx.Graph()
-for word, count in word_counts.iteritems():
-    G.add_node(word, weight=float(count)/len(words)/2)
-for text, words in text_words.items():
+for word, counts in all_word_counts.items():
+    G.add_node(word, weights=counts)
+for source, text_words in all_text_words.items():
     edge_weights = {}
-    for word1 in words:
-        for word2 in words:
-            if word1 >= word2: continue
-            if (word1, word2) not in edge_weights:
-                edge_weights[(word1, word2)] = 0
-            edge_weights[(word1, word2)] += 1
+    for text, words in text_words.items():
+        for word1 in words:
+            for word2 in words:
+                if word1 >= word2: continue
+                if (word1, word2) not in edge_weights:
+                    edge_weights[(word1, word2)] = 0
+                edge_weights[(word1, word2)] += 1
     G.add_weighted_edges_from([list(edge) + [weight] for edge, weight in edge_weights.items()])
 
 #nx.write_gexf(G, 'output/reviews.gexf')
 
-DEBUG_LIMIT = 80  # sometimes works, sometimes fails!
+DEBUG_LIMIT = 200  # sometimes works, sometimes fails!
 #G.add_node(str(DEBUG_LIMIT), weight=0.05)
-node_weights = nx.get_node_attributes(G, 'weight')
+all_node_weights = nx.get_node_attributes(G, 'weights')
+node_weights = {node: sum(weights.values()) for node, weights in all_node_weights.items()}
 def gexf_escape(str):
     return str.replace('\'', '\\')
 nodes = list(G.nodes)
@@ -83,8 +112,8 @@ gexf_contents = f"""
         '''.join([
             f'''
             <node id="{node_ids[node]}" label="{gexf_escape(node)}">
-                <viz:size value="{node_weights[node] * 100 + random() / 10}"></viz:size>
-                <viz:color r="{(node_weights[node] - node_weights[node] + 0.05) * 4000}" g="12" b="12"></viz:color>
+                <viz:size value="{node_weights[node] * len(words) + random() / 10000}"></viz:size>
+                <viz:color r="{round(all_node_weights[node][sources[0]] * 20000)}" g="{round(all_node_weights[node][sources[1]] * 20000)}" b="{round(all_node_weights[node][sources[2]] * 20000)}"></viz:color>
             </node>
             '''
             for node in nodes
