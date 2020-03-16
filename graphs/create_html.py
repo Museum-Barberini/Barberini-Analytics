97c7075631e8c586bd55de7161fd3eddc4bbf9b1
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-03-16 18:47:45 +0100

    Fix encoding

--- graphs/create_html.py
+++ graphs/create_html.py
@@ -7,7 +7,7 @@ import shutil
 with open('output/reviews.gexf', encoding='utf-8') as gexf_file:
     gexf = gexf_file.read()
 gexf = re.sub(r"\s+", " ", gexf).strip()
-with open('graphs/graph_template.html') as template_file:
+with open('graphs/graph_template.html', encoding='utf-8') as template_file:
     template = template_file.read()
 html = template.replace('<?gefx template="true"?>', gexf)
 with open('output/graph.html', 'w', encoding='utf-8') as html_file:
