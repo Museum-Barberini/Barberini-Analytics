298f876b1deacedce06ac0fd4c011fdeb3b9f7ce
LinqLover <christoph.thiede@student.hpi.de>
2020-03-19 17:11:31 +0100

    Work at pbiviz - improve imports - set up capabilities.json - next step: create gexf in ts

--- graphs/pbiviz/src/visual.ts
+++ graphs/pbiviz/src/visual.ts
@@ -9,13 +9,11 @@ import powerbi from "powerbi-visuals-api";
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
-import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
-import VisualObjectInstance = powerbi.VisualObjectInstance;
 import DataView = powerbi.DataView;
-import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
-import IVisualHost = powerbi.extensibility.visual.IVisualHost;
-
-type Selection<T extends d3.BaseType> = d3.Selection<T, any,any, any>;
+import DataViewTable = powerbi.DataViewTable;
+import DataViewValueColumns = powerbi.DataViewValueColumns;
+import DataViewValueColumn = powerbi.DataViewValueColumn;
+import PrimitiveValue = powerbi.PrimitiveValue;
 
 //import 'sigma/build/plugins/sigma.parsers.json.require.js'; // file does not exist
 //import '../node_modules/sigma/build/plugins/sigma.parsers.json.min.js'; // Browser: sigma is not declared, even if added to pbiviz.json/externalJS
@@ -23,23 +21,25 @@ type Selection<T extends d3.BaseType> = d3.Selection<T, any,any, any>;
 //import {sigma} from 'sigma';
 //const sigmaatlas = import('sigma/build/plugins/sigma.layout.forceAtlas2.min'); // Browser: ChunkLoadError (promise rejeted), even if added to pbiviz.json/externalJS
 //const sigmaatlas = require('sigma/build/plugins/sigma.layout.forceAtlas2.min'); // Browser: uncaught sigma is not declared, even if added to pbiviz.json/externalJS
-const sigmaatlas = null; // DEBUG
 // The stuff above documents all fruiteless approaches to import the whole sigma module.
 // PROBLEM: I failed on several ways to import sigma.parsers.gexfparser here. sigma should be a module, but is a public function from the module.
 // See also here: https://github.com/jacomyal/sigma.js/issues/871#issuecomment-600577941
 // And see also here: https://github.com/DefinitelyTyped/DefinitelyTyped/issues/34776
-        
+
+require("./js/sigma.min");
+require("./js/sigma.forceatlas2");
+require("./js/sigma.parseGexf");
+
+// IMPORT TODO: This looks better now. If we cannot use TypeScript, we should declare sigma here as any.
+
+
 export class Visual implements IVisual {
     
     private target: HTMLElement;
-    private host: IVisualHost;
     private outerContainer: HTMLDivElement;
     private container: HTMLDivElement;
-    private circle: Selection<SVGElement>;
-    private textValue: Selection<SVGElement>;
-    private textLabel: Selection<SVGElement>;
     
-    private updateCount: number;
+    private sigInst: any;
     
     constructor(options: VisualConstructorOptions) {
         console.log("Visual constructor", options, new Date().toLocaleString());
@@ -58,53 +58,27 @@ export class Visual implements IVisual {
         this.container.style.height = '100%';
         this.outerContainer.appendChild(this.container);
         
-        /*var script = document.createElement('script');
-        script.type = "text/javascript"; 
-        script.src = "https://textexture.com/js/sigma.min.js";
-        this.container.appendChild(script);*/
         
-        var jQ = window['$'];
-        console.log(jQ);
-        jQ.getScript("https://textexture.com/js/sigma.min.js", function (data, textStatus, jqxhr) {
-            // LATEST TODO: Start here to work with sigma.
-            var sig: any = window.sigma;
-            console.log("window.sigma", sig);
-            var sigInst = sig.init(document.getElementById("foo"));//.drawingProperties({});
-            console.log("hhhheeeerrrreeee", sigInst);
-        });
-        /*var script2 = document.createElement('script');
-        script2.type = "text/javascript"; 
-        script2.innerHTML = '// We can import public js files by dynically \
-        console.log("before");\
-        $.getScript("https://textexture.com/js/sigma.min.js", function (data, textStatus, jqxhr) {\
-            var sigInst = sigma.init(document.getElementById("foo")).drawingProperties({}); console.log("hhhheeeerrrreeee", sigInst);\
-        });';
-        this.container.appendChild(script2);*/
+        this.sigInst = (sigma as any).init(document.getElementById("foo"));
+        this.sigInst.drawingProperties = {
+            defaultLabelColor: '#f00',
+            defaultLabelSize: 20,
+            defaultLabelBGColor: '#eee',
+            defaultLabelHoverColor: '#f00',
+            labelThreshold: 4,
+            defaultEdgeType: 'curve',
+            edgeWeightInfluence: 0,
+            minNodeSize: 0.5,
+            maxNodeSize: 15,
+            minEdgeSize: 0.3,
+            maxEdgeSize: 1
+        }
+        this.sigInst.mouseProperties = {
+            maxRatio: 4 // max zoom factor
+        };
+        this.sigInst.startForceAtlas2();
+        this.sigInst.stopForceAtlas2();
         
-        /*console.log("sigma?");
-        console.log(sigma);
-        console.log("sigmaatlas?");
-        console.log(sigmaatlas);
-        var sigInst: SigmaJs.Sigma = sigma.sigma({
-            container: this.container,
-            settings: {
-                defaultLabelColor: '#f00',
-                defaultLabelSize: 20,
-                defaultLabelBGColor: '#eee',
-                defaultLabelHoverColor: '#f00',
-                labelThreshold: 4,
-                defaultEdgeType: 'curve',
-                edgeWeightInfluence: 0,
-                minNodeSize: 0.5,
-                maxNodeSize: 15,
-                minEdgeSize: 0.3,
-                maxEdgeSize: 1
-            }
-        });*//*}).mouseProperties({
-            maxRatio: 4
-        });* /
-        console.log("siginst?");
-        console.log(sigInst);
         
         // TODO: Apply rest of sigma stuff here.
         
@@ -128,13 +102,29 @@ export class Visual implements IVisual {
                 "height": "100%"
             });*/
         
-        this.updateCount = 0;
         console.log("constructor done");
     }
 
     public update(options: VisualUpdateOptions) {
         console.log('Visual update', options, new Date().toLocaleString());
-        this.container.innerHTML = '<p>Update count: <a href="http://google.de"><em>' + this.updateCount++ + '</em></a> at ' + new Date().toLocaleString() + '</p>';
-        this.container.style.background = 'green';
+        
+        const dataView: DataView = options.dataViews[0];
+        const tableDataView: DataViewTable = dataView.table;
+        
+        if (!tableDataView) {
+            return;
+        }
+        
+        console.log(tableDataView.rows);
+        // LATEST TODO: Process table here and translate reviews_gexf.py
+        
+        const gexfString = '<?xml version="1.0" encoding="UTF-8"?> <gexf xmlns="http://www.gexf.net/1.3" version="1.3" xmlns:viz="http://www.gexf.net/1.3/viz" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.gexf.net/1.3 http://www.gexf.net/1.3/gexf.xsd"> <graph defaultedgetype="undirected" mode="static"> <attributes class="node" mode="static"> <attribute id="Gender" title="Gender" type="string"></attribute> </attributes> <nodes> <node id="0" label="Myriel"> <attvalues> <attvalue for="Gender" value="M"></attvalue> </attvalues> <viz:size value="1.0"></viz:size> <viz:position x="-95.274315" y="-46.711082" z="0.0"></viz:position> <viz:color r="153" g="153" b="153"></viz:color> </node> <node id="1" label="Napoleon"> <attvalues> <attvalue for="Gender" value="M"></attvalue> </attvalues> <viz:size value="1.0"></viz:size> <viz:position x="-48.155075" y="45.006344" z="0.0"></viz:position> <viz:color r="153" g="153" b="153"></viz:color> </node> <node id="2" label="MlleBaptistine"> <attvalues> <attvalue for="Gender" value="F"></attvalue> </attvalues> <viz:size value="1.0"></viz:size> <viz:position x="-25.879744" y="61.14878" z="0.0"></viz:position> <viz:color r="153" g="153" b="153"></viz:color> </node> <node id="3" label="MmeMagloire"> <attvalues> <attvalue for="Gender" value="F"></attvalue> </attvalues> <viz:size value="1.0"></viz:size> <viz:position x="8.465163" y="13.662047" z="0.0"></viz:position> <viz:color r="153" g="153" b="153"></viz:color> </node> <node id="4" label="CountessDeLo"> <attvalues> <attvalue for="Gender" value="F"></attvalue> </attvalues> <viz:size value="1.0"></viz:size> <viz:position x="72.727455" y="-87.02954" z="0.0"></viz:position> <viz:color r="153" g="153" b="153"></viz:color> </node> <node id="5" label="Geborand"> <attvalues> <attvalue for="Gender" value="F"></attvalue> </attvalues> <viz:size value="1.0"></viz:size> <viz:position x="-93.04595" y="5.3092685" z="0.0"></viz:position> <viz:color r="153" g="153" b="153"></viz:color> </node> </nodes> <edges> <edge source="1" target="0"> <attvalues></attvalues> </edge> <edge source="2" target="0" weight="8.0"> <attvalues></attvalues> </edge> <edge source="3" target="0" weight="10.0"> <attvalues></attvalues> </edge> <edge source="3" target="2" weight="6.0"> <attvalues></attvalues> </edge> <edge source="4" target="0"> <attvalues></attvalues> </edge> <edge source="5" target="0"> <attvalues></attvalues> </edge> </edges> </graph> </gexf>';
+        var gexf = new window.DOMParser().parseFromString(gexfString, "text/xml");
+        
+        this.sigInst.parseGexf(this.sigInst,    gexf);
+        this.sigInst.draw(2, 2, 2);
+        
+        
+        console.log("Update done");
     }
 }
