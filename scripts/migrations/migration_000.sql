006878cd58d19b44d1b47e73c4c85c8ca6857cf3
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-03-24 16:30:28 +0100

    Finish migration_000.sql

--- scripts/migrations/migration_000.sql
+++ scripts/migrations/migration_000.sql
@@ -1,7 +1,7 @@
 BEGIN;
 CREATE TABLE appstore_review (
     appstore_review_id TEXT,
-    text TEXT,
+    TEXT TEXT,
     rating INTEGER,
     app_version TEXT,
     vote_count INTEGER,
@@ -15,7 +15,7 @@ ALTER TABLE appstore_review
 
 CREATE TABLE fb_post (
     post_date TIMESTAMP,
-    text TEXT,
+    TEXT TEXT,
     fb_post_id TEXT
 );
 ALTER TABLE fb_post
@@ -44,64 +44,91 @@ ALTER TABLE fb_post_performance
 
 CREATE TABLE gomus_booking (
     booking_id INTEGER,
-    customer_id integer,
-    category text,
-    participants integer,
-    guide_id integer,
-    duration integer,
-    exhibition text,
-    title text,
-    status text,
+    customer_id INTEGER,
+    category TEXT,
+    participants INTEGER,
+    guide_id INTEGER,
+    duration INTEGER,
+    exhibition TEXT,
+    title TEXT,
+    status TEXT,
     start_datetime TIMESTAMP,
-    order_date date,
-    language text
+    order_date DATE,
+    language TEXT
 );
 ALTER TABLE gomus_booking
     ADD CONSTRAINT gomus_booking_primkey PRIMARY KEY (booking_id);
 
 CREATE TABLE gomus_customer (
-    customer_id integer,
-    postal_code text,
-    newsletter boolean,
-    gender text,
-    category text,
-    language text,
-    country text,
-    type text,
-    register_date date,
-    annual_ticket boolean,
-    valid_mail boolean
+    customer_id INTEGER,
+    postal_code TEXT,
+    newsletter BOOLEAN,
+    gender TEXT,
+    category TEXT,
+    language TEXT,
+    country TEXT,
+    type TEXT,
+    register_date DATE,
+    annual_ticket BOOLEAN,
+    valid_mail BOOLEAN
 );
 ALTER TABLE gomus_customer
     ADD CONSTRAINT gomus_customer_primkey PRIMARY KEY (customer_id);
 
 CREATE TABLE gomus_daily_entry (
-    id integer,
-    ticket text,
-    datetime timestamp without time zone NOT NULL,
-    count integer
+    id INTEGER,
+    ticket TEXT,
+    datetime TIMESTAMP,
+    count INTEGER
 );
 ALTER TABLE gomus_daily_entry
     ADD CONSTRAINT gomus_daily_entry_primkey PRIMARY KEY (id, datetime);
 
 CREATE TABLE gomus_event (
-    event_id integer NOT NULL,
-    customer_id integer,
-    booking_id integer,
-    reservation_count integer,
-    order_date date,
-    status text,
-    category text
+    event_id INTEGER,
+    customer_id INTEGER,
+    booking_id INTEGER,
+    reservation_count INTEGER,
+    order_date DATE,
+    status TEXT,
+    category TEXT
 );
 ALTER TABLE gomus_event
     ADD CONSTRAINT gomus_event_primkey PRIMARY KEY (event_id);
+
+CREATE TABLE gomus_expected_daily_entry (
+    id INTEGER,
+    ticket TEXT,
+    datetime TIMESTAMP,
+    count INTEGER
+);
+ALTER TABLE gomus_expected_daily_entry
+    ADD CONSTRAINT gomus_expected_daily_entry_primkey PRIMARY KEY (id, datetime);
+
+CREATE TABLE gomus_order (
+    order_id INTEGER,
+    order_date DATE,
+    customer_id INTEGER,
+    valid BOOLEAN,
+    paid BOOLEAN,
+    origin TEXT
+);
+ALTER TABLE gomus_order
+    ADD CONSTRAINT gomus_order_primkey PRIMARY KEY (order_id);
+
+CREATE TABLE gomus_to_customer_mapping (
+    gomus_id INTEGER,
+    customer_id INTEGER
+);
+ALTER TABLE gomus_to_customer_mapping
+    ADD CONSTRAINT gomus_to_customer_mapping_primkey PRIMARY KEY (gomus_id);
 
 CREATE TABLE google_maps_review (
     google_maps_review_id TEXT,
     date DATE,
     rating INTEGER,
-    text TEXT,
-    text_english TEXT,
+    TEXT TEXT,
+    TEXT_english TEXT,
     language TEXT
 );
 ALTER TABLE google_maps_review
@@ -115,10 +142,34 @@ CREATE TABLE gtrends_value (
 ALTER TABLE gtrends_value
     ADD CONSTRAINT gtrends_value_primkey PRIMARY KEY (topic, date);
 
+CREATE TABLE tweet (
+    user_id TEXT,
+    tweet_id TEXT,
+    TEXT TEXT,
+    response_to TEXT,
+    post_date DATE,
+    is_from_barberini BOOLEAN
+);
+ALTER TABLE tweet
+    ADD CONSTRAINT tweet_primkey PRIMARY KEY (tweet_id);
+
+CREATE TABLE tweet_performance (
+    tweet_id TEXT,
+    likes INTEGER,
+    retweets INTEGER,
+    replies INTEGER,
+    timestamp TIMESTAMP
+);
+ALTER TABLE tweet_performance
+    ADD CONSTRAINT tweet_performance_primkey PRIMARY KEY (tweet_id, timestamp);
+
+
 ALTER TABLE gomus_booking
     ADD CONSTRAINT customer_id_fkey FOREIGN KEY (customer_id) REFERENCES gomus_customer (customer_id);
 
 ALTER TABLE gomus_event
     ADD CONSTRAINT booking_id_fkey FOREIGN KEY (booking_id) REFERENCES gomus_booking (booking_id);
 
+ALTER TABLE gomus_order
+    ADD CONSTRAINT customer_id_fkey FOREIGN KEY (customer_id) REFERENCES gomus_customer (customer_id);
 COMMIT;
