55b40fa759214b391d6680f0ebe06f2b3257bcbf
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-03-24 11:47:16 +0100

    Fix import error

--- src/gomus/_utils/scrape_gomus.py
+++ src/gomus/_utils/scrape_gomus.py
@@ -14,7 +14,7 @@ from lxml import html
 
 from data_preparation_task import DataPreparationTask
 from gomus.orders import OrdersToDB
-from gomus._utils.extract_bookings import ExtractGomusBookings, hash_booker_id
+from gomus._utils.extract_bookings import ExtractGomusBookings, hash_id
 from set_db_connection_options import set_db_connection_options
 
 
@@ -263,7 +263,7 @@ class EnhanceBookingsWithScraper(GomusScraperTask):
                         'div[1]/div[1]/div[2]/small[1]').strip().split('\n')[0]
 
                     if re.match(r'^\S+@\S+\.\S+$', customer_mail):
-                        row['customer_id'] = hash_booker_id(customer_mail)
+                        row['customer_id'] = hash_id(customer_mail)
 
                 except IndexError:  # can't find customer mail
                     row['customer_id'] = 0
