edc7e9982c8219c4c66e9e501b4e480c95b64d87
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2020-03-26 11:39:12 +0100

    Apply suggestion to tests/test_gplay_reviews.py

--- tests/test_gplay_reviews.py
+++ tests/test_gplay_reviews.py
@@ -167,8 +167,7 @@ class TestFetchGplayReviews(unittest.TestCase):
 
         res = FetchGplayReviews().fetch_for_language('xyz')
 
-        self.assertIsInstance(res, list)
-        self.assertEqual(len(res), 0)
+        self.assertCountEqual([], res)
 
     @patch('gplay_reviews.FetchGplayReviews.get_app_id',
            return_value='com.barberini.museum.barberinidigital')
