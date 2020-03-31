69100ec68848ff36056c485ebf861e36c37ca1ef
Tom Wollnik <twollnik@Analytics-Projekt.e3m0rjbr2aaurkbfgmktcx0yre.ax.internal.cloudapp.net>
2020-03-31 16:57:04 +0200

    create table tweet_author in migration

--- scripts/migrations/migration_001.sql
+++ scripts/migrations/migration_001.sql
@@ -1,7 +1,12 @@
 BEGIN;
 
-	ALTER TABLE appstore_review RENAME COLUMN date TO post_date;
-	ALTER TABLE google_maps_review RENAME COLUMN date TO post_date;
-	ALTER TABLE gplay_review RENAME COLUMN date TO post_date;
+    ALTER TABLE appstore_review RENAME COLUMN date TO post_date;
+    ALTER TABLE google_maps_review RENAME COLUMN date TO post_date;
+    ALTER TABLE gplay_review RENAME COLUMN date TO post_date;
+
+    CREATE TABLE tweet_author (
+        user_id TEXT PRIMARY KEY, 
+	user_name TEXT NOT NULL
+    );
 
 COMMIT;
