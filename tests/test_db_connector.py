ee678a681b7b5fa674d2f0147d0e38f2e5cef8a6
Tom Wollnik <twollnik@Analytics-Projekt.e3m0rjbr2aaurkbfgmktcx0yre.ax.internal.cloudapp.net>
2020-04-14 12:30:45 +0200

    add tests for db connector

---
+++ tests/test_db_connector.py
@@ -0,0 +1,58 @@
+import psycopg2
+import time
+
+from db_connector import DbConnector
+from task_test import DatabaseTaskTest
+
+
+class TestDbConnector(DatabaseTaskTest):
+
+    def setUp(self):
+        super().setUp()
+        # give each test a temporary table to work with
+        self.temp_table = f'tmp_{time.time()}'.replace('.', '')
+        with self.db.connection as conn:
+            with conn.cursor() as curs:
+                curs.execute(f'CREATE TABLE {self.temp_table}'
+                              '(col1 INT, col2 INT);')
+                curs.execute(f'INSERT INTO {self.temp_table} '
+                              'VALUES (1,2),(3,4);')
+    def tearDown(self):
+        self.db.connection.set_isolation_level(0)
+        self.db.commit(f'DROP TABLE {self.temp_table};')
+        super().tearDown()
+
+    def test_query(self):
+
+        res = DbConnector.query(f'SELECT * FROM {self.temp_table};') 
+        self.assertEqual(res, [(1,2),(3,4)])
+
+    def test_execute(self):
+
+        DbConnector.execute(f'DELETE FROM {self.temp_table} WHERE col1=1;')
+
+        with self.db.connection as conn:
+            with conn.cursor() as curs:
+                curs.execute(f'SELECT * from {self.temp_table};')
+                table_content = curs.fetchall()
+
+        self.assertEqual(table_content, [(3,4)])
+        
+    def test_exists_case_not_empty(self):
+
+        exists = DbConnector.exists(f'SELECT * from {self.temp_table}')
+
+        self.assertTrue(exists)
+
+    def test_exists_case_empty(self):
+
+        exists = DbConnector.exists(
+                f'SELECT * from {self.temp_table} WHERE col1=10')
+
+        self.assertFalse(exists)
+
+    def test_error_is_raised_on_bad_table_name(self):
+
+        with self.assertRaises(psycopg2.Error):
+            DbConnector.query('SELECT * FROM table_that_does_not_exist;')
+
