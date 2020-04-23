81dfbedb140decb8b3a564069a3bd7aeb1bb5903
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-04-23 11:29:32 +0200

    New DB backup system incl. automatic deletion of old ones

---
+++ scripts/running/backup_db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BACKUP_DIR='/var/db-backups'
+
+case $1 in
+    monthly)
+        INTERVAL='monthly'
+        ;;
+    weekly)
+        INTERVAL='weekly'
+        ;;
+    daily)
+        INTERVAL='daily'
+        ;;
+    *)
+        exit 1
+        ;;
+esac
+BASE_NAME="db_dump_$INTERVAL"
+
+rm "$BACKUP_DIR/$BASE_NAME*"
+docker exec db pg_dump -U postgres barberini > "$BACKUP_DIR/$BASE_NAME-`date +%Y-%m-%d`.sql"
