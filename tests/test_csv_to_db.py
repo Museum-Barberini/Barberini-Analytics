86cd53954f69867c2a09d54b6f9a3157563dec45
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-04-28 13:14:37 +0200

    WIP

--- tests/test_csv_to_db.py
+++ tests/test_csv_to_db.py
@@ -78,6 +78,7 @@ class TestCsvToDb(DatabaseTaskTest):
 
     def test_adding_data_to_database_new_table(self):
 
+        import pdb; pdb.set_trace()  # TODO: db not closed here???
         self.dummy.run()
         actual_data = self.db.request(f"select * from {self.table_name};")
         self.assertEqual(actual_data, expected_data)
@@ -103,6 +104,7 @@ class TestCsvToDb(DatabaseTaskTest):
 
     def test_no_duplicates_are_inserted(self):
 
+        import pdb; pdb.set_trace()  # TODO: db already in use???
         # ----- Set up database -----
         self.db.commit(
             f"CREATE TABLE {self.table_name} (id int, A int, B text, C text);",
