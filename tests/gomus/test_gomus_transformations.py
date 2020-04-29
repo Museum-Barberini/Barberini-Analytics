a7479739a27bc0254ade327fc7f26def6e345817
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-04-29 15:12:04 +0200

    Reformat import

--- tests/gomus/test_gomus_transformations.py
+++ tests/gomus/test_gomus_transformations.py
@@ -8,9 +8,8 @@ from luigi.parameter import UnknownParameterException
 
 from gomus.customers import ExtractCustomerData, ExtractGomusToCustomerMapping
 from gomus.daily_entries import ExtractDailyEntryData
-from gomus.events import (cleanse_umlauts,
-                          ExtractEventData,
-                          FetchCategoryReservations)
+from gomus.events import \
+    cleanse_umlauts, ExtractEventData, FetchCategoryReservations
 from gomus.orders import ExtractOrderData
 from gomus._utils.extract_bookings import ExtractGomusBookings
 from gomus._utils.fetch_report import FetchEventReservations
