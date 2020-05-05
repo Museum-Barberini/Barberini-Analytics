3795d00821d7628cdc775c8e32bba5a0bc60e047
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-05-05 18:54:12 +0200

    Remove depecrated features

--- src/gomus/_utils/fetch_report_helper.py
+++ src/gomus/_utils/fetch_report_helper.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import argparse
 import csv
 import datetime as dt
 import logging
-import sys
 
 import requests
 import xlrd
@@ -32,62 +30,6 @@ REPORT_IDS = {
 
     'guides': -2
 }
-REPORT_IDS_INV = {v: k for k, v in REPORT_IDS.items()}
-
-
-def parse_arguments(args):
-    parser = argparse.ArgumentParser(
-        description="Refresh and fetch reports from go~mus")
-    report_group = parser.add_mutually_exclusive_group(required=True)
-
-    report_group.add_argument(
-        '-i',
-        '--report-id',
-        type=int,
-        help='ID of the report',
-        choices=REPORT_IDS.values())
-    report_group.add_argument(
-        '-t',
-        '--report-type',
-        type=str,
-        help='Type of the report',
-        choices=REPORT_IDS.keys())
-    parser.add_argument(
-        'action',
-        type=str,
-        help='Action to take',
-        choices=[
-            'refresh',
-            'fetch'],
-        nargs='?',
-        default='fetch')
-    parser.add_argument(
-        '-s',
-        '--session-id',
-        type=str,
-        help='Session ID to use for authentication',
-        required=True)
-
-    parser.add_argument(
-        '-I',
-        '--sheet-index',
-        type=int,
-        help="Excel sheet page number",
-        default=0)
-
-    parser.add_argument(
-        '-o',
-        '--output-file',
-        type=str,
-        help='Name of Output file (for fetching)')
-
-    parser.add_argument(
-        '-l',
-        '--luigi',
-        help='Set true if run as part of a Luigi task',
-        action='store_true')
-
-    return parser.parse_args(args)
 
 
 def parse_timespan(timespan, today=dt.datetime.today()):
@@ -148,19 +90,10 @@ def csv_from_excel(xlsx_content, target_csv, sheet_index):
         writer.writerow(sheet.row_values(row_num))
 
 
-def request_report(args=sys.argv[1:]):
-    args = parse_arguments(args)
-    if args.report_id:
-        report_id = args.report_id
-    else:
-        try:
-            report_id = REPORT_IDS[args.report_type]
-        except KeyError:  # should never happen because of argparse choices
-            raise ValueError(
-                f"Report type '{args.report_type}' not supported!")
-
+def request_report(report_type, session_id):
     base_url = 'https://barberini.gomus.de'
-    report_parts = REPORT_IDS_INV[report_id].split("_")
+    report_parts = report_type.split("_")
+    report_id = REPORT_IDS[report_type]
 
     logger.info(f"Working with report '{report_parts[0]}.xlsx'")
 
@@ -168,13 +101,8 @@ def request_report(args=sys.argv[1:]):
     if report_id > 0:
         base_url += f'/admin/reports/{report_id}'
 
-        if args.action == 'refresh':
-            logger.info("Refreshing report")
-            url = base_url + '/refresh'
-
-        elif args.action == 'fetch':
-            logger.info("Fetching report")
-            url = base_url + '.xlsx'
+        logger.info("Fetching report")
+        url = base_url + '.xlsx'
 
     else:  # Work with the kind of report that is requested directly
         logger.info("Directly downloading report")
@@ -185,25 +113,4 @@ def request_report(args=sys.argv[1:]):
 
         url = direct_download_url(base_url, report_parts[0], timespan)
 
-    res_content = get_request(url, args.session_id)
-
-    if args.action == 'fetch':
-        if not args.luigi:
-            filename = args.output_file
-            if not filename:
-                filename = REPORT_IDS_INV[report_id] + '.csv'
-            with open(filename, 'w', encoding='utf-8') as csv_file:
-                csv_from_excel(res_content, csv_file, args.sheet_index)
-            logger.info(f'Saved report to file "{filename}"')
-        else:
-            logger.info("Running as Luigi task, returning response content")
-            return res_content
-
-
-if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(levelname)s: %(message)s'
-    )
-    request_report()
+    return get_request(url, session_id)
