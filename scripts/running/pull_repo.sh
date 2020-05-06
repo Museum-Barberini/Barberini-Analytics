c61dcdabc90ad217d26c97a75199f470daf40c7b
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-06 12:20:43 +0200

    Get rid of migration scripts in db container
    Just run the migration scripts from the host.

--- scripts/running/pull_repo.sh
+++ scripts/running/pull_repo.sh
@@ -4,4 +4,8 @@ set -e
 cd "$(dirname "$0")"
 git pull
 
-docker exec -i db /app/scripts/migrations/migrate.sh /var/lib/postgresql/data/applied_migrations.txt
+set -a
+. /etc/barberini-analytics/secrets/database.env
+POSTGRES_HOST=localhost
+./scripts/migrations/migrate.sh /var/barberini-analytics/db-data/applied_migrations.txt
+set +a
--- scripts/setup/install_toolchain.sh
+++ scripts/setup/install_toolchain.sh
@@ -20,3 +20,11 @@ apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+# postgresql
+wget --quiet --no-check-certificate -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc \
+	| APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" \
+	| tee /etc/apt/sources.list.d/pgdg.list
+# scan new sources
+apt-get update
+apt-get install -y postgresql-client-12
