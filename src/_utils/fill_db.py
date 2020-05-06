727a54bc0b70e68f5c3c34e400413dd7709711b3
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2020-05-06 13:55:21 +0200

    Update fill_db.py

--- src/_utils/fill_db.py
+++ src/_utils/fill_db.py
@@ -37,14 +37,14 @@ class FillDBDaily(luigi.WrapperTask):
         yield TweetsToDB()
 
         # === Gomus ===
-        yield BookingsToDB()
-        yield CustomersToDB()
+        # yield BookingsToDB()
+        # yield CustomersToDB()
         yield DailyEntriesToDB()
         yield ExpectedDailyEntriesToDB()
-        yield EventsToDB()
-        yield GomusToCustomerMappingToDB()
-        yield OrderContainsToDB()
-        yield OrdersToDB()
+        # yield EventsToDB()
+        # yield GomusToCustomerMappingToDB()
+        # yield OrderContainsToDB()
+        # yield OrdersToDB()
 
 
 class FillDBHourly(luigi.WrapperTask):
