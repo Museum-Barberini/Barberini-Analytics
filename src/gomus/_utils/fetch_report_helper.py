17a690d9751c00723589283099aa954812120b88
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-05-07 12:18:10 +0200

    Reintroduce timespan calcs

--- src/gomus/_utils/fetch_report_helper.py
+++ src/gomus/_utils/fetch_report_helper.py
@@ -40,6 +40,15 @@ def parse_timespan(timespan, today=dt.datetime.today()):
         start_time = end_time - dt.timedelta(days=365)
     elif timespan == '1day':
         start_time = end_time
+    elif timespan == 'nextYear':
+        start_time = end_time
+        end_time = end_time + dt.timedelta(days=365)
+    elif timespan == 'nextWeek':
+        start_time = end_time
+        end_time = end_time + dt.timedelta(days=7)
+    elif timespan == 'all':
+        start_time = today - dt.timedelta(days=365*5)
+        end_time = today + dt.timedelta(days=365*2)
     else:
         start_time = dt.date.min  # check this for error handling
     return start_time, end_time
