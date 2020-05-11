ad42c44e1dc93ae60bdddabbd8baca31476022eb
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-11 20:04:34 +0200

    Make CsvToDb able to deal with generated columns

--- src/_utils/sql_scripts/copy.sql
+++ src/_utils/sql_scripts/copy.sql
@@ -1,12 +1,16 @@
--- {0} -- target table name
--- {1} -- column pairs to update, of format foo=EXCLUDED.foo, comma-separated
+/** arguments:
+  * {0}: target table name
+  * {1}: casted columns to update, of format CAST(foo AS type),
+         comma-separated
+  * {2}: column pairs to update, of format foo=EXCLUDED.foo, comma-separated
+  */
 
 BEGIN;
 CREATE TEMPORARY TABLE {0}_tmp (LIKE {0} INCLUDING ALL);
 COPY {0}_tmp FROM stdin WITH (FORMAT CSV);
 
 INSERT INTO {0}
-    SELECT * FROM {0}_tmp
+    SELECT {1} FROM {0}_tmp
 ON CONFLICT ON CONSTRAINT {0}_primkey
-    DO UPDATE SET {1};
+    DO UPDATE SET {2};
 COMMIT;
