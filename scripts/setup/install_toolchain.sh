597cec8ae7e76ccdc0a0f348615e8c0a233e2eff
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-11 16:44:28 +0200

    Remove superfluous tool from toolchain

--- scripts/setup/install_toolchain.sh
+++ scripts/setup/install_toolchain.sh
@@ -13,8 +13,7 @@ apt-get install -y \
 	ca-certificates \
 	curl \
 	gnupg2 \
-	software-properties-common \
-	postgresql-client-common
+	software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get install -y docker-ce docker-ce-cli containerd.io
