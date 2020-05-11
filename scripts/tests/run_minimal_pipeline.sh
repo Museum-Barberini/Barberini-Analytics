d55a821a78c31c129d69403420a98af5c955021f
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-11 16:15:36 +0200

    Try fix run_minimal_pipeline.sh

--- scripts/tests/run_minimal_pipeline.sh
+++ scripts/tests/run_minimal_pipeline.sh
@@ -2,23 +2,21 @@
 # Set up a fresh database and perform a minimal pipeline run on it. Fail if any errors occur.
 set -e
 
-#set -a
-#source /etc/secrets/database.env
-#set +a
-
-export POSTGRES_DB='barberini_test'
-TEMP_DIR="$(mktemp -d minimal_pipeline_XXX)"
-export APPLIED_FILE="$TEMP_DIR/applied_migrations.txt"
-touch $APPLIED_FILE
+set -a
+source /etc/secrets/database.env
+POSTGRES_HOST=localhost
+POSTGRES_DB='barberini_test'
+set +a
 
 set +e
 {
     set -e
-    docker exec -i db psql -U postgres -a postgres -c "DROP DATABASE IF EXISTS $POSTGRES_DB;"
-    docker exec -i db psql -U postgres -a postgres -c "CREATE DATABASE $POSTGRES_DB;"
+    docker exec -i db psql -U postgres -a postgres <<< "
+        DROP DATABASE IF EXISTS $POSTGRES_DB;
+        CREATE DATABASE $POSTGRES_DB;"
 
     ./scripts/migrations/migrate.sh
-    
+
     make startup
     do='make luigi-minimal' make docker-do
     make shutdown
