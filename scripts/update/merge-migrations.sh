e2f6e2c6228a124a445d4ff29cfe815b505bfc96
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-12 17:11:22 +0200

    Add simple merge-migrations logic

--- scripts/update/merge-migrations.sh
+++ scripts/update/merge-migrations.sh
@@ -1,32 +1,8 @@
-echo ""
-echo "#######################"
-echo "# Merge driver called #"
-echo "#######################"
-echo ""
+echo "merge-migrations"
 
-echo "ancestor: $1"
-echo "---------"
-cat $1
-echo ""
+mv "$2" "$4+1"
+echo "$3" > "$2"
 
-echo " current: $2"
-echo "---------"
-cat $2
-echo ""
-
-echo "   other: $3"
-echo "---------"
-cat $3
-echo ""
-
-echo "Resolving conflict ..."
-sleep 2
-echo "This is the merge result" > $2
-echo "Conflict resolved!"
-
-echo "resolved: $2"
-echo "---------"
-cat $2
-echo ""
+echo "merge-migrations: Resolved"
 
 exit 0
