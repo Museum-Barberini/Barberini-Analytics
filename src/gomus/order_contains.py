93e03321320892b4f94725fe129b3195939e7983
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-05-13 10:45:50 +0200

    Test if fault is in ToDB or Scrape task2

--- src/gomus/order_contains.py
+++ src/gomus/order_contains.py
@@ -1,10 +1,13 @@
 from csv_to_db import CsvToDb
 from gomus._utils.scrape_gomus import ScrapeGomusOrderContains
 
+from gomus.customers import ExtractGomusToCustomerMapping, GomusToCustomerMappingToDB
+
 
 class OrderContainsToDB(CsvToDb):
 
     table = 'gomus_order_contains'
 
     def requires(self):
-        return ScrapeGomusOrderContains(table=self.table)
+        gtcmtdb = GomusToCustomerMappingToDB()
+        return ExtractGomusToCustomerMapping(table=gtcmtdb.table, today=gtcmtdb.today, columns=gtcmtdb.columns)
