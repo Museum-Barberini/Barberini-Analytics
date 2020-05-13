42375dd7ccd0c79d3ee6eaca4abffe3ba3bf07ad
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-05-13 10:47:11 +0200

    Revert testing after concluding that fault is in scraper

--- src/gomus/order_contains.py
+++ src/gomus/order_contains.py
@@ -1,13 +1,10 @@
 from csv_to_db import CsvToDb
 from gomus._utils.scrape_gomus import ScrapeGomusOrderContains
 
-from gomus.customers import ExtractGomusToCustomerMapping, GomusToCustomerMappingToDB
-
 
 class OrderContainsToDB(CsvToDb):
 
     table = 'gomus_order_contains'
 
     def requires(self):
-        gtcmtdb = GomusToCustomerMappingToDB()
-        return ExtractGomusToCustomerMapping(table=gtcmtdb.table, today=gtcmtdb.today, columns=gtcmtdb.columns)
+        return ScrapeGomusOrderContains(table=self.table)
