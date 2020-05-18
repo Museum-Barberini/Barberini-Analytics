b001cc32044a99eb95a2c0165d3ee63d3f84015f
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-18 18:43:52 +0200

    set -e in setup scripts

--- scripts/setup/setup_db.sh
+++ scripts/setup/setup_db.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 # to be used right after the first 'make startup'
+set -e
+
 source /etc/barberini-analytics/secrets/database.env
 docker exec -it db psql -U postgres -a postgres -c "ALTER USER $POSTGRES_USER PASSWORD '$POSTGRES_PASSWORD';"
 docker exec -it db psql -U postgres -a postgres -c "CREATE DATABASE $POSTGRES_DB;"
