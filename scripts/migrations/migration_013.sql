c986341af4433f2853d7428688fd9de620bfda10
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-05-19 16:42:31 +0200

    Move migration from 12 to 13

--- scripts/migrations/migration_013.sql
+++ scripts/migrations/migration_013.sql
@@ -1,16 +1,15 @@
-BEGIN;
+BEGIN
 CREATE TABLE fb_post_comment (
     comment_id TEXT PRIMARY KEY,
     page_id TEXT,
     post_id TEXT,
     post_date TIMESTAMP,
     message TEXT,
-    from_barberini BOOLEAN,
+    is_from_museum BOOLEAN,
     parent TEXT,
     -- "parent TEXT REFERENCES fb_post_comment" does not work
     -- because ensure_foreign_keys would delete values which
     -- are not yet in the DB, although it would be more semantically precise
     FOREIGN KEY (page_id, post_id) REFERENCES fb_post
 );
-
 COMMIT;
