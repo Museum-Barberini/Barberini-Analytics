d29a1498ed0143ca3c52b9ffb628d50e8a8b59c9
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-21 02:07:11 +0200

    Rework and fix ensure_foreign_key_table

--- scripts/migrations/migration_014.sql
+++ scripts/migrations/migration_014.sql
@@ -4,27 +4,12 @@ BEGIN;
 
     CREATE SCHEMA absa;
 
-    CREATE PROCEDURE ensure_foreign_key(
+    CREATE PROCEDURE ensure_foreign_key_table(
             "table" regclass,
-            columns text[],
-            reftable text,
-            refcolumns text[]
+            key text
         )
         LANGUAGE plpgsql
         AS $$
-        DECLARE
-            key TEXT := (
-                SELECT string_agg(col, ', ')
-                FROM unnest(columns) col
-            );
-            newkey TEXT := (
-                SELECT string_agg('NEW.' || col, ', ')
-                FROM unnest(columns) col
-            );
-            refkey TEXT := (
-                SELECT string_agg(col, ', ')
-                FROM unnest(refcolumns) col
-            );
         BEGIN
             EXECUTE format('
                 CREATE
@@ -32,25 +17,18 @@ BEGIN;
                 FUNCTION foreign_key_trigger()
                     RETURNS "trigger" AS
                     $BODY$ BEGIN
-                        -- If reftable is invalid, this will raise an error
-                        PERFORM CAST(''%2$s'' AS regclass);
-                        -- Disabled due to performance reasons (O(n²)) 😢
-                        /*IF (SELECT (%3$s)) NOT IN (SELECT (%4$s) FROM %2$s)
-                        THEN
-                            RAISE EXCEPTION ''Foreign key violation: Key (%%=%%) \
-                            is not present in table %%'',
-                            ''(%5$s)'', (SELECT (%3$s)), ''%2$s'';
-                        END IF;*/
+                        -- If refkey is invalid, this will raise an error
+                        PERFORM CAST(NEW.%2$s AS regclass);
                         RETURN NEW;
                     END; $BODY$
                     LANGUAGE ''plpgsql'';
                 CREATE TRIGGER tr_before_insert_or_update
-                    BEFORE INSERT OR UPDATE OF %5$s
+                    BEFORE INSERT OR UPDATE OF %2$s
                     ON %1$s
                     FOR EACH ROW
                     EXECUTE PROCEDURE foreign_key_trigger();
                 ',
-                "table", reftable, newkey, refkey, key);
+                "table", key);
         END;
         $$;
 
@@ -65,10 +43,7 @@ BEGIN;
         word TEXT,
         PRIMARY KEY (source, post_id, word_index)
     );
-    CALL ensure_foreign_key(
-        'absa.post_word', array ['source', 'post_id'],
-        'post', array ['source', 'post_id']
-    );
+    CALL ensure_foreign_key_table('absa.post_word', 'source');
 
     CREATE TABLE absa.post_ngram (
         source TEXT,
@@ -78,9 +53,6 @@ BEGIN;
         ngram TEXT,
         PRIMARY KEY (source, post_id, n, word_index)
     );
-    CALL ensure_foreign_key(
-        'absa.post_ngram', array ['source', 'post_id'],
-        'post', array ['source', 'post_id']
-    );
+    CALL ensure_foreign_key_table('absa.post_ngram', 'source');
 
 COMMIT;
