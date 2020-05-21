23ad40afce026f0cbffbb8c8a80a8ae6dacf7fe5
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-05-21 02:17:28 +0200

    Nuke ensure_foreign_key_table that will just never work without materialized views

--- scripts/migrations/migration_014.sql
+++ scripts/migrations/migration_014.sql
@@ -4,34 +4,6 @@ BEGIN;
 
     CREATE SCHEMA absa;
 
-    CREATE PROCEDURE ensure_foreign_key_table(
-            "table" regclass,
-            key text
-        )
-        LANGUAGE plpgsql
-        AS $$
-        BEGIN
-            EXECUTE format('
-                CREATE
-                    OR REPLACE
-                FUNCTION foreign_key_trigger()
-                    RETURNS "trigger" AS
-                    $BODY$ BEGIN
-                        -- If refkey is invalid, this will raise an error
-                        PERFORM CAST(NEW.%2$s AS regclass);
-                        RETURN NEW;
-                    END; $BODY$
-                    LANGUAGE ''plpgsql'';
-                CREATE TRIGGER tr_before_insert_or_update
-                    BEFORE INSERT OR UPDATE OF %2$s
-                    ON %1$s
-                    FOR EACH ROW
-                    EXECUTE PROCEDURE foreign_key_trigger();
-                ',
-                "table", key);
-        END;
-        $$;
-
     CREATE TABLE absa.stopword (
         word TEXT PRIMARY KEY
     );
@@ -43,7 +15,6 @@ BEGIN;
         word TEXT,
         PRIMARY KEY (source, post_id, word_index)
     );
-    CALL ensure_foreign_key_table('absa.post_word', 'source');
 
     CREATE TABLE absa.post_ngram (
         source TEXT,
@@ -53,6 +24,5 @@ BEGIN;
         ngram TEXT,
         PRIMARY KEY (source, post_id, n, word_index)
     );
-    CALL ensure_foreign_key_table('absa.post_ngram', 'source');
 
 COMMIT;
