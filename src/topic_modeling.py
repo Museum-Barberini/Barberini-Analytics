deac0b444d21cdd6501c37c958f97cad1b4287fe
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2020-05-25 18:35:44 +0200

    add minimal mode, change quotation style

--- src/topic_modeling.py
+++ src/topic_modeling.py
@@ -22,10 +22,9 @@ from twitter import TweetsToDB
 logger = logging.getLogger('luigi-interface')
 
 # TODO: add migration script
-# TODO: add minimal mode
 
 
-"""
+'''
 Flow of control
 --------------
 
@@ -45,7 +44,7 @@ TopicModelingTextsDf     TopicModelingTopicsDf
                   |
                   |
        TopicModelingCreateCorpus()
-"""
+'''
 
 
 class TopicModeling(luigi.WrapperTask):
@@ -57,7 +56,7 @@ class TopicModeling(luigi.WrapperTask):
 
 class TopicModelingTextsToDb(CsvToDb):
 
-    table = "topic_modeling_texts"
+    table = 'topic_modeling_texts'
 
     def requires(self):
         return TopicModelingTextDf()
@@ -65,74 +64,74 @@ class TopicModelingTextsToDb(CsvToDb):
 
 class TopicModelingTopicsToDb(CsvToDb):
 
-    table = "topic_modeling_topics"
+    table = 'topic_modeling_topics'
 
     def requires(self):
         return TopicModelingTopicsDf()
 
 
 class TopicModelingTopicsDf(DataPreparationTask):
-    """
+    '''
     This task's main purpose is to provide a
     single output target that can be used a
     CsvToDb task.
     The the task also deletes existing data to 
     ensure consistency.
-    """
+    '''
 
     def requires(self):
         return TopicModelingFindTopics()
 
     def output(self):
         return luigi.LocalTarget(
-            f"{self.output_dir}/topic_modeling/topics_2.csv",
+            f'{self.output_dir}/topic_modeling/topics_2.csv',
             format=UTF8
         )
 
     def run(self):
 
-        with list(self.input())[0].open("r") as topics_file:
+        with list(self.input())[0].open('r') as topics_file:
             data = pd.read_csv(topics_file)
-        with self.output().open("w") as output_file:
+        with self.output().open('w') as output_file:
             data.to_csv(output_file, index=True)
 
         # delete existing topics data to make sure
         # there are no inconsistencies after the new
         # topics data is inserted.
         db_connector().execute(
-            "TRUNCATE topic_modeling_topics"
+            'TRUNCATE topic_modeling_topics'
         )
 
 class TopicModelingTextDf(DataPreparationTask):
-    """
+    '''
     This task's only purpose is to provide a
     single output target that can be used a
     CsvToDb task.
     The the task also deletes existing data to 
     ensure consistency.
-    """
+    '''
 
     def requires(self):
         return TopicModelingFindTopics()
 
     def output(self):
         return luigi.LocalTarget(
-            f"{self.output_dir}/topic_modeling/text_2.csv",
+            f'{self.output_dir}/topic_modeling/text_2.csv',
             format=UTF8
         )
 
     def run(self):
         
-        with list(self.input())[1].open("r") as text_file:
+        with list(self.input())[1].open('r') as text_file:
             data = pd.read_csv(text_file)
-        with self.output().open("w") as output_file:
+        with self.output().open('w') as output_file:
             data.to_csv(output_file, index=True)
 
         # delete existing topics data to make sure
         # there are no inconsistencies after the new
         # topics data is inserted.
         db_connector().execute(
-            "TRUNCATE topic_modeling_texts"
+            'TRUNCATE topic_modeling_texts'
         )
 
 
@@ -143,25 +142,25 @@ class TopicModelingFindTopics(DataPreparationTask):
 
     def output(self):
         yield luigi.LocalTarget(
-            f"{self.output_dir}/topic_modeling/topics.csv",
+            f'{self.output_dir}/topic_modeling/topics.csv',
             format=UTF8
         )
         yield luigi.LocalTarget(
-            f"{self.output_dir}/topic_modeling/texts.csv",
+            f'{self.output_dir}/topic_modeling/texts.csv',
             format=UTF8
         )
 
     def run(self):
 
-        with self.input().open("rb") as corpus_file:
+        with self.input().open('rb') as corpus_file:
             corpus = pickle.load(corpus_file)
 
         terms_df, texts_df = self.find_topics(corpus)
 
         output_files = self.output()
-        with next(output_files).open("w") as terms_file:
+        with next(output_files).open('w') as terms_file:
             terms_df.to_csv(terms_file, index=False)
-        with next(output_files).open("w") as texts_file:
+        with next(output_files).open('w') as texts_file:
             texts_df.to_csv(texts_file, index=False)
 
 
@@ -178,7 +177,7 @@ class TopicModelingFindTopics(DataPreparationTask):
             docs_in_timespan = [
                 doc for doc in docs if doc.in_year(model_name)]
 
-            if model_name == "all":
+            if model_name == 'all':
                 model = self.train_mgp(docs_in_timespan, K=12)
             else:
                 model = self.train_mgp(docs_in_timespan, K=10)
@@ -194,26 +193,26 @@ class TopicModelingFindTopics(DataPreparationTask):
             for i, topic_terms in enumerate(self.top_terms(model)):
                 for term in topic_terms:
                     out.append({
-                        "topic": i,
-                        "term": term[0],
-                        "count": term[1],
-                        "model": model_name
+                        'topic': i,
+                        'term': term[0],
+                        'count': term[1],
+                        'model': model_name
                     })
             topic_df = pd.DataFrame(out)
 
             # name topics
             get_title = lambda x: self.top_terms(model)[x][0][0]
-            topic_df["topic"] = topic_df["topic"].apply(get_title)
-            text_df["topic"] = text_df["topic"].apply(get_title)
+            topic_df['topic'] = topic_df['topic'].apply(get_title)
+            text_df['topic'] = text_df['topic'].apply(get_title)
 
-            # Small topics are bundled in topic "other".
+            # Small topics are bundled in topic 'other'.
             # A topic is considered small if it contains less than 2 percent 
             # of all text comments.
             total_size = len(text_df)
-            topic_sizes = text_df["topic"].value_counts().to_dict()
-            validate_topic = lambda x: x if topic_sizes[x] >= 0.02 * total_size else "other"
-            topic_df["topic"] = topic_df["topic"].apply(validate_topic)
-            text_df["topic"] = text_df["topic"].apply(validate_topic)
+            topic_sizes = text_df['topic'].value_counts().to_dict()
+            validate_topic = lambda x: x if topic_sizes[x] >= 0.02 * total_size else 'other'
+            topic_df['topic'] = topic_df['topic'].apply(validate_topic)
+            text_df['topic'] = text_df['topic'].apply(validate_topic)
 
             text_dfs.append(text_df)
             topic_dfs.append(topic_df)
@@ -242,12 +241,12 @@ class TopicModelingPreprocessCorpus(DataPreparationTask):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.stop_words = [
-            *get_stop_words("german"),
-            *get_stop_words("english"),
-            *["http", "https", "www", "com", "de", "google", "translated", 
-              "twitter", "fur", "uber", "html", "barberini", 
-              "museumbarberini", "museum", "ausstellung", "ausstellungen",
-              "potsdam", "mal"]
+            *get_stop_words('german'),
+            *get_stop_words('english'),
+            *['http', 'https', 'www', 'com', 'de', 'google', 'translated', 
+              'twitter', 'fur', 'uber', 'html', 'barberini', 
+              'museumbarberini', 'museum', 'ausstellung', 'ausstellungen',
+              'potsdam', 'mal']
         ]
 
     def requires(self):
@@ -255,25 +254,25 @@ class TopicModelingPreprocessCorpus(DataPreparationTask):
 
     def output(self):
         return luigi.LocalTarget(
-            f"{self.output_dir}/topic_modeling/corpus_preprocessed.pkl",
+            f'{self.output_dir}/topic_modeling/corpus_preprocessed.pkl',
             format=luigi.format.Nop  # output is binary file
         )
 
     def run(self):
 
-        with self.input().open("rb") as corpus_file:
+        with self.input().open('rb') as corpus_file:
             corpus = pickle.load(corpus_file)
 
         corpus = self.preprocess(corpus)
 
-        with self.output().open("w") as output_file:
+        with self.output().open('w') as output_file:
             pickle.dump(corpus, output_file)
 
     def preprocess(self, docs):
         
         for doc in docs:
-            # remove leading "None" (introduced by DB export)
-            doc.text = doc.text.replace("None ", "", 1)
+            # remove leading 'None' (introduced by DB export)
+            doc.text = doc.text.replace('None ', '', 1)
             doc.tokens = doc.text.lower()
             doc.guess_language()
             doc.tokens = word_tokenize(doc.tokens)
@@ -285,7 +284,7 @@ class TopicModelingPreprocessCorpus(DataPreparationTask):
             doc.tokens = [token for token in doc.tokens if len(token) > 1]
 
         # consider only german docs
-        docs = [doc for doc in docs if doc.language == "de"]
+        docs = [doc for doc in docs if doc.language == 'de']
 
         # stemming
         #stemmer = nltk.stem.cistem.Cistem()
@@ -318,23 +317,26 @@ class TopicModelingCreateCorpus(DataPreparationTask):
 
     def output(self):
         return luigi.LocalTarget(
-            f"{self.output_dir}/topic_modeling/corpus.pkl",
+            f'{self.output_dir}/topic_modeling/corpus.pkl',
             format=luigi.format.Nop  # output is binary file
         )
 
     def run(self):
 
-        texts = db_connector().query("""
+        query = '''
             SELECT text, source, post_date, post_id 
             FROM post
             WHERE NOT is_from_museum AND text IS NOT NULL
-        """)
+        '''
+        if self.minimal_mode:
+            query += 'LIMIT 200'
+        texts = db_connector().query(query)
         corpus = [
             Doc(row[0], row[1], row[2], row[3])
             for row in texts if row[0] is not None
         ]
 
-        with self.output().open("w") as output_file:
+        with self.output().open('w') as output_file:
             pickle.dump(corpus, output_file)
 
 
@@ -350,7 +352,7 @@ class Doc:
         self.language = None
 
     def in_year(self, year):
-        if year == "all":
+        if year == 'all':
             return True
         return str(self.post_date.year) == str(year)
 
@@ -366,15 +368,15 @@ class Doc:
             self.language = langdetect.detect(self.text)
         except langdetect.lang_detect_exception.LangDetectException as e:
             # langdetect can not handle emoji-only and link-only texts
-            logger.warning(f"langdetect failed for one Doc. Error: {e}")
-            logger.info(f"Failure happened for Doc {self.to_dict()}")
+            logger.warning(f'langdetect failed for one Doc. Error: {e}')
+            logger.info(f'Failure happened for Doc {self.to_dict()}')
 
     def to_dict(self):
         return {
-            "post_id": self.post_id,
-            "text": self.text,
-            "source": self.source,
-            "post_date": self.post_date,
-            "topic": self.topic,
-            "model_name": self.model_name
+            'post_id': self.post_id,
+            'text': self.text,
+            'source': self.source,
+            'post_date': self.post_date,
+            'topic': self.topic,
+            'model_name': self.model_name
         }
