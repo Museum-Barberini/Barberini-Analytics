cb489414d7e784825b84c0d6c4196034b38beff5
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-06-03 15:41:22 +0200

    Integrate PostAspectsToDb into daily pipeline

--- src/_utils/fill_db.py
+++ src/_utils/fill_db.py
@@ -4,6 +4,7 @@ from posts import PostsToDb, PostPerformanceToDb
 from gomus.gomus import GomusToDb
 from google_trends.gtrends_values import GtrendsValuesToDB
 from absa.post_ngrams import PostNgramsToDb
+from absa.post_aspects import PostAspectsToDb
 from topic_modeling import TopicModeling
 
 
@@ -25,6 +26,7 @@ class FillDBDaily(luigi.WrapperTask):
         yield GomusToDb()
 
         # Analysis tasks
+        yield PostAspectsToDb()
         yield PostNgramsToDb()
         yield TopicModeling()
 
