a3dc812221d058497040bfd7bcc77e3e3ea08954
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-06-03 18:43:38 +0200

    Refactoring and documentation of db_test

--- tests/_utils/db_test.py
+++ tests/_utils/db_test.py
@@ -21,6 +21,7 @@ logger = logging.getLogger('luigi-interface')
 
 @contextmanager
 def enforce_luigi_notifications(format):
+
     email = luigi.notifications.email()
     original = email.force_send, email.format
     luigi.notifications.email().format = format
@@ -31,6 +32,7 @@ def enforce_luigi_notifications(format):
 
 
 def check_env(name: str) -> bool:
+
     value = os.getenv(name, 'False')
     return distutils.util.strtobool(value) if value else False
 
@@ -41,6 +43,7 @@ def _perform_query(query):
     specific database. Meta queries include construction and deletion of
     databases.
     """
+
     connection = psycopg2.connect(
         host=os.environ['POSTGRES_HOST'],
         user=os.environ['POSTGRES_USER'],
@@ -63,10 +66,12 @@ class DatabaseTestProgram(suitable.PluggableTestProgram):
     """
 
     def handleUnsuccessfulResult(self, result):
+
         super().handleUnsuccessfulResult(result)
         self.send_notifications(result)
 
     def send_notifications(self, result):
+
         if result.wasSuccessful():
             return
         if not (check_env('GITLAB_CI') and check_env('FULL_TEST')):
@@ -111,7 +116,9 @@ class DatabaseTestProgram(suitable.PluggableTestProgram):
                             if tests
                         })))
 
-    def load_django_renderer(self):
+    @staticmethod
+    def load_django_renderer():
+
         import django
         from django.conf import settings
         from django.template.loader import render_to_string
@@ -130,10 +137,17 @@ class DatabaseTestSuite(suitable.FixtureTestSuite):
     """
 
     def setUpSuite(self):
+
         super().setUpSuite()
         self.setup_database_template()
 
     def setup_database_template(self):
+        """
+        Create an empty template database that will be used for testing.
+        Apply all migrations to ensure it has the latest schema.
+        See also DatabaseTestCase.setup_database().
+        """
+
         self.db_name = f'barberini_test_template{id(self)}'
         # avoid accidental access to production database
         os.environ['POSTGRES_DB'] = ''
@@ -149,14 +163,26 @@ class DatabaseTestSuite(suitable.FixtureTestSuite):
 
 
 class DatabaseTestCase(unittest.TestCase):
+    """
+    The base class of all test cases that make any access to the database or
+    luigi. Amongst others, this provides an isolated environment in terms of
+    database and luigi registry.
+    """
 
     def setUp(self):
+
         super().setUp()
         self.setup_database()
         self.setup_luigi()
         self.setup_filesystem()
 
     def setup_database(self):
+        """
+        Provide a throw-away database instance during the execution of the
+        test case. Database is created from the template which was prepared
+        in DatabaseTestSuite.setup_database_template().
+        """
+
         # Generate "unique" database name
         outer_db = os.getenv('POSTGRES_DB')
         os.environ['POSTGRES_DB'] = 'barberini_test_{clazz}_{id}'.format(
@@ -181,6 +207,7 @@ class DatabaseTestCase(unittest.TestCase):
         Clear luigi task cache to avoid reusing old task instances.
         For reference, see also luigi.test.helpers.LuigiTestCase.
         """
+
         _stashed_reg = luigi.task_register.Register._get_reg()
         luigi.task_register.Register.clear_instance_cache()
 
@@ -191,11 +218,13 @@ class DatabaseTestCase(unittest.TestCase):
             lambda: luigi.task_register.Register.clear_instance_cache())
 
     def setup_filesystem(self):
+
         self.dirty_file_paths = []
         self.addCleanup(lambda: [
             os.remove(file) for file in self.dirty_file_paths])
 
     def install_mock_target(self, mock_object, store_function):
+
         mock_target = luigi.mock.MockTarget(
             f'mock{hash(mock_object.hash())}', format=luigi.format.UTF8)
         with mock_target.open('w') as input_file:
@@ -204,7 +233,10 @@ class DatabaseTestCase(unittest.TestCase):
         return mock_target
 
     def dump_mock_target_into_fs(self, mock_target):
-        # We need to bypass MockFileSystem for accessing the file from node.js
+        """
+        We need to bypass MockFileSystem for accessing the file from node.js
+        """
+        
         with open(mock_target.path, 'w') as output_file:
             self.dirty_file_paths.append(mock_target.path)
             with mock_target.open('r') as input_file:
@@ -218,6 +250,7 @@ class DatabaseTestCase(unittest.TestCase):
         but I don't know how to do this better.
         Note that this approach does not support dynamic dependencies.
         """
+
         all_tasks = Queue()
         all_tasks.put(task)
         requirements = []
@@ -241,6 +274,7 @@ class DatabaseTestCase(unittest.TestCase):
         installed mocking stuff, coverage detection etc. Also, luigi's
         native scheduler is used.
         """
+
         sp.run(
             check=True,
             args=f'''make
