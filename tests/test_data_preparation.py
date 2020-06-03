3c21f8351797be33cbe25de1ac446a605b594e99
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-06-03 21:35:54 +0200

    Test ConcatCsv

--- tests/test_data_preparation.py
+++ tests/test_data_preparation.py
@@ -1,8 +1,11 @@
 from unittest.mock import MagicMock
 
+from luigi.format import UTF8
+from luigi.mock import MockTarget
 import pandas as pd
+from unittest.mock import patch
 
-from data_preparation import DataPreparationTask
+from data_preparation import DataPreparationTask, ConcatCsv
 from db_test import DatabaseTestCase
 
 TABLE_NAME = 'test_table'
@@ -399,3 +402,59 @@ class TestDataPreparationTask(DatabaseTestCase):
         self.task = DataPreparationTask(table=TABLE_NAME)
         self.task.condense_performance_values(df)
         pd.testing.assert_frame_equal(df, df_copy)
+
+
+class TestConcatCsv(DatabaseTestCase):
+
+    @patch.object(ConcatCsv, 'output')
+    @patch.object(ConcatCsv, 'input')
+    def test_one(self, input_mock, output_mock):
+
+        df_in = pd.DataFrame([[1, 'foo'], [2, 'bar']], columns=['a', 'b'])
+        self.install_mock_target(
+            input_mock,
+            lambda file: df_in.to_csv(file, index=False)
+        )
+        output_target = MockTarget(str(self))
+        output_mock.return_value = output_target
+
+        self.task = ConcatCsv()
+        self.run_task(self.task)
+        with output_target.open('r') as output:
+            df_out = pd.read_csv(output)
+
+        pd.testing.assert_frame_equal(df_in, df_out)
+
+    @patch.object(ConcatCsv, 'output')
+    @patch.object(ConcatCsv, 'input')
+    def test_two(self, input_mock, output_mock):
+
+        df_in0 = pd.DataFrame(
+            [[1, 'foo'], [2, 'bar']],
+            columns=['a', 'b']
+        )
+        df_in1 = pd.DataFrame(
+            [[42, 'spam'], [1337, 'häm']],
+            columns=['a', 'b']
+        )
+        input_mock.return_value = [
+            self.install_mock_target(
+                MagicMock(),
+                lambda file: df.to_csv(file, index=False)
+            ) for df in [df_in0, df_in1]]
+        output_target = MockTarget(str(self), format=UTF8)
+        output_mock.return_value = output_target
+
+        self.task = ConcatCsv()
+        self.run_task(self.task)
+        with output_target.open('r') as output:
+            df_out = pd.read_csv(output)
+
+        df_expected = pd.DataFrame(
+            [
+                [1, 'foo'], [2, 'bar'],
+                [42, 'spam'], [1337, 'häm'],
+            ],
+            columns=['a', 'b']
+        )
+        pd.testing.assert_frame_equal(df_expected, df_out)
