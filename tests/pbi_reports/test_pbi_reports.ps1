076eea98fc0af33c484dd51baf4b2c60be4c28f1
LinqLover <christoph.thiede@student.hpi.de>
2020-06-04 18:22:48 +0200

    Debug $csharpProvider

--- tests/pbi_reports/test_pbi_reports.ps1
+++ tests/pbi_reports/test_pbi_reports.ps1
@@ -1,4 +1,5 @@
 ﻿$csharpProvider = &"tests/pbi_reports/csharp_provider.ps1"
+Write-Host $csharpProvider  # debug
 $drawingLib = "nuget\NETStandard.Library.2.0.3\build\netstandard2.0\ref\System.Drawing.dll"
 Add-Type -CodeDomProvider $csharpProvider -ReferencedAssemblies ([System.Reflection.Assembly]::LoadFrom($drawingLib)) -TypeDefinition (Get-Content -Path tests/pbi_reports/test_pbi_reports.cs | Out-String)
 if (!$?) {
