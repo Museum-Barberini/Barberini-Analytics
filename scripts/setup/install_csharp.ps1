1b44daf37ddb8764d85da9ef2aab32a6c7ecb148
LinqLover <christoph.thiede@student.hpi.de>
2020-06-04 03:09:04 +0200

    Cleanup install scripts

--- scripts/setup/install_csharp.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile Nuget.exe
-
-&.\nuget.exe install Microsoft.CodeDom.Providers.DotNetCompilerPlatform -Version 2.0.1
