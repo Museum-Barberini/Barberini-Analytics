4739c976744b14d3dfe6129383c35e88d9f0efd8
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-06-15 11:12:40 +0200

    Test new args parameter

--- tests/test_query_db.py
+++ tests/test_query_db.py
@@ -25,7 +25,7 @@ class TestQueryDb(DatabaseTestCase):
             '''
         )
 
-    def test_query_db(self):
+    def test_query(self):
 
         self.task = QueryDb(query=f'SELECT * FROM {self.table}')
         self.task.output = lambda: \
@@ -38,4 +38,21 @@ class TestQueryDb(DatabaseTestCase):
         expected_result = pd.DataFrame(
             [(1, 2), (3, 4)],
             columns=['col1', 'col2'])
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def test_args(self):
+
+        self.task = QueryDb(
+            query=f'''SELECT * FROM (VALUES (%s, %s, '%%')) x(a, b, c)''',
+            args=(42, 'foo'))
+        self.task.output = lambda: \
+            luigi.mock.MockTarget(f'output/{self.table}')
+
+        self.run_task(self.task)
+        with self.task.output().open('r') as output_stream:
+            actual_result = pd.read_csv(output_stream)
+
+        expected_result = pd.DataFrame(
+            [(42, 'foo', '%')],
+            columns=['a', 'b', 'c'])
         pd.testing.assert_frame_equal(expected_result, actual_result)
