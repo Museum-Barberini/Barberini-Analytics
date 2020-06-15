813b2615882d54e8d2db435ec34df4976db33ebb
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-06-15 18:19:37 +0200

    Added external_error.py

--- scripts/running/cron.sh
+++ scripts/running/cron.sh
@@ -4,6 +4,10 @@ set -e
 USER=$1-run
 BASEDIR=$(dirname "$0")/../..
 PATH="/usr/local/bin:$PATH"
+
+EXT_ERROR = docker-compose /external_error.py
+EMAIL_STR = "INFO: Sending email to ['BP2019FN1-students@hpi.de']"
+
 export BARBERINI_ANALYTICS_CONTEXT=PRODUCTION
 
 LOGPATH="/var/log/barberini-analytics"
@@ -17,7 +21,10 @@ echo "======================================================================="
 echo "Starting $1 run at [$(date +"%Y-%m-%d %H:%M")]"
 make -C $BASEDIR startup USER=$USER
 docker-compose -p $USER -f $BASEDIR/docker/docker-compose.yml exec -T \
-    barberini_analytics_luigi /app/scripts/running/fill_db.sh $1
+        barberini_analytics_luigi /app/scripts/running/fill_db.sh $1 \
+    || grep -q $EMAIL_STR $LOGFILE \
+    || docker-compose -p $USER -f $BASEDIR/docker/docker-compose.yml exec -T \
+        barberini_analytics_luigi /app/scripts/running/external_error.py $1
 make -C $BASEDIR shutdown USER=$USER
 echo "Ending $1 run at [$(date +"%Y-%m-%d %H:%M")]"
 echo "======================================================================="
---
+++ scripts/running/external_error.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# The intention of this script is to send an error mail even outside of
+# Luigi's default capability, so that external errors are noticed (make
+#  startup is still assumed to work, since luigi's send_error_mail is used)
+import sys
+import luigi.notifications
+
+
+def send_error_mail(error_source):
+    luigi.notifications.send_error_email(
+        subject="[BP] External error in production pipeline",
+        message=("An external error has occured while trying to run the "
+                 "pipeline. For details see the according log.\n"
+                 f"Host: {error_source}")
+    )
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        raise ValueError(f"Usage: {sys.argv[0]} <error source>")
+    send_error_mail(sys.argv[1])
+    exit(1)  # If part of a bash-pipeline, it should still fail
