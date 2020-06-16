64df5a122328d0f4b8d05d5b5e0599eb5b12bb10
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-06-16 09:55:24 +0200

    Added tempfile so that the error mail may be triggered by every run

--- scripts/running/cron.sh
+++ scripts/running/cron.sh
@@ -11,6 +11,7 @@ export BARBERINI_ANALYTICS_CONTEXT=PRODUCTION
 
 LOGPATH="/var/log/barberini-analytics"
 LOGFILE=$LOGPATH/"$1-$(date +%Y-%m-%d).log"
+TMPFILE=$(mktemp)
 
 # Delete 2 weeks old log
 rm $LOGPATH/"$1-$(date -d '2 weeks ago' +%Y-%m-%d).log" || true
@@ -21,10 +22,12 @@ echo "Starting $1 run at [$(date +"%Y-%m-%d %H:%M")]"
 make -C $BASEDIR startup USER=$USER
 docker-compose -p $USER -f $BASEDIR/docker/docker-compose.yml exec -T \
         barberini_analytics_luigi /app/scripts/running/fill_db.sh $1 \
-    || grep -Eq $EMAIL_STR $LOGFILE \
+    || grep -Eq $EMAIL_STR $TMPFILE \
     || docker-compose -p $USER -f $BASEDIR/docker/docker-compose.yml exec -T \
         barberini_analytics_luigi /app/scripts/running/external_error.py $1
 make -C $BASEDIR shutdown USER=$USER
 echo "Ending $1 run at [$(date +"%Y-%m-%d %H:%M")]"
 echo "======================================================================="
-} >> $LOGFILE 2>&1
+} > $TMPFILE 2>&1
+
+cat $TMPFILE >> $LOGFILE
