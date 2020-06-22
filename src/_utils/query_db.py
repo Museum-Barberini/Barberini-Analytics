0dd2a864cbc11435b796f919689126f56ea3fb0a
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-06-22 18:27:16 +0200

    Revert REPORT_PROGRESS stuff (moved to !251 for the moment)

--- src/_utils/query_db.py
+++ src/_utils/query_db.py
@@ -1,23 +1,15 @@
 from ast import literal_eval
-import datetime as dt
-import logging
-import os
-import re
-import signal
-from time import sleep
-import traceback
 
 import luigi
 import pandas as pd
 
 from data_preparation import DataPreparationTask
 
-logger = logging.getLogger('luigi-interface')
-
 
 class QueryDb(DataPreparationTask):
 
     def __init__(self, *args, **kwargs):
+
         super().__init__(*args, **kwargs)
         self.args = str(self.args)
 
@@ -42,24 +34,26 @@ class QueryDb(DataPreparationTask):
         description="If True, all posts will be shuffled. For debugging and "
                     "exploration purposes. Might impact performance.")
 
-    report_progress_function_pattern = re.compile(r'''
-        \/\*<REPORT_PROGRESS
-        (\('(?P<message>[^']*)'\))?
-        >\*\/
-        (?P<relation>(?P<q>"?)[\w\.]+(?P=q))
-        (
-            \s*(?:[Aa][Ss]\s*)
-            (?P<alias>(?P<_q>"?)[\w\.]+(?P=_q))
-        )?
-    ''', flags=re.VERBOSE)
+    def output(self):
 
-    report_progress_row_interval = 1000
+        return luigi.LocalTarget(
+            f'{self.output_dir}/{self.task_id}.csv',
+            format=luigi.format.UTF8
+        )
 
-    report_progress_update_interval = dt.timedelta(seconds=1)
+    def run(self):
 
-    report_progress_enabled = False
+        # Unpack luigi-serialized parameter
+        args = literal_eval(self.args)
+
+        query = self.build_query()
+        rows, columns = self.db_connector.query_with_header(query, *args)
+        df = pd.DataFrame(rows, columns=columns)
+        with self.output().open('w') as output_stream:
+            df.to_csv(output_stream, index=False, header=True)
 
     def build_query(self):
+
         query = self.query
         if self.shuffle:
             query += ' ORDER BY RANDOM()'
@@ -68,151 +62,3 @@ class QueryDb(DataPreparationTask):
         if self.limit and self.limit != -1:
             query += f' LIMIT {self.limit}'
         return query
-
-    def output(self):
-        return luigi.LocalTarget(
-            f'{self.output_dir}/{self.task_id}.csv',
-            format=luigi.format.UTF8
-        )
-
-    def run(self):
-
-        query = self.build_query()
-        self.report_progress(self._run, query)
-
-    def _run(self, query):
-
-        # Unpack luigi-serialized parameter
-        args = literal_eval(self.args)
-
-        rows, columns = self.db_connector.query_with_header(query, *args)
-        df = pd.DataFrame(rows, columns=columns)
-        with self.output().open('w') as output_stream:
-            return df.to_csv(output_stream, index=False, header=True)
-
-    # TODO: Support message string parameter (save in list and increment
-    # sequence index). When printing, start new line if value if message has
-    # changed.
-    def report_progress(self, fun, query):
-        """
-        Time overhead: ~60%
-        """
-
-        has_reporter = False
-        progresses = {}
-        cleanups = []
-
-        def compile_progress(match):
-
-            nonlocal has_reporter
-            has_reporter = True
-
-            table_name = match.group('relation')
-            alias_name = match.group('alias')
-            message = match.group('message')
-            progress_name = f'progress_{len(progresses)}_{id(self)}'[:58]
-            progresses.update({progress_name: message})
-
-            for sequence in [progress_name, f'max_{progress_name}']:
-                self.db_connector.execute(f'''
-                    CREATE SEQUENCE {sequence} START 1
-                ''')
-                cleanups.append((
-                    lambda sequence_closure:
-                    # See https://stackoverflow.com/q/2295290
-                    lambda: self.db_connector.execute(f'''
-                        DROP SEQUENCE {sequence_closure}
-                    '''))(sequence))
-
-            return f'''(
-                SELECT * FROM (
-                    SELECT
-                        _table.*
-                    FROM
-                        SETVAL(
-                            'max_{progress_name}',
-                            (SELECT COUNT(*) FROM {table_name}) + 1,
-                            true
-                        ),
-                        (
-                            SELECT _table.*, ROW_NUMBER() OVER() _row_number
-                            FROM {table_name} _table
-                        ) _table
-                    WHERE CASE
-                        WHEN random() * {self.report_progress_row_interval}
-                            <= 1 THEN
-                        SETVAL('{progress_name}', _row_number, true ) <> 0
-                        ELSE TRUE
-                    END
-                ) _table
-            ) "{alias_name if alias_name else table_name}"'''
-
-        if self.report_progress_enabled:  # skipping progress for now, too slow
-            return fun(query)
-
-        try:
-            complex_query = self.report_progress_function_pattern.sub(
-                compile_progress,
-                query
-            )
-            if not has_reporter:
-                return fun(complex_query)
-
-            pid = os.fork()
-            # No zombie children
-            signal.signal(signal.SIGCHLD, signal.SIG_IGN)
-
-            if pid:  # we are parent
-                try:
-                    result = fun(complex_query)
-                    print("\nDone.", flush=True)
-                    return result
-                finally:
-                    os.kill(pid, signal.SIGTERM)
-
-            # we are child
-            try:
-                print()
-                while True:
-                    states = {
-                        message: self.db_connector.query(
-                            f'''
-                                SELECT
-                                    CAST(current.last_value AS int),
-                                    CAST(max.last_value AS int) - 1
-                                FROM
-                                    {progress_name} current,
-                                    max_{progress_name} max
-                            ''',
-                            only_first=True
-                        )
-                        for progress_name, message
-                        in progresses.items()
-                    }
-                    progress_strings = [
-                        (
-                            f"{message}: " if message else ""
-                        ) + (
-                            f"{current}/{max}, {current / max :2.1%}"
-                        )
-                        for message, (current, max)
-                        in states.items()
-                        if max
-                    ]
-                    print(
-                        "\rExecuting query ..." + (
-                            f" ({' | '.join(progress_strings)})"
-                            if progress_strings
-                            else ""
-                        ),
-                        end='',
-                        flush=True
-                    )
-                    sleep(self.report_progress_update_interval.total_seconds())
-            except Exception:
-                traceback.print_exc()
-                os._exit(1)
-
-        finally:
-            while cleanups:
-                cleanups.pop()()
--- tests/test_query_db.py
+++ tests/test_query_db.py
@@ -1,25 +1,13 @@
-from contextlib import contextmanager
-import datetime as dt
-import os
-import re
-from shutil import rmtree
-from string import Formatter
-import sys
-from tempfile import mkdtemp
 import time
 
-from ddt import ddt, data
 import luigi.mock
 import pandas as pd
 import pandas.testing
-import psycopg2.errors
-import psutil
 
 from db_test import DatabaseTestCase
 from query_db import QueryDb
 
 
-@ddt
 class TestQueryDb(DatabaseTestCase):
 
     def setUp(self):
@@ -66,193 +54,8 @@ class TestQueryDb(DatabaseTestCase):
         )
         pd.testing.assert_frame_equal(expected_result, actual_result)
 
-    @data('{}', '{} aS {}')
-    def test_report_progress(self, alias):
-
-        self.task = QueryDb(query=f'''
-            CREATE TEMPORARY TABLE series AS (
-                SELECT i FROM generate_series(1, 10) s(i)
-            );
-            SELECT {'seires' if count_placeholders(alias) > 1 else 'series'}.i
-            FROM
-                /*<REPORT_PROGRESS>*/{alias.format('series', 'seires')},
-                pg_sleep(
-                    -- Make sure sleep is not cached
-                    CASE WHEN i = i THEN 0.2 END
-                )
-        ''')
-        self.task.report_progress_enabled = True
-        self.task.report_progress_row_interval = 2
-
-        try:
-            with stdout_redirected() as buf_path:
-                try:
-                    with self.assert_process_invariant():
-                        actual_result = self.run_query()
-                finally:
-                    with open(buf_path) as buf:
-                        output = buf.read()
-        finally:
-            print(output)
-
-        expected_result = pd.DataFrame(
-            range(1, 10 + 1),
-            columns=['i']
-        )
-        self.task.report_progress_update_interval = dt.timedelta(seconds=0.2)
-        pd.testing.assert_frame_equal(expected_result, actual_result)
-
-        output_lines = output.strip().splitlines()
-        self.assertGreaterEqual(len(output_lines), 2)
-        for line in output_lines[:-1]:
-            match = re.fullmatch(r'.+(?: \(\d+/10, (\d+\.\d)+%\))?', line)
-            self.assertTrue(match)
-            percentage = match.group(1)
-            if percentage:  # maybe not yet loaded
-                self.assertTrue(0 <= int(percentage) <= 100)
-        self.assertEqual('Done.', output_lines[-1])
-
-    def test_report_progress_multiple(self):
-
-        self.task = QueryDb(query='''
-            CREATE TEMPORARY TABLE series AS (
-                SELECT i FROM generate_series(1, 10) s(i)
-            );
-            CREATE TEMPORARY TABLE squares AS (
-                SELECT series.i ^ 2 AS j
-                FROM
-                    /*<REPORT_PROGRESS('Doing this')>*/series,
-                    pg_sleep(
-                        -- Make sure sleep is not cached
-                        CASE WHEN i = i THEN 0.2 END
-                    )
-                WHERE mod(i, 2) <> 0
-            );
-            SELECT sqrt(j)::int AS i
-            FROM
-                /*<REPORT_PROGRESS('Doing that')>*/squares,
-                pg_sleep(
-                    -- Make sure sleep is not cached
-                    CASE WHEN j = j THEN 0.4 END
-                );
-        ''')
-        self.task.report_progress_enabled = True
-        self.task.report_progress_row_interval = 2
-
-        try:
-            with stdout_redirected() as buf_path:
-                try:
-                    with self.assert_process_invariant():
-                        actual_result = self.run_query()
-                finally:
-                    with open(buf_path) as buf:
-                        output = buf.read()
-        finally:
-            print(output)
-
-        expected_result = pd.DataFrame(
-            range(1, 10 + 1, 2),
-            columns=['i']
-        )
-        self.task.report_progress_update_interval = dt.timedelta(seconds=0.2)
-        pd.testing.assert_frame_equal(expected_result, actual_result)
-
-        output_lines = output.strip().splitlines()
-        self.assertGreaterEqual(len(output_lines), 3)
-        for line in output_lines[:-1]:
-            match = re.fullmatch(r'.+(?: \(\d+/10, (\d+\.\d)+%\))?', line)
-            self.assertTrue(match)
-            percentage = match.group(1)
-            if percentage:  # maybe not yet loaded
-                self.assertTrue(0 <= int(percentage) <= 100)
-        for msg in ['Doing this', 'Doing that']:
-            self.assertTrue(any(
-                line for line in output_lines if msg in line
-            ))
-        self.assertEqual('Done.', output_lines[-1])
-
-    @data(False, True)
-    def test_invalid_query(self, report_progress: bool):
-
-        self.task = QueryDb(query=f'''
-            CREATE TEMPORARY TABLE series AS (
-                SELECT i FROM generate_series(1, 10) s(i)
-            );
-            SELECT i / (i - 5)  -- raise "division by zero" error for i = 5
-            FROM
-                {
-                    '/*<REPORT_PROGRESS>*/series'
-                    if report_progress
-                    else 'series'
-                }, pg_sleep(
-                    -- Make sure sleep is not cached
-                    CASE WHEN i = i THEN 0.2 END
-                )
-        ''')
-        self.task.report_progress_enabled = True
-        self.task.report_progress_update_interval = dt.timedelta(seconds=0.2)
-        self.task.report_progress_row_interval = 2
-
-        with self.assertRaises(psycopg2.errors.DivisionByZero):
-            with self.assert_process_invariant():
-                self.run_query()
-
-    @contextmanager
-    def assert_process_invariant(self):
-
-        pid_before = os.getpid()
-        children_before = psutil.Process().children()
-        try:
-            yield
-        finally:
-            pid_after = os.getpid()
-            children_after = psutil.Process().children()
-            self.assertEqual(pid_before, pid_after)
-            self.assertCountEqual(children_before, children_after)
-
     def run_query(self):
 
         self.run_task(self.task)
         with self.task.output().open('r') as output_stream:
             return pd.read_csv(output_stream)
-
-
-# Helpers
-# ---
-# Inspired by: https://stackoverflow.com/a/22434262
-def fileno(file_or_fd):
-    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
-    if not isinstance(fd, int):
-        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
-    return fd
-
-
-@contextmanager
-def stdout_redirected(stdout=None):
-    if stdout is None:
-        stdout = sys.stdout
-
-    try:
-        temp_dir = mkdtemp()
-        temp_path = temp_dir + '/stdout'
-        with open(temp_path, 'w') as target:
-            stdout_fd = fileno(stdout)
-            # copy stdout_fd before it is overwritten
-            with os.fdopen(os.dup(stdout_fd), 'wb') as copied:
-                stdout.flush()
-                os.dup2(target.fileno(), stdout_fd)  # $ exec > to
-                try:
-                    yield temp_path
-                finally:
-                    # restore stdout to its previous value
-                    # NOTE: dup2 makes stdout_fd inheritable unconditionally
-                    stdout.flush()
-                    os.dup2(copied.fileno(), stdout_fd)  # $ exec >&copied
-    finally:
-        rmtree(temp_dir)
-# ---
-
-
-def count_placeholders(format_string: str) -> int:
-
-    return len(list(Formatter().parse(format_string)))
