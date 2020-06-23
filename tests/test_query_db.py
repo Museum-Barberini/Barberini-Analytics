02950c0284755cf3156c9a2b3d544957be7995bb
LinqLover <christoph.thiede@student.hpi.de>
2020-06-23 19:40:55 +0200

    Add support for kwargs in QueryDb
    Also fixes minimal mode implementation of QueryDb (setting LIMIT).

--- tests/test_query_db.py
+++ tests/test_query_db.py
@@ -31,9 +31,7 @@ class TestQueryDb(DatabaseTestCase):
         self.task.output = lambda: \
             luigi.mock.MockTarget(f'output/{self.table}')
 
-        self.run_task(self.task)
-        with self.task.output().open('r') as output_stream:
-            actual_result = pd.read_csv(output_stream)
+        actual_result = self.run_query()
 
         expected_result = pd.DataFrame(
             [(1, 2), (3, 4)],
@@ -48,11 +46,36 @@ class TestQueryDb(DatabaseTestCase):
         self.task.output = lambda: \
             luigi.mock.MockTarget(f'output/{self.table}')
 
-        self.run_task(self.task)
-        with self.task.output().open('r') as output_stream:
-            actual_result = pd.read_csv(output_stream)
+        actual_result = self.run_query()
 
         expected_result = pd.DataFrame(
             [(42, 'foo', '%')],
-            columns=['a', 'b', 'c'])
+            columns=['a', 'b', 'c']
+        )
         pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def test_kwargs(self):
+
+        self.task = QueryDb(
+            query='''
+                SELECT *
+                FROM (VALUES (%(spam)s, %(eggs)s, '%%'))
+                x(a, b, c)
+            ''',
+            kwargs={'spam': 42, 'eggs': 'foo'})
+        self.task.output = lambda: \
+            luigi.mock.MockTarget(f'output/{self.table}')
+
+        actual_result = self.run_query()
+
+        expected_result = pd.DataFrame(
+            [(42, 'foo', '%')],
+            columns=['a', 'b', 'c']
+        )
+        pd.testing.assert_frame_equal(expected_result, actual_result)
+
+    def run_query(self):
+
+        self.run_task(self.task)
+        with self.task.output().open('r') as output_stream:
+            return pd.read_csv(output_stream)
