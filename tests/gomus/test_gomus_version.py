fe7cbad044669a38ad16dacc27db82008ce191b2
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-06-26 15:46:44 +0200

    Refactor TestGomusVersion

--- tests/gomus/test_gomus_version.py
+++ tests/gomus/test_gomus_version.py
@@ -10,15 +10,20 @@ EXPECTED_VERSION_TAG = 'v4.1.4.1 – Premium Edition'
 
 
 class TestGomusVersion(DatabaseTestCase):
+
     def test_session_id_is_valid(self):
-        # test if GOMUS_SESS_ID env variable contains a valid session id
+        """
+        test if GOMUS_SESS_ID env variable contains a valid session id
+        """
+
         response = requests.get(
             'https://barberini.gomus.de/',
             cookies={'_session_id': os.environ['GOMUS_SESS_ID']},
             allow_redirects=False)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(200, response.status_code)
 
     def test_gomus_version(self):
+
         if GOMUS_SESS_ID == '':
             raise ValueError(
                 "Please make sure a valid Gomus session ID is provided")
@@ -34,4 +39,4 @@ class TestGomusVersion(DatabaseTestCase):
         # if this line no. changes, that also means that adjustments to Gomus
         # have been made
         version_tag = response.text.splitlines()[774]
-        self.assertEqual(version_tag, EXPECTED_VERSION_TAG)
+        self.assertEqual(EXPECTED_VERSION_TAG, version_tag)
