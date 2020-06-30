67ec4c4bd1405195f639cda080c451aa795222b8
Leon Schmidt <leon.schmidt@student.hpi.de>
2020-06-30 21:59:41 +0200

    Refine merge_databases.sh

--- scripts/update/merge_databases.sh
+++ scripts/update/merge_databases.sh
@@ -2,16 +2,16 @@
 set -e
 
 if [ -z "$1" ] || [ -z "$2" ]
-    then echo "Usage: $0 <FROM-Host> <TO-Host>"
+    then echo "Usage: $0 <PATCH-Host> <BASE-Host>"
     exit 1
 fi
 
-HOST_FROM=$1
-HOST_TO=$2
+HOST_PATCH=$1
+HOST_BASE=$2
 
-HOST_FROM_DUMP="/tmp/$HOST_FROM.sql"
-HOST_TO_DUMP="/tmp/$HOST_TO.sql"
-MERGE_DUMP="/tmp/$HOST_FROM-TO-$HOST_TO.pgdump"
+HOST_PATCH_DUMP="/tmp/$HOST_PATCH.sql"
+HOST_BASE_DUMP="/tmp/$HOST_BASE.sql"
+MERGE_DUMP="/tmp/$HOST_PATCH-BASE-$HOST_BASE.pgdump"
 export PGUSER="postgres"
 export PGDATABASE="barberini"
 if [ ! -z $POSTGRES_PASSWORD ]
@@ -23,35 +23,44 @@ function enable_foreign_key_checks {
 }
 trap enable_foreign_key_checks EXIT
 
-# Dump data into custom psql format (except exhibition)
-echo "Exporting TO-state"
-pg_dump -Fc -a -h "$HOST_TO" -f "$HOST_TO_DUMP" -T exhibition -T table_updates
-pg_dump -a -h "$HOST_TO" -f "$HOST_TO_DUMP-exhibitions.sql" -t exhibition
+# Dump BASE data into custom psql format (except exhibition)
+echo "Exporting BASE-state"
+pg_dump -Fc -a -h "$HOST_BASE" -f "$HOST_BASE_DUMP" -T exhibition \
+    -T table_updates
+pg_dump -a -h "$HOST_BASE" -f "$HOST_BASE_DUMP-exhibitions.sql" -t exhibition
 
 # disable search_path modification so existing relations can be found properly
-sed -i -e "s/^SELECT pg_catalog.set_config('search_path', '', false);$//" "$HOST_TO_DUMP-exhibitions.sql"
+sed -i -e "s/^SELECT pg_catalog.set_config('search_path', '', false);$//" \
+    "$HOST_BASE_DUMP-exhibitions.sql"
 
-echo "Exporting FROM-state"
-pg_dump -a -h "$HOST_FROM" -f "$HOST_FROM_DUMP" --column-inserts --on-conflict-do-nothing -T table_updates -T gomus_daily_entry -T gomus_expected_daily_entry
+# Dump PATCH data into "INSERT INTO ... ON CONFLICT DO NOTHING";
+# SQL statements to be applied to the BASE data
+echo "Exporting PATCH-state"
+pg_dump -a -h "$HOST_PATCH" -f "$HOST_PATCH_DUMP" --column-inserts \
+    --on-conflict-do-nothing -T table_updates -T gomus_daily_entry \
+    -T gomus_expected_daily_entry
 
 # Put data into local DB (so it isn't altered on the remote host)
-# first put TO-data (data on the host TO which the other should be added)
+# first put BASE-data, then add PATCH-data
 echo "Re-creating local DB '$PGDATABASE' and applying migrations"
-psql -h "localhost" -d "postgres" -c "DROP DATABASE $PGDATABASE;" -c "CREATE DATABASE $PGDATABASE;"
+psql -h "localhost" -d "postgres" -c "DROP DATABASE $PGDATABASE;" \
+    -c "CREATE DATABASE $PGDATABASE;"
 docker exec -it "$USER"-barberini_analytics_luigi bash -c \
-    "echo \"\" > /var/lib/postgresql/data/applied_migrations.txt && POSTGRES_DB=\"$PGDATABASE\" make apply-pending-migrations"
+    "echo \"\" > /var/lib/postgresql/data/applied_migrations.txt \
+    && POSTGRES_DB=\"$PGDATABASE\" make apply-pending-migrations"
 
 echo "Dropping gomus_customer '0' to prevent inserting issues"
-psql -h "localhost" -c "DELETE FROM gomus_customer WHERE customer_id = 0;"
+psql -h "localhost" -c "DELETE PATCH gomus_customer WHERE customer_id = 0;"
 
-echo "Restoring TO-state"
-pg_restore -Fc -a -h "localhost" -j 5 --disable-triggers -d $PGDATABASE "$HOST_TO_DUMP"
-psql -f "$HOST_TO_DUMP-exhibitions.sql" -h "localhost"
+echo "Restoring BASE-state"
+pg_restore -Fc -a -h "localhost" -j 5 --disable-triggers -d "$PGDATABASE" \
+    "$HOST_BASE_DUMP"
+psql -f "$HOST_BASE_DUMP-exhibitions.sql" -h "localhost"
 
-echo "Merging FROM-state"
+echo "Merging PATCH-state"
 psql -h "localhost" \
     -c "SET session_replication_role = 'replica';" \
-    -f "$HOST_FROM_DUMP" \
+    -f "$HOST_PATCH_DUMP" \
     -c "SET session_replication_role = 'origin';"
 
 # ensure performance data is condensed
@@ -62,5 +71,5 @@ docker exec -it "$USER"-barberini_analytics_luigi bash -c \
 echo "Exporting MERGED state"
 pg_dump -Fc -h "localhost" -f "$MERGE_DUMP"
 
-echo "See '$MERGE_DUMP' for merge result. Insert into empty database like this:"
+echo "See '$MERGE_DUMP' for merge result. Example for importing:"
 echo "pg_restore -Fc -h <HOST> -d <DB> -U <USER> $MERGE_DUMP"
