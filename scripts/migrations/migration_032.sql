798484f49d0025f4df79c75dd71dfb5a99849088
laura.holz <laura.holz@student.hpi.uni-potsdam.de>
2020-07-05 15:11:03 +0200

    change joins

--- scripts/migrations/migration_032.sql
+++ scripts/migrations/migration_032.sql
@@ -4,166 +4,154 @@ BEGIN;
 
     ALTER TABLE gomus_customer ADD COLUMN tourism_tags TEXT[];
 
-    CREATE VIEW customer_characteristics as (
-        with 
-            o_general as (
-                select 
-                    goc.customer_id as customer_id,
-                    count(goc.o_id) as order_count,
-                    sum(goc.quantity_sum) as ordered_article_count, 
-                    sum(goc.order_price) as sum_order_price 
-                from (
-                    select 
-                        o.order_id as o_id,
-                        sum(oc.quantity) as quantity_sum,
-                        sum(oc.price) as order_price,
-                        o.customer_id as customer_id 
-                    from 
-                        gomus_order as o,
-                        gomus_order_contains as oc 
-                    where 
-                        o.order_id = oc.order_id
-                    group by 
+    CREATE VIEW customer_characteristics AS (
+        WITH 
+            o_general AS (
+                SELECT 
+                    goc.customer_id AS customer_id,
+                    count(goc.o_id) AS order_count,
+                    sum(goc.quantity_sum) AS ordered_article_count, 
+                    sum(goc.order_price) AS sum_order_price 
+                FROM (
+                    SELECT 
+                        o.order_id AS o_id,
+                        sum(oc.quantity) AS quantity_sum,
+                        sum(oc.price) AS order_price,
+                        o.customer_id AS customer_id 
+                    FROM 
+                        gomus_order AS o
+                        JOIN gomus_order_contains AS oc USING (order_id)
+                    GROUP BY 
                         o.order_id,
                         o.customer_id
-                    ) as goc
-                group by 
+                    ) AS goc
+                GROUP BY 
                     goc.customer_id
             ),
-            o_common as (
-                select 
+            o_common AS (
+                SELECT 
                     customer_id,
                     (array_agg(
                         ticket_name ORDER BY ticket_quantity DESC
-                    ))[1] as most_common_ticket
-                from (
-                    select 
-                        o.customer_id as customer_id,
-                        oc.ticket as ticket_name,
-                        sum(quantity) as ticket_quantity
-                    from 
-                        gomus_customer as c,
-                        gomus_order as o,
-                        gomus_order_contains as oc 
-                    where 
-                        o.order_id=oc.order_id 
-                        and
-                        c.customer_id=o.customer_id 
-                    group by 
+                    ))[1] AS most_common_ticket
+                FROM (
+                    SELECT 
+                        o.customer_id AS customer_id,
+                        oc.ticket AS ticket_name,
+                        sum(quantity) AS ticket_quantity
+                    FROM
+                        gomus_customer AS c
+                        JOIN gomus_order AS o USING (customer_id)
+                        JOIN gomus_order_contains AS oc USING (order_id)
+                    GROUP BY 
                         o.customer_id,
                         oc.ticket
-                    ) as grouped_ticket
-                group by 
+                    ) AS grouped_ticket
+                GROUP BY 
                     customer_id
             ),
-            b_general as (
-                select 
-                    c.customer_id as customer_id,
-                    count(b.booking_id) as booking_count, 
-                    sum(b.participants) as sum_booked_participants
-                from 
-                    gomus_customer as c,
-                    gomus_booking as b
-                where 
-                    c.customer_id = b.customer_id
-                group by 
+            b_general AS (
+                SELECT 
+                    c.customer_id AS customer_id,
+                    count(b.booking_id) AS booking_count, 
+                    sum(b.participants) AS sum_booked_participants
+                FROM 
+                    gomus_customer AS c
+                    JOIN gomus_booking AS b USING (customer_id)
+                GROUP BY 
                     c.customer_id
             ),
-            b_common_name as (
-                select 
+            b_common_name AS (
+                SELECT 
                     customer_id,
                     (array_agg(
                         title ORDER BY title_count DESC
-                    ))[1] as most_common_booking
-                from (
-                    select 
+                    ))[1] AS most_common_booking
+                FROM (
+                    SELECT 
                         customer_id,
                         title,
-                        count(*) as title_count
-                    from 
-                        gomus_booking as b 
-                    group by 
+                        count(*) AS title_count
+                    FROM 
+                        gomus_booking AS b 
+                    GROUP BY 
                         customer_id,
                         title
-                    ) as grouped_booking
-                group by customer_id
+                    ) AS grouped_booking
+                GROUP BY customer_id
             ), 
-            b_common_category as (
-                select 
+            b_common_category AS (
+                SELECT 
                     customer_id,
                     (array_agg(
                         category ORDER BY category_count DESC
-                    ))[1] as most_common_booking_category
-                from (
-                    select
+                    ))[1] AS most_common_booking_category
+                FROM (
+                    SELECT
                         customer_id,
                         category,
-                        count(*) as category_count
-                    from 
-                        gomus_booking as b 
-                    group by 
+                        count(*) AS category_count
+                    FROM 
+                        gomus_booking AS b 
+                    GROUP BY 
                         customer_id,
                         category
-                    ) as grouped_booking
-                group by customer_id
+                    ) AS grouped_booking
+                GROUP BY customer_id
             ),
-            e_general as (
-                select 
-                    c.customer_id as customer_id,
-                    count(e.event_id) as event_count, 
-                    sum(e.reservation_count) as sum_event_reservations
-                from 
-                    gomus_customer as c,
-                    gomus_event as e
-                where 
-                    c.customer_id=e.customer_id
-                group by 
+            e_general AS (
+                SELECT 
+                    c.customer_id AS customer_id,
+                    count(e.event_id) AS event_count, 
+                    sum(e.reservation_count) AS sum_event_reservations
+                FROM
+                    gomus_customer AS c
+                    JOIN gomus_event AS e USING (customer_id)
+                GROUP BY 
                     c.customer_id
             ),
-            e_common_name as (
-                select 
-                    grouped_event.customer_id as customer_id,
+            e_common_name AS (
+                SELECT 
+                    grouped_event.customer_id AS customer_id,
                     (array_agg(
                         b.title ORDER BY booking_reservations_count DESC
-                    ))[1] as most_common_event
-                from (
-                    select 
+                    ))[1] AS most_common_event
+                FROM (
+                    SELECT 
                         customer_id,
                         booking_id,
-                        count(*) as booking_reservations_count
-                    from 
-                        gomus_event as e
-                    group by 
+                        count(*) AS booking_reservations_count
+                    FROM 
+                        gomus_event AS e
+                    GROUP BY 
                         customer_id,
                         booking_id
-                    ) as grouped_event, 
-                    gomus_booking as b
-                where
-                    grouped_event.booking_id=b.booking_id
-                group by 
+                    ) AS grouped_event
+                    JOIN gomus_booking AS b USING (booking_id)
+                GROUP BY 
                     grouped_event.customer_id
             ),
-            e_common_category as (
-                select 
-                    customer_id as customer_id,
+            e_common_category AS (
+                SELECT 
+                    customer_id,
                     (array_agg(
                         category ORDER BY category_count DESC
-                    ))[1] as most_common_event_category
-                from (
-                    select 
+                    ))[1] AS most_common_event_category
+                FROM (
+                    SELECT 
                         customer_id,
                         category,
-                        count(*) as category_count
-                    from 
-                        gomus_event as e
-                    group by 
+                        count(*) AS category_count
+                    FROM 
+                        gomus_event AS e
+                    GROUP BY 
                         customer_id,
                         category
-                    ) as grouped_event
-                group by 
+                    ) AS grouped_event
+                GROUP BY 
                     customer_id
             )
-        select 
+        SELECT 
             c.*,
             order_count,
             ordered_article_count,
@@ -177,24 +165,16 @@ BEGIN;
             most_common_event,
             most_common_booking_category,
             most_common_event_category
-        from 
-            gomus_customer as c
-            left join o_general 
-                on c.customer_id=o_general.customer_id
-            left join o_common 
-                on c.customer_id=o_common.customer_id
-            left join b_general 
-                on c.customer_id=b_general.customer_id
-            left join b_common_name
-                on c.customer_id=b_common_name.customer_id
-            left join b_common_category
-                on c.customer_id=b_common_category.customer_id
-            left join e_general
-                on c.customer_id=e_general.customer_id
-            left join e_common_name
-                on c.customer_id=e_common_name.customer_id
-            left join e_common_category
-                on c.customer_id=e_common_category.customer_id
+        FROM 
+            gomus_customer AS c
+            LEFT JOIN o_general USING (customer_id)
+            LEFT JOIN o_common USING (customer_id)
+            LEFT JOIN b_general USING (customer_id)
+            LEFT JOIN b_common_name USING (customer_id)
+            LEFT JOIN b_common_category USING (customer_id)
+            LEFT JOIN e_general USING (customer_id)
+            LEFT JOIN e_common_name USING (customer_id)
+            LEFT JOIN e_common_category USING (customer_id)
     );
 
 COMMIT;
