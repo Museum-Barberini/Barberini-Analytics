d235500453b9095d9dbda4a06eff5bf523e3d5ea
laura.holz <laura.holz@student.hpi.uni-potsdam.de>
2020-07-18 01:03:37 +0200

    try out new approach

--- src/gomus/_utils/cleanse_data.py
+++ src/gomus/_utils/cleanse_data.py
@@ -93,6 +93,10 @@ class CleansePostalCodes(DataPreparationTask):
 
         self.total_count = len(customer_df)
 
+        # TODO remove
+        nomi = pgeocode.Nominatim('DE')
+        postal_code_data = nomi.query_postal_code('14478')
+
         if not customer_df.empty:
             results = customer_df.apply(
                     lambda x: self.match_postal_code(
@@ -106,13 +110,47 @@ class CleansePostalCodes(DataPreparationTask):
             customer_df['cleansed_country'] = \
                 [result[1] for result in results]
 
-            postal_analysis = \
-                customer_df['cleansed_postal_code'].apply(self.query_lat_long)
+            unique_postal = \
+                customer_df.loc[
+                    customer_df['cleansed_country'] == 'Deutschland',
+                    'cleansed_postal_code'].sort_values().unique()
+            unique_postal = unique_postal[~pd.isnull(unique_postal)]
+
+            nomi = pgeocode.Nominatim('DE')
+
+            lat_list = []
+            long_list = []
+            for postal in unique_postal:
+                data = nomi.query_postal_code(postal)
+                lat_list.append(data['latitude'])
+                long_list.append(data['longitude'])
+
+            lat_dict = dict(zip(unique_postal, lat_list))
+            long_dict = dict(zip(unique_postal, long_list))
 
             customer_df['latitude'] = \
-                [lat_long[0] for lat_long in postal_analysis]
+                customer_df['cleansed_postal_code'].map(lat_dict)
+
             customer_df['longitude'] = \
-                [lat_long[1] for lat_long in postal_analysis]
+                customer_df['cleansed_postal_code'].map(long_dict)
+
+            # customer_df['latitude'] = \
+            #     customer_df['cleansed_postal_code'].replace(
+            #         {'latitude': lat_dict})['latitude']
+            # customer_df['latitude'] = customer_df['latitude'].fillna(0)
+
+            # customer_df['longitude'] = \
+            #     customer_df['cleansed_postal_code'].replace(
+            #         {'longitude': long_dict})['longitude']
+            # customer_df['longitude'] = customer_df['longitude'].fillna(0)
+
+            # postal_analysis = \
+            #     customer_df['cleansed_postal_code'].apply(self.query_lat_long)
+
+            # customer_df['latitude'] = \
+            #     [lat_long[0] for lat_long in postal_analysis]
+            # customer_df['longitude'] = \
+            #     [lat_long[1] for lat_long in postal_analysis]
 
         skip_percentage = '{0:.0%}'.format(
             self.skip_count / self.total_count if self.total_count else 0
