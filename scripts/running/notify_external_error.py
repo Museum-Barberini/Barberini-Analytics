c19ddd9bcab98b02f320efa65f1b0c5230c7752e
LinqLover <christoph.thiede@student.hpi.de>
2020-07-27 09:40:56 +0200

    Include hostname into external error notification

--- scripts/running/notify_external_error.py
+++ scripts/running/notify_external_error.py
@@ -2,13 +2,18 @@
 # The intention of this script is to send an error mail even outside of
 # Luigi's default capability, so that external errors are noticed (make
 # startup is still assumed to work, since luigi's send_error_mail is used)
+
+import socket
 import sys
+
 import luigi.notifications
 
 
 def send_error_mail(error_source):
+
     luigi.notifications.send_error_email(
-        subject="External error in production pipeline",
+        subject=("External error in production pipeline. "
+                 f"Host: {socket.gethostname()}"),
         message=("An external error has occured while trying to run the "
                  "pipeline. For details see the according log.\n"
                  f"Error source: {error_source}")
