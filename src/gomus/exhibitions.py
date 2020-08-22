217fd7c366e0321df0378acfd778c5d68c0f3188
Christoph Thiede <cthiede@Analytics-Projekt.e3m0rjbr2aaurkbfgmktcx0yre.ax.internal.cloudapp.net>
2020-08-22 13:30:02 +0200

    Refine requirements in gomus.exhibitions

--- src/gomus/exhibitions.py
+++ src/gomus/exhibitions.py
@@ -7,34 +7,46 @@ import requests
 from _utils import CsvToDb, DataPreparationTask
 
 
+class ExhibitionTimesToDb(CsvToDb):
+
+    table = 'exhibition_time'
+
+    def _requires(self):
+
+        return luigi.task.flatten([
+            ExhibitionsToDb(),
+            super()._requires()
+        ])
+
+    def requires(self):
+
+        return FetchExhibitionTimes()
+
+
 class ExhibitionsToDb(CsvToDb):
 
     table = 'exhibition_raw'
 
     def requires(self):
+
         return FetchExhibitions()
-
-
-class ExhibitionTimesToDb(CsvToDb):
-
-    table = 'exhibition_time'
-
-    def requires(self):
-        return FetchExhibitionTimes()
 
 
 class FetchExhibitions(DataPreparationTask):
 
     url = luigi.Parameter(
-        default='https://barberini.gomus.de/api/v4/exhibitions?per_page=100')
+        default='https://barberini.gomus.de/api/v4/exhibitions?per_page=100'
+    )
 
     def output(self):
+
         return luigi.LocalTarget(
             f'{self.output_dir}/gomus/exhibitions.csv',
             format=UTF8
         )
 
     def run(self):
+
         response = requests.get(self.url)
         response.raise_for_status()
         content = response.json()
@@ -45,12 +57,14 @@ class FetchExhibitions(DataPreparationTask):
             df.to_csv(output_file, index=False, header=True)
 
     def extract_rows(self, content):
+
         return [
             self.extract_row(exhibition)
             for exhibition in content['exhibitions']
         ]
 
     def extract_row(self, exhibition):
+
         row = {'title': exhibition['title']}
         picture = exhibition.get('picture')
         if picture:
@@ -63,15 +77,18 @@ class FetchExhibitions(DataPreparationTask):
 class FetchExhibitionTimes(FetchExhibitions):
 
     def requires(self):
+
         yield ExhibitionsToDb()
 
     def output(self):
+
         return luigi.LocalTarget(
             f'{self.output_dir}/gomus/exhibition_times.csv',
             format=UTF8
         )
 
     def extract_rows(self, content):
+
         return [
             row
             for exhibition in content['exhibitions']
@@ -79,6 +96,7 @@ class FetchExhibitionTimes(FetchExhibitions):
         ]
 
     def extract_row(self, exhibition):
+
         return [
             {
                 'title': exhibition['title'],
