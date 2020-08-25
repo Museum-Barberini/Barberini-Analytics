0f04ed2748a3baa7b309fb0fdb0f97d3005760d4
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2020-08-25 17:19:04 +0200

    fix view

--- scripts/migrations/migration_046.sql
+++ scripts/migrations/migration_046.sql
@@ -8,18 +8,20 @@ BEGIN;
 	FROM
 		-- keyword-intervals enriched with interval-based R value
 		(
-		SELECT ki.term, ki.start_date, ki.end_date, ki.count_interval, ki.count_interval::float/count(*)::float AS R_interval
-		FROM twitter_keyword_intervals ki INNER JOIN twitter_extended_candidates ec
-		ON ki.term = ec.term AND ec.post_date BETWEEN ki.start_date AND ki.end_date
-		GROUP BY ki.term, ki.start_date, ki.end_date, ki.count_interval
+		SELECT * FROM (
+			SELECT ki.term, ki.start_date, ki.end_date, ki.count_interval, count(*)::float/ki.count_interval::float AS R_interval
+			FROM twitter_keyword_intervals ki INNER JOIN twitter_extended_candidates ec
+			ON ki.term = ec.term AND ec.post_date BETWEEN ki.start_date AND ki.end_date
+			WHERE ki.count_interval > 0
+			GROUP BY ki.term, ki.start_date, ki.end_date, ki.count_interval
+			) as temp
+		-- only consider keyword-intervals with an R value below 50
+		WHERE R_interval <= 50
 		) AS ki_r
 	INNER JOIN twitter_extended_candidates ec
 	ON
 	-- match tweets to intervals based on the post date
 		ec.post_date between ki_r.start_date and ki_r.end_date
-	AND
-	-- only consider keyword-intervals with an R value below 50
-		ki_r.R_interval <= 50
 	AND
 	-- tweet should contain the term as a whole word
 		ec.text ~* ('\m' || ki_r.term || '\M')
