ae19f521dd230942b3220437da291f2795a0584c
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-08-25 17:05:39 +0200

    Please linter

--- src/extended_twitter_collection/__init__.py
+++ src/extended_twitter_collection/__init__.py
@@ -1,3 +1,9 @@
+"""
+Extended twitter collection.
+
+TODO: Refine docstrings!
+"""
+
 from .collect_tweets import ExtendedTwitterDatasetToDB
 
 __all__ = [
--- src/extended_twitter_collection/collect_tweets.py
+++ src/extended_twitter_collection/collect_tweets.py
@@ -1,13 +1,19 @@
+"""
+Extended twitter collection.
+
+TODO: Refine docstrings!
+"""
+
+import datetime as dt
+import logging
+
 import pandas as pd
 import luigi
 from luigi.format import UTF8
-import datetime as dt
 import twint
-import sys
-import os
 
-from _utils import DataPreparationTask, CsvToDb
-from extended_twitter_collection.keyword_intervals import KeywordIntervalsToDB
+from _utils import CsvToDb, DataPreparationTask, StreamToLogger, logger
+from .keyword_intervals import KeywordIntervalsToDB
 
 
 class ExtendedTwitterDatasetToDB(CsvToDb):
@@ -31,11 +37,11 @@ class CollectExtendedTwitterDataset(DataPreparationTask):
 
     def run(self):
 
-        active_intervals = self.db_connector.query("""
+        active_intervals = self.db_connector.query('''
             SELECT term, count_interval
             FROM twitter_keyword_intervals
             WHERE end_date >= CURRENT_DATE
-        """)
+        ''')
 
         seven_days_ago = (
             dt.datetime.today() - dt.timedelta(days=7)
@@ -50,6 +56,7 @@ class CollectExtendedTwitterDataset(DataPreparationTask):
             ))
 
         # always query 'museumbarberini'
+        # TODO: Don't hardcode this.
         tweet_dfs.append(self.fetch_tweets(
             query="museumbarberini",
             start_date=seven_days_ago,
@@ -57,53 +64,40 @@ class CollectExtendedTwitterDataset(DataPreparationTask):
         ))
 
         tweet_df = pd.concat(tweet_dfs)
-        tweet_df = tweet_df.drop_duplicates(subset=["term", "tweet_id"])
+        tweet_df = tweet_df.drop_duplicates(subset=['term', 'tweet_id'])
 
         with self.output().open('w') as output_file:
             tweet_df.to_csv(output_file, index=False, header=True)
 
     def fetch_tweets(self, query, start_date, limit):
-        """
-        All searches are limited to German tweets (twitter lang code de)
-        """
-
-        print(f"Querying Tweets. term \"{query}\" "
-              "limit: {limit}, start_date: {start_date}")
+        """All searches are limited to German tweets (twitter lang code de)."""
+        logger.info(
+            f"Querying Tweets. term \"{query}\" "
+            f"limit: {limit}, start_date: {start_date}"
+        )
         tweets = []  # tweets go in this list
 
         c = twint.Config()
         c.Limit = limit
         c.Search = query
         c.Store_object = True
-        c.Since = f"{start_date} 00:00:00"
-        c.Lang = "de"
+        c.Since = f'{start_date} 00:00:00'
+        c.Lang = 'de'
         c.Store_object_tweets_list = tweets
 
         # suppress twint output
-        with HiddenPrints():
+        with StreamToLogger(log_level=logging.DEBUG).activate():
             twint.run.Search(c)
 
-        tweets_df = pd.DataFrame([
+        return pd.DataFrame([
             {
-                "term": query,
-                "user_id": t.user_id,
-                "tweet_id": t.id,
-                "text": t.tweet,
-                "response_to": "",
-                "post_date": t.datestamp,
-                "permalink": t.link
+                'term': query,
+                'user_id': t.user_id,
+                'tweet_id': t.id,
+                'text': t.tweet,
+                'response_to': "",
+                'post_date': t.datestamp,
+                'permalink': t.link
             }
             for t in tweets
         ])
-
-        return tweets_df
-
-
-class HiddenPrints:
-    def __enter__(self):
-        self._original_stdout = sys.stdout
-        sys.stdout = open(os.devnull, 'w')
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        sys.stdout.close()
-        sys.stdout = self._original_stdout
