317b612ad092b6e2065616110e6b877925247c10
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2020-08-26 12:33:50 +0200

    tabs to spaces

--- scripts/migrations/migration_047.sql
+++ scripts/migrations/migration_047.sql
@@ -1,31 +1,31 @@
 BEGIN;
 
-	DROP TABLE twitter_extended_candidates;
-	DROP TABLE twitter_extended_dataset;
+    DROP TABLE twitter_extended_candidates;
+    DROP TABLE twitter_extended_dataset;
 
-	CREATE TABLE twitter_extended_candidates (
-		term TEXT,
-		user_id TEXT,
-		tweet_id TEXT,
-		text TEXT,
-		response_to TEXT,
-		post_date TIMESTAMP,
-		permalink TEXT,
-		likes INT,
-		retweets INT,
-		replies INT,
-		PRIMARY KEY (term, tweet_id)
-	);
-	CREATE TABLE twitter_extended_dataset (
-		user_id TEXT,
-		tweet_id TEXT,
-		text TEXT,
-		response_to TEXT,
-		post_date TIMESTAMP,
-		permalink TEXT,
-		likes INT,
-		retweets INT,
-		replies INT,
-		PRIMARY KEY (tweet_id)
-	);
+    CREATE TABLE twitter_extended_candidates (
+        term TEXT,
+        user_id TEXT,
+        tweet_id TEXT,
+        text TEXT,
+        response_to TEXT,
+        post_date TIMESTAMP,
+        permalink TEXT,
+        likes INT,
+        retweets INT,
+        replies INT,
+        PRIMARY KEY (term, tweet_id)
+    );
+    CREATE TABLE twitter_extended_dataset (
+        user_id TEXT,
+        tweet_id TEXT,
+        text TEXT,
+        response_to TEXT,
+        post_date TIMESTAMP,
+        permalink TEXT,
+        likes INT,
+        retweets INT,
+        replies INT,
+        PRIMARY KEY (tweet_id)
+    );
 COMMIT;
