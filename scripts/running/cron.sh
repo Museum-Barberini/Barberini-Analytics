1b20cbf807701eccfc3928fb773f43b03fc4e30e
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-09-07 23:02:46 +0200

    Refactor cron.sh
    Use tee for tempfile and ensure correct stream order

--- scripts/running/cron.sh
+++ scripts/running/cron.sh
@@ -2,38 +2,38 @@
 set -e
 set -o pipefail
 
-USER=$1-run
+export USER=$1-run
 BASEDIR=$(dirname "$0")/../..
 PATH="/usr/local/bin:$PATH"
-
+LOGPATH="/var/log/barberini-analytics"
+LOGFILE="$LOGPATH/$1-$(date +%Y-%m-%d).log"
+TMPFILE=$(mktemp --suffix=.log)
 EMAIL_STR="INFO: Sending email to \[.+\]"
-
 export BARBERINI_ANALYTICS_CONTEXT=PRODUCTION
 
-LOGPATH="/var/log/barberini-analytics"
-LOGFILE=$LOGPATH/"$1-$(date +%Y-%m-%d).log"
-TMPFILE=$(mktemp)
-
 # Delete 2 weeks old log
-rm $LOGPATH/"$1-$(date -d '2 weeks ago' +%Y-%m-%d).log" || true
+rm -f "$LOGPATH/$1-$(date -d '2 weeks ago' +%Y-%m-%d).log"
 
 
-if ! ({
-    echo "======================================================================="
-    echo "Starting $1 run at [$(date +"%Y-%m-%d %H:%M")]"
-    make -C "$BASEDIR" startup USER="$USER"
-    docker-compose -p "$USER" -f "$BASEDIR/docker/docker-compose.yml" exec -T \
-            barberini_analytics_luigi /app/scripts/running/fill_db.sh "$1"
-} > "$TMPFILE" 2>&1 \
-    || grep -Eq "$EMAIL_STR" "$TMPFILE"); then {
-    docker-compose -p "$USER" -f "$BASEDIR/docker/docker-compose.yml" exec -T \
-        barberini_analytics_luigi /app/scripts/running/notify_external_error.py "$1"
-} > "$TMPFILE" 2>&1
-fi
 {
-    make -C "$BASEDIR" shutdown USER="$USER"
+    echo "===================================================================="
+    echo "Starting $1 run at [$(date +"%Y-%m-%d %H:%M")]"
+
+    {
+        make -C "$BASEDIR" startup
+
+        docker-compose -p "$USER" -f "$BASEDIR/docker/docker-compose.yml" \
+            exec -T barberini_analytics_luigi \
+            /app/scripts/running/fill_db.sh "$1"
+    } |& tee "$TMPFILE" \
+        || grep -Eq "$EMAIL_STR" "$TMPFILE" \
+        || docker-compose -p "$USER" -f "$BASEDIR/docker/docker-compose.yml" \
+            exec -T barberini_analytics_luigi \
+            /app/scripts/running/notify_external_error.py "$1"
+
+    rm -f "$TMPFILE"
+    make -C "$BASEDIR" shutdown
+
     echo "Ending $1 run at [$(date +"%Y-%m-%d %H:%M")]"
     echo "======================================================================="
-} > "$TMPFILE" 2>&1
-
-cat "$TMPFILE" >> "$LOGFILE"
+} > "$LOGFILE" 2>&1
