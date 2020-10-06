84e098dc8c6c94f47b55ed676c993b0f5632e46a
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-10-06 13:02:38 +0200

    SQL MUST SHOUT

--- scripts/migrations/migration_050.sql
+++ scripts/migrations/migration_050.sql
@@ -7,6 +7,6 @@ BEGIN;
         ADD CONSTRAINT gomus_booking_customer_id_fkey
             FOREIGN KEY (customer_id)
             REFERENCES gomus_customer
-            ON UPDATE cascade;
+            ON UPDATE CASCADE;
 
 COMMIT;
