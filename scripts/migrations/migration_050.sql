e78e8344c8cb558bd452846b5e03e92088dee81b
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2020-10-06 12:59:26 +0200

    Fix ForeignKeyViolation in EnhanceBookingsWithScraper
    Closes #372

---
+++ scripts/migrations/migration_050.sql
@@ -0,0 +1,12 @@
+-- Fix ForeignKeyViolation in EnhanceBookingsWithScraper (#372)
+
+BEGIN;
+
+    ALTER TABLE gomus_booking
+        DROP CONSTRAINT gomus_booking_customer_id_fkey,
+        ADD CONSTRAINT gomus_booking_customer_id_fkey
+            FOREIGN KEY (customer_id)
+            REFERENCES gomus_customer
+            ON UPDATE cascade;
+
+COMMIT;
