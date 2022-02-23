523050a8eb662b9e47cd43e9b007bab3afa1e416
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2022-02-23 11:45:36 +0100

    Update gomus version to 4.1.14.9.01

--- tests/gomus/test_gomus_connection.py
+++ tests/gomus/test_gomus_connection.py
@@ -9,8 +9,8 @@ from db_test import DatabaseTestCase, logger
 from _utils import utils
 
 BASE_URL = 'https://barberini.gomus.de/'
-EXPECTED_VERSION_LINE_NUMBER = 770
-EXPECTED_VERSION_TAG = '4.1.14.7'
+EXPECTED_VERSION_LINE_NUMBER = 84
+EXPECTED_VERSION_TAG = '4.1.14.9.01'
 GOMUS_SESS_ID = os.environ['GOMUS_SESS_ID']
 
 
@@ -77,7 +77,7 @@ class TestGomusConnection(DatabaseTestCase):
             utils.strcoord(response.text, match.start(0))[0]: match
             for match in re.finditer(
                 r'''<small class='text-muted' title='go~mus Version'>
-(\d+\.\d+\.\d+(?:\.\d+)?)
+(\d+\.\d+\.\d+(?:\.\d+)*)
  - .+ Edition''',
                 response.text
             )
