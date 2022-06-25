e65b49a4e177472cf8afb9bf4a7733145a5e267f
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2022-06-25 18:19:15 +0200

    Fix Apple Appstore reviews permalink

---
+++ scripts/migrations/migration_059.sql
@@ -0,0 +1,14 @@
+-- Fix Apple Appstore reviews permalink (!448)
+
+BEGIN;
+
+	ALTER TABLE appstore_review
+        DROP COLUMN permalink,
+        ADD COLUMN permalink TEXT GENERATED ALWAYS AS (
+                'https://apps.apple.com/de/app/museum-barberini/id'
+                    || app_id
+                    || '?see-all=reviews'
+                -- Apple seems not to support review-specific URLs
+            ) STORED
+
+COMMIT;
