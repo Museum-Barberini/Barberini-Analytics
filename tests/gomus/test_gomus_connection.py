09e5dd3fe87782bb2c626933727002d48819bbb3
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2022-06-25 15:33:19 +0200

    Update gomus version to 4.1.15.12

--- tests/gomus/test_gomus_connection.py
+++ tests/gomus/test_gomus_connection.py
@@ -9,8 +9,8 @@ from db_test import DatabaseTestCase, logger
 from _utils import utils
 
 BASE_URL = 'https://barberini.gomus.de/'
-EXPECTED_VERSION_LINE_NUMBER = 770
-EXPECTED_VERSION_TAG = '4.1.14.11'
+EXPECTED_VERSION_LINE_NUMBER = 794
+EXPECTED_VERSION_TAG = '4.1.15.12'
 GOMUS_SESS_ID = os.environ['GOMUS_SESS_ID']
 
 
