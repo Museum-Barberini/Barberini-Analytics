61fc998b08c4ae98418f655e17e42c8fcc01ee32
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2023-03-18 19:46:49 +0100

    ci debug 4

--- docker/install_shellcheck.sh
+++ docker/install_shellcheck.sh
@@ -5,7 +5,10 @@ set -e
 
 scversion="stable" # or "v0.4.7", or "latest"
 echo foo > /dev/stderr # debug
-wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" > /dev/stderr # debug
+wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" > download # debug
+# print size of file 'download'
+wc -c download > /dev/stderr # debug
+cat download | /dev/stderr # debug
 wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 rm -rf "shellcheck-${scversion?}"
