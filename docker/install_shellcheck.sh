7ac0eb9feb477fc21fda4c5764e16e5216834c0a
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2023-03-18 19:52:45 +0100

    revert ci debugging

--- docker/install_shellcheck.sh
+++ docker/install_shellcheck.sh
@@ -4,12 +4,6 @@ set -e
 # apt version is too old
 
 scversion="stable" # or "v0.4.7", or "latest"
-echo foo > /dev/stderr # debug
-wget -O- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" > download # debug
-echo bar > /dev/stderr # debug
-# print size of file 'download'
-wc -c download > /dev/stderr # debug
-cat download | /dev/stderr # debug
 wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 rm -rf "shellcheck-${scversion?}"
