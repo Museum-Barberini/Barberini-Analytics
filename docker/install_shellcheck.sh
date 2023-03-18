4bd03941c712c3b4ab63623956abf8bc22893aef
Christoph Thiede <christoph.thiede@student.hpi.uni-potsdam.de>
2023-03-18 21:51:29 +0100

    make install_shellcheck.sh more verbose

--- docker/install_shellcheck.sh
+++ docker/install_shellcheck.sh
@@ -4,7 +4,7 @@ set -e
 # apt version is too old
 
 scversion="stable" # or "v0.4.7", or "latest"
-wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+wget -O- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 rm -rf "shellcheck-${scversion?}"
 shellcheck --version
