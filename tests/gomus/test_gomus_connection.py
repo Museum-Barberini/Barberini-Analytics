bd30c3f778842c4d3ee83e35f918c39e0709b39c
GitLab CI <GitLab CI noreply@gitlab.com>
2023-07-12 04:05:45 +0200

    Update gomus version number to v4.1.22.2

--- tests/gomus/test_gomus_connection.py
+++ tests/gomus/test_gomus_connection.py
@@ -11,8 +11,8 @@ from db_test import DatabaseTestCase, logger
 from _utils import utils
 
 BASE_URL = 'https://barberini.gomus.de/'
-EXPECTED_VERSION_LINE_NUMBER = 84
-EXPECTED_VERSION_TAG = '4.1.21.5'
+EXPECTED_VERSION_LINE_NUMBER = 798
+EXPECTED_VERSION_TAG = '4.1.22.2'
 GOMUS_SESS_ID = os.environ['GOMUS_SESS_ID']
 
 
